533746cd3aca2fa36119b176071975294ec09108
Alexander Vetter <vettera@gmail.com>
2015-08-27 17:35:55 +0200

    add fetchPage to Rows model

--- src/main/js/tableaux/models/Rows.js
+++ src/main/js/tableaux/models/Rows.js
@@ -1,14 +1,32 @@
 var Collection = require('ampersand-rest-collection');
+var _ = require('lodash');
 var apiUrl = require('../apiUrl');
 var Row = require('./Row');
 
 var Rows = Collection.extend({
   model : Row,
-  url : function() {
+
+  currentPage : {
+    offset : 0,
+    limit : 20
+  },
+
+  url : function () {
     return apiUrl('/tables/' + this.parent.getId() + '/rows');
   },
-  parse: function(resp) {
+
+  parse : function (resp) {
     return resp.rows;
+  },
+
+  fetchPage : function (options) {
+    // Defaults so the requested collections stays unchanged
+    options = options || {remove : false, merge : false, add : true};
+    options.data = options.data || this.currentPage;
+
+    this.fetch(options);
+
+    this.currentPage.offset += options.data.limit;
   }
 });
 
