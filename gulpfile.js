418503641994f0207846d9e6807127ad4d29e88c
Alexander Vetter <vettera@gmail.com>
2015-09-23 12:55:10 +0200

    remove unused stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 var gulp = require('gulp');
-
-var Path = require('path');
 var compass = require('gulp-compass');
+var plumber = require('gulp-plumber');
 var minifyCss = require('gulp-minify-css');
 var del = require('del');
-var browserSync = require('browser-sync');
-var spa = require("browser-sync-spa");
-var reload = browserSync.reload;
-var plumber = require('gulp-plumber');
-var karma = require('karma').server;
-var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
 var url = require('url');
+var Path = require('path');
+var reactify = require('reactify');
+var spa = require("browser-sync-spa");
+var browserify = require('browserify');
 var proxy = require('proxy-middleware');
+var browserSync = require('browser-sync');
+var source = require('vinyl-source-stream');
+
 var config = {"tableauxUrl" : "http://localhost:8080"};
 try {
   config = require('./config.json');
@@ -26,16 +24,15 @@ gulp.task('assets', assetCopy);
 gulp.task('appScript', scriptCompileApp);
 gulp.task('clean', clean);
 
-gulp.task('reload:scripts', ['appScript'], reload);
-gulp.task('reload:assets', ['assets'], reload);
-gulp.task('reload:sass', ['sass'], reload);
-gulp.task('dev', ['build'], server);
-gulp.task('test', ['build'], test);
-gulp.task('testWatch', ['build'], testWatch);
+gulp.task('reload:scripts', ['appScript'], browserSync.reload);
+gulp.task('reload:assets', ['assets'], browserSync.reload);
+gulp.task('reload:sass', ['sass'], browserSync.reload);
 
 gulp.task('build', ['sass', 'assets', 'appScript']);
-gulp.task('default', ['build']);
 
+gulp.task('dev', ['build'], server);
+
+gulp.task('default', ['build']);
 
 function sassCompile() {
   return gulp.src('src/main/scss/main.scss')
@@ -51,7 +48,8 @@ function sassCompile() {
       sass : 'src/main/scss',
       image : 'src/main/img'
     }))
-    //for speed now disabled .pipe(minifyCss())
+    //for speed now disabled
+    //.pipe(minifyCss())
     .pipe(gulp.dest('out/css'))
     .pipe(browserSync.reload({stream : true}));
 }
@@ -74,26 +72,6 @@ function assetCopy() {
     .pipe(gulp.dest('out/'));
 }
 
-function test(done) {
-  karma.start({
-    configFile : __dirname + '/karma.conf.js',
-    action : 'run',
-    proxies : {
-      '/api' : 'http://localhost:8181'
-    }
-  }, done);
-}
-
-function testWatch(done) {
-  karma.start({
-    configFile : __dirname + '/karma.conf.js',
-    action : 'watch',
-    proxies : {
-      '/api' : 'http://localhost:8181'
-    }
-  }, done);
-}
-
 function server() {
   var proxyOptions = url.parse(config.tableauxUrl);
   proxyOptions.route = '/api';
@@ -112,11 +90,6 @@ function server() {
   gulp.watch(['src/main/**', '!src/main/js/**', '!src/main/scss/**/*.scss'], {}, ['reload:assets']);
   gulp.watch(['src/main/js/**'], {}, ['reload:scripts']);
   gulp.watch(['src/main/scss/**/*.scss'], {}, ['sass']);
-
-  //gulp.src('src/test/**/*Spec.js').pipe(karma({
-  //  configFile : 'karma.conf.js',
-  //  action : 'watch'
-  //}));
 }
 
 function clean(cb) {
