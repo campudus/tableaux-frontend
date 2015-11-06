d26df50ca2449008edfaf601a1ffb39a0bc59071
Pixl <oe@campudus.com>
2015-11-06 15:00:27 +0100

    Sticky Header - needs review

--- src/main/js/components/Columns.jsx
+++ src/main/js/components/Columns.jsx
@@ -1,12 +1,64 @@
 var React = require('react');
 var AmpersandMixin = require('ampersand-react-mixin');
+var tableHeaderDefaultCoordinates;
 
 var Columns = React.createClass({
   mixins : [AmpersandMixin],
 
+  getInitialState : function () {
+    return {
+      stickyHeader : false,
+      tableHeaderDefaultCoordinates : {
+        x : 0,
+        y : 0
+      },
+      stickyHeaderOffset : 0
+    };
+  },
+
+  getHeaderHeight : function () {
+    return this.refs.tableHeader.getDOMNode().offsetHeight;
+  },
+
+  //add sticky class, also to parent and set correct x offset of header
+  handleScroll : function (event) {
+    var scrolledX = event.srcElement.body.scrollLeft;
+    var scrolledY = event.srcElement.body.scrollTop;
+
+    if (scrolledY >= this.state.tableHeaderDefaultCoordinates.y) {
+      this.setState({stickyHeader : true});
+      this.props.guiUpdate(true);
+    } else {
+      this.setState({stickyHeader : false});
+      this.props.guiUpdate(false);
+    }
+    this.setState({stickyHeaderOffset : -scrolledX + this.state.tableHeaderDefaultCoordinates.x});
+  },
+
+  componentDidUpdate : function () {
+
+  },
+
+  componentDidMount : function () {
+    var tableHeader = this.refs.tableHeader.getDOMNode();
+    var rect = tableHeader.getBoundingClientRect();
+    this.setState({
+      "tableHeaderDefaultCoordinates" : {
+        "x" : rect.left,
+        "y" : rect.top
+      }
+    });
+    window.addEventListener('scroll', this.handleScroll);
+  },
+
+  componentWillUnmount : function () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
   render : function () {
     return (
-      <div className="heading">
+      <div id="tableHeader" ref="tableHeader" className={this.state.stickyHeader ? 'heading sticky' : 'heading'}
+           style={{left: this.state.stickyHeaderOffset + "px"}}>
         <div className="column-head language" key="-1"></div>
         {this.props.columns.map(function (col, index) {
           return <div className="column-head" key={index}>{col.name}</div>;
--- src/main/js/components/Table.jsx
+++ src/main/js/components/Table.jsx
@@ -17,6 +17,13 @@ var Table = React.createClass({
     table : React.PropTypes.object.isRequired
   },
 
+  getInitialState : function () {
+    return {
+      stickyHeader : false,
+      headerHeight : 0
+    }
+  },
+
   componentWillMount : function () {
     var table = this.props.table;
 
@@ -27,15 +34,33 @@ var Table = React.createClass({
     });
   },
 
+  guiUpdate : function (isSticky) {
+    if (typeof isSticky !== 'undefined') {
+      this.setState({stickyHeader : isSticky});
+    }
+    this.setState({headerHeight : this.refs.columns.getHeaderHeight()});
+  },
+
   componentWillUnmount : function () {
     console.log('Table.componentWillUnmount', this.props.table.getId());
+    window.removeEventListener('scroll', this.handleScroll);
   },
 
   render : function () {
+
+    var self = this;
+
+    function paddingCalc() {
+      if (self.state.stickyHeader) {
+        return self.state.headerHeight + "px";
+      } else return 0;
+    }
+
     return (
       <section id="table-wrapper" ref="tableWrapper">
-        <div className="tableaux-table" ref="tableInner">
-          <Columns columns={this.props.table.columns}/>
+        <div style={{paddingTop: paddingCalc()}}
+             className={this.state.stickyHeader ? 'tableaux-table sticky' : 'tableaux-table'} ref="tableInner">
+          <Columns ref="columns" columns={this.props.table.columns} guiUpdate={this.guiUpdate}/>
 
           <Rows rows={this.props.table.rows} langtag={this.props.langtag}/>
 
