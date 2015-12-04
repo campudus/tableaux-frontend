9fd9a011c6fd339338b602403b6ff9227bbebf58
Pixl <oe@campudus.com>
2015-12-04 08:13:31 +0100

    wip

--- src/main/js/components/cell/BooleanCell.jsx
+++ src/main/js/components/cell/BooleanCell.jsx
@@ -11,68 +11,47 @@ var BooleanCell = React.createClass({
   },
 
   getInitialState : function () {
-    return {isEditing : false};
+    return {
+      checked : !!this.props.cell.value
+    }
   },
 
   componentDidMount : function () {
     console.log("Boolean did mount");
-    console.log(this.props.cell);
+    console.log(this.props.cell.column);
     console.log(this.props.cell.tableId);
     console.log(this.props.cell.value);
   },
 
-  handleLabelClick : function () {
-    this.setState({isEditing : true});
+  handleCheckboxClick : function (e) {
+    console.log(e.target.checked);
+    this.setState({checked : !this.state.checked});
+
+    //get the new value
+    var newValue = 0;
+    //this.handleEditDone(newValue);
   },
 
   handleEditDone : function (newValue) {
-    var cell = this.props.cell;
-
-    this.setState({isEditing : false});
-
-    if (cell.isMultiLanguage) {
-      var value = _.clone(cell.value);
-      value[this.props.language] = newValue;
-      newValue = value;
-    }
-
     Dispatcher.trigger(cell.changeCellEvent, {newValue : newValue});
   },
 
-  renderSingleLanguage : function () {
-    var cell = this.props.cell;
-    return (
-      <div className={'cell cell-' + cell.column.getId() + '-' + cell.rowId} onClick={this.handleLabelClick}>
-        {cell.value}
-      </div>
-    );
-  },
-
-  renderMultiLanguage : function () {
-    var cell = this.props.cell;
-    var language = this.props.language;
-    var value = cell.value[language];
-
-    return (
-      <div className={'cell cell-' + cell.column.getId() + '-' + cell.rowId} onClick={this.handleLabelClick}>
-        {value}
-      </div>
-    );
+  getBooleanValueFromCell : function () {
+    var cellValue = this.props.cell.value;
+    return !!cellValue;
   },
 
   render : function () {
     var cell = this.props.cell;
     var language = this.props.language;
 
-    if (!this.state.isEditing) {
-      if (cell.isMultiLanguage) {
-        return this.renderMultiLanguage();
-      } else {
-        return this.renderSingleLanguage();
-      }
-    } else {
-      return <NumericEditCell cell={cell} language={language} onSave={this.handleEditDone}/>;
-    }
+    return (
+        <div className={'cell cell-' + cell.column.getId() + '-' + cell.rowId}>
+          <input type="checkbox" checked={this.state.checked} onClick={this.handleCheckboxClick}/>
+          {cell.value}
+        </div>
+    );
+
   }
 });
 
