9f914d194174a0fae4b5802b40eac4aa30577a61
Max Stemplinger <ms@campudus.com>
2016-02-10 11:16:59 +0100

    progress bar in filechangeupload

---
+++ src/main/js/components/media/ProgressBar.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+
+var ProgressBar = React.createClass({
+
+  propTypes : {
+    progress : React.PropTypes.number.isRequired
+  },
+
+  render : function () {
+
+    var completed = this.props.progress;
+    if (completed < 0) {
+      completed = 0;
+    }
+    if (completed > 100) {
+      completed = 100;
+    }
+
+    var style = {
+      width : completed + '%',
+      transition : "width 100ms"
+    };
+
+    return (
+      <div className="progressbar-container">
+        <div className="progressbar-progress" style={style}>{completed + '%'}</div>
+      </div>
+    );
+  }
+});
+
+module.exports = ProgressBar;
