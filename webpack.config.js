0cd8b8da7464f8854fdadfd756facc2bf40eac8a
Pixl <oe@campudus.com>
2016-03-24 11:57:46 +0100

    Moving fonts to /fonts directory

--- webpack.config.js
+++ webpack.config.js
@@ -40,23 +40,23 @@ module.exports = {
     },
       {
         test : /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader : "url-loader?limit=10000&mimetype=application/font-woff2"
+        loader : "url-loader?limit=10000&name=./fonts/[hash].[ext]&mimetype=application/font-woff2"
       },
       {
         test : /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader : "url-loader?limit=10000&mimetype=application/font-woff"
+        loader : "url-loader?limit=10000&name=./fonts/[hash].[ext]&mimetype=application/font-woff"
       },
       {
         test : /\.ttf(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader : "url-loader?limit=10000&mimetype=application/font-ttf"
+        loader : "url-loader?limit=10000&name=./fonts/[hash].[ext]&mimetype=application/font-ttf"
       },
       {
         test : /\.eot(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader : "url-loader?limit=10000&mimetype=application/vnd.ms-fontobject"
+        loader : "url-loader?limit=10000&name=./fonts/[hash].[ext]&mimetype=application/vnd.ms-fontobject"
       },
       {
         test : /\.(svg|gif|jpg|jpeg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader : "file-loader"
+        loader : "file-loader?name=./img/[hash].[ext]"
       }
     ]
   },
