c926f643c8e4b34f1320a1d61f118b19b1e47acf
Pixl <oe@campudus.com>
2016-05-09 15:29:18 +0200

    Fixed incorrect merge

--- src/main/js/components/cells/link/LinkOverlay.jsx
+++ src/main/js/components/cells/link/LinkOverlay.jsx
@@ -1,18 +1,23 @@
-var React = require('react');
-var AmpersandMixin = require('ampersand-react-mixin');
-var _ = require('lodash');
-var OverlayHeadRowIdentificator = require('../../overlay/OverlayHeadRowIdentificator.jsx');
-var RowConcatHelper = require('../../../helpers/RowConcatHelper.js');
-var ActionCreator = require('../../../actions/ActionCreator');
-var XhrPoolMixin = require('../../mixins/XhrPoolMixin');
+const React = require('react');
+const _ = require('lodash');
+const OverlayHeadRowIdentificator = require('../../overlay/OverlayHeadRowIdentificator.jsx');
+const RowConcatHelper = require('../../../helpers/RowConcatHelper.js');
+const ActionCreator = require('../../../actions/ActionCreator');
+const XhrPoolMixin = require('../../mixins/XhrPoolMixin');
+import shallowCompare from 'react-addons-shallow-compare';
+import 'react-virtualized/styles.css';
+import { VirtualScroll } from 'react-virtualized';
+import {translate} from 'react-i18next';
 var apiUrl = require('../../../helpers/apiUrl');
 
-var LinkOverlay = React.createClass({
-  mixins : [AmpersandMixin, XhrPoolMixin],
+//we use this value to get the exact offset for the link list
+const CSS_SEARCH_HEIGHT = 70;
+
+const LinkOverlay = React.createClass({
+  mixins : [XhrPoolMixin],
 
   getInitialState : function () {
     return {
-      search : "",
       rowResults : {},
       loading : true
     };
@@ -21,29 +26,31 @@ var LinkOverlay = React.createClass({
   propTypes : {
     cell : React.PropTypes.object,
     langtag : React.PropTypes.string.isRequired,
-    tableId : React.PropTypes.number
+    tableId : React.PropTypes.number,
+    contentHeight : React.PropTypes.number,
+    contentWidth : React.PropTypes.number
   },
 
+  //saves all the results from server
+  allRowResults : {},
+
   componentWillMount : function () {
-    var self = this;
-    var toTableId = this.props.cell.column.toTable;
-    var toTable = this.props.cell.tables.get(toTableId);
+    let self = this;
+    let toTableId = this.props.cell.column.toTable;
+    let toTable = this.props.cell.tables.get(toTableId);
+    let rowXhr, colXhr;
 
-    /*
-     * TODO: Combine both api calls. There's a api route available: http://localhost:8080/completetable/1
-     * TBD: Ampersand Table Models
-     */
+    //Data already fetched, show it instantly and update it in the background
+    if (toTable.rows.length > 0) {
+      self.setRowResult(toTable.rows);
+    }
 
-    var rowXhr, colXhr;
     colXhr = toTable.columns.fetch({
       success : function () {
         rowXhr = toTable.rows.fetch({
           url : apiUrl('/tables/' + toTableId + '/columns/first/rows'),
           success : function () {
-            self.setState({
-              rowResults : toTable.rows,
-              loading : false
-            });
+            self.setRowResult(toTable.rows, true);
           },
           error : function (err) {
             console.log('error fetching rows', err);
@@ -58,22 +65,86 @@ var LinkOverlay = React.createClass({
     self.addAbortableXhrRequest(colXhr);
   },
 
+  /**
+   * Get the input string of the search field
+   * @returns {String} Search value lowercased and trimmed
+   */
+  getCurrentSearchValue : function () {
+    const searchRef = this.refs.search;
+    if (searchRef) {
+      let searchVal = this.refs.search.value;
+      return searchVal.toString().toLowerCase().trim();
+    } else {
+      return "";
+    }
+  },
+
   onSearch : function (event) {
-    var search = this.refs.search.value;
     this.setState({
-      search : search
+      rowResults : this.filterRowsBySearch(this.getCurrentSearchValue())
     });
   },
 
-  addLinkValue : function (isLinked, row, rowCellIdValue) {
-    var cell = this.props.cell;
+  //we set the row result depending if a search value is set
+  setRowResult : function (rowResult, fromServer) {
+    //just set the models, because we filter it later which also returns the models.
+    this.allRowResults = rowResult.models;
+    //data comes from server, so we rebuild the row names
+    if (fromServer) {
+      this.buildRowConcatString();
+    }
+    this.setState({
+      //we show all the rows
+      rowResults : this.filterRowsBySearch(this.getCurrentSearchValue()),
+      loading : false
+    });
+  },
 
-    var link = {
+  //Extends the model by a cached row name string
+  buildRowConcatString : function () {
+    const {allRowResults, props:{cell:{column:{toColumn}}}} = this;
+    _.forEach(allRowResults, (row)=> {
+      row["cachedRowName"] = RowConcatHelper.getRowConcatStringWithFallback(this.getRowValues(row), toColumn, this.props.langtag);
+    });
+  },
+
+  getRowValues : function (row) {
+    const {toColumn, toTable} = this.props.cell.column;
+    const toTableObj = this.props.cell.tables.get(toTable);
+    const toTableColumns = toTableObj.columns;
+    const toIdColumnIndex = toTableColumns.indexOf(toTableColumns.get(toColumn.id)); //This is the index of the identifier / concat column 
+    return row.values[toIdColumnIndex];
+  },
+
+  //searchval is already trimmed and to lowercase
+  filterRowsBySearch : function (searchVal) {
+    let newRowResults = {};
+    let {allRowResults} = this;
+
+    if (searchVal !== "" && allRowResults.length > 0) {
+      newRowResults = allRowResults.filter((row)=> {
+        let rowName = row["cachedRowName"].toLowerCase();
+        return _.every(_.words(searchVal), function (word) {
+          return rowName.indexOf(word) > -1;
+        });
+      });
+    }
+    else {
+      newRowResults = allRowResults;
+    }
+    return newRowResults;
+  },
+
+  addLinkValue : function (isLinked, row, event) {
+    event.preventDefault();
+    const cell = this.props.cell;
+    const rowCellIdValue = this.getRowValues(row);
+
+    const link = {
       id : row.id,
       value : rowCellIdValue
     };
-
-    var links = _.clone(cell.value);
+    let links = _.clone(cell.value);
 
     if (isLinked) {
       _.remove(links, function (linked) {
@@ -82,9 +153,10 @@ var LinkOverlay = React.createClass({
     } else {
       links.push(link);
     }
-
     ActionCreator.changeCell(cell.tableId, cell.rowId, cell.id, links);
 
+    //tell the virtual scroller to redraw
+    this.refs.OverlayScroll.forceUpdate();
   },
 
   closeOverlay : function () {
@@ -95,73 +167,60 @@ var LinkOverlay = React.createClass({
     return (stringToCheck && stringToCheck.trim() !== "");
   },
 
-  renderOverlay : function () {
-    var self = this;
-    var listItems = null;
-    var cell = this.props.cell;
-    var toColumn = self.props.cell.column.toColumn;
-    var currentCellValue = cell ? cell.value : null;
-    var toTableId = this.props.cell.column.toTable;
-    var toTable = this.props.cell.tables.get(toTableId);
-    var toTableColumns = toTable.columns;
-    var toIdColumnIndex = toTableColumns.indexOf(toTableColumns.get(toColumn.id)); //This is the index of the identifier / concat column
+  getOverlayItem : function (index) {
+    let {rowResults} = this.state;
+    let cell = this.props.cell;
+    let currentCellValue = cell ? cell.value : null;
+    let row = rowResults[index];
 
-    //check for empty obj or map fails
-    if (!_.isEmpty(this.state.rowResults)) {
-
-      listItems = (
-        <ul>
-          {this.state.rowResults.map(function (row) {
-
-            var rowConcatString;
-            var isLinked = !!_.find(currentCellValue, function (link) {
-              return link.id === row.id;
-            });
-            var rowCellIdValue = row.values[toIdColumnIndex];
-            rowConcatString = RowConcatHelper.getRowConcatStringWithFallback(rowCellIdValue, toColumn, self.props.langtag);
-
-            //Search filter
-            if (_.isString(rowConcatString) && self.state.search !== null) {
-              var found = _.every(_.words(self.state.search.toString().toLowerCase().trim()), function (word) {
-                return rowConcatString.toLowerCase().indexOf(word) > -1;
-              });
-
-              if (found) {
-                if (rowConcatString && rowConcatString !== "") {
-                  return <li key={row.id} className={isLinked ? 'isLinked' : ''}
-                             onClick={self.addLinkValue.bind(self, isLinked, row, rowCellIdValue)}>{rowConcatString}</li>;
-                } else {
-                  return null;
-                }
-              }
-            }
-          })}
-
-        </ul>
-      );
-
-    } else {
-      listItems = "No data in table '" + self.props.cell.column.name + "'";
+    if (!_.isEmpty(rowResults) && !_.isEmpty(row)) {
+      let isLinked = !!_.find(currentCellValue, (link) => {
+        return link.id === row.id;
+      });
+      let rowName = row["cachedRowName"];
+      return <a href="#" key={row.id} className={isLinked ? 'isLinked overlay-table-row' : 'overlay-table-row'}
+                onClick={this.addLinkValue.bind(this, isLinked, row)}>{rowName}</a>;
     }
+  },
 
-    return listItems;
+  noRowsRenderer : function () {
+    const {t} = this.props;
+    return (
+      <div className="error">
+        {this.getCurrentSearchValue().length > 0 ? t('search_no_results') : t('overlay_no_rows_in_table')}
+      </div>);
   },
 
   render : function () {
-    var listDisplay;
+    let listDisplay;
+    const {rowResults, loading} = this.state;
+    const {contentHeight, contentWidth} = this.props;
+    const rowsCount = rowResults.length || 0;
 
-    if (this.state.loading) {
+    if (loading) {
       listDisplay = "Loading...";
     } else {
-      listDisplay = this.renderOverlay();
+      listDisplay = (
+        <VirtualScroll
+          ref="OverlayScroll"
+          width={contentWidth}
+          height={contentHeight - CSS_SEARCH_HEIGHT}
+          rowsCount={rowsCount}
+          rowHeight={50}
+          rowRenderer={this.getOverlayItem}
+          noRowsRenderer={this.noRowsRenderer}
+        />
+      );
     }
 
     return (
       <div>
-        <div className="search-input-wrapper">
-          <input type="text" className="search-input" placeholder="Search..." onChange={this.onSearch}
-                 defaultValue={this.state.search} ref="search" autoFocus/>
-          <i className="fa fa-search"></i>
+        <div className="search-input-wrapper2" style={{height:CSS_SEARCH_HEIGHT}}>
+          <div className="search-input-wrapper">
+            <input type="text" className="search-input" placeholder="Search..." onChange={this.onSearch} ref="search"
+                   autoFocus/>
+            <i className="fa fa-search"></i>
+          </div>
         </div>
         {listDisplay}
       </div>
@@ -170,4 +229,4 @@ var LinkOverlay = React.createClass({
 
 });
 
-module.exports = LinkOverlay;
+module.exports = translate(['table'])(LinkOverlay);
