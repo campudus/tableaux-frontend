c430a83eb1f972910dcfd0a62bb0a273a407493d
Pixl <oe@campudus.com>
2016-06-23 00:11:40 +0200

    changed loading behaviour of dependent data. Shows instant overlay to user [WIP]

--- src/main/js/components/overlay/ConfirmDependentOverlay.jsx
+++ src/main/js/components/overlay/ConfirmDependentOverlay.jsx
@@ -18,65 +18,112 @@ DeleteRowOverlayFooter.propTypes = {
 };
 
 //Builds the actual dependent tables/rows DOM elements
-let DependentTablesAndRows = (props) => {
+class DependentTablesAndRows extends React.Component {
 
-  const {dependency, langtag} = props;
+  static propTypes = {
+    row : React.PropTypes.object.isRequired,
+    langtag : React.PropTypes.string.isRequired
+  };
 
-  if (dependency && dependency.length > 0) {
-    let dependentTables = dependency.map((dep)=> {
-      const {table, column, rows} = dep;
-      const tableId = table.id;
-      const linkToTable = `/${langtag}/table/${tableId}`;
+  state = {
+    loadingDependency : true,
+    dependency : null
+  };
 
-      //Builds dependent rows inside dependent tables
-      const rowsDisplay = rows.map((row, idx)=> {
-        const dependentRowDisplayLabel = RowConcatHelper.getRowConcatStringWithFallback(row.value, column, langtag);
+  constructor(props) {
+    super(props);
+    this.checkDependency();
+  }
+
+  componentWillMount() {
+
+  }
+
+  checkDependency() {
+    //check dependent rows
+    this.props.row.dependent(
+      (error)=> {
+        console.log("checkDependency dependent error:", error);
+        alert("Dependencies could not be checked. Please try again.");
+      },
+      (res) => {
+        this.setState({
+          dependency : res.dependent,
+          loadingDependency : false
+        });
+      }
+    );
+  }
+
+
+  render() {
+    const {langtag} = props;
+    const {loadingDependency, dependency} = this.state;
+    let dependentInfoText = null, dependentTables = null;
+
+    if (loadingDependency) {
+      return <div className="dependent-loading-data">Fetching dependent data...</div>;
+    }
+
+    if (dependency && dependency.length > 0) {
+
+      dependentInfoText = i18n.t('table:delete_row_dependent_text');
+
+      dependentTables = dependency.map((dep)=> {
+        const {table, column, rows} = dep;
+        const tableId = table.id;
+        const linkToTable = `/${langtag}/table/${tableId}`;
+
+        //Builds dependent rows inside dependent tables
+        const rowsDisplay = rows.map((row, idx)=> {
+          const dependentRowDisplayLabel = RowConcatHelper.getRowConcatStringWithFallback(row.value, column, langtag);
+          return (
+            <div key={idx} className="dependent-row">
+              <span className="dependent-row-id">#{row.id}</span>
+              {dependentRowDisplayLabel}
+            </div>
+          );
+        });
+
+        //Builds dependent tables
         return (
-          <div key={idx} className="dependent-row">
-            <span className="dependent-row-id">#{row.id}</span>
-            {dependentRowDisplayLabel}
+          <div key={tableId} className="dependent-table">
+            <a className="table-link" href={linkToTable} target="_blank"><i
+              className="fa fa-columns"/>{getTableDisplayName(table, langtag)}<i className="fa fa-angle-right"/></a>
+            <div className="dependent-rows">
+              {rowsDisplay}
+            </div>
           </div>
         );
       });
 
-      //Builds dependent tables
-      return (
-        <div key={tableId} className="dependent-table">
-          <a className="table-link" href={linkToTable} target="_blank"><i
-            className="fa fa-columns"/>{getTableDisplayName(table, langtag)}<i className="fa fa-angle-right"/></a>
-          <div className="dependent-rows">
-            {rowsDisplay}
-          </div>
-        </div>
-      );
-    });
+    } else {
+      dependentInfoText = i18n.t('table:delete_row_no_dependent_rows');
+    }
 
     return (
       <div className="dependent-wrapper">
+        <p className="dependent-row-info">{dependentInfoText}</p>
         {dependentTables}
       </div>
     );
-  } else return null;
-};
-DependentTablesAndRows.propTypes = {
-  dependency : React.PropTypes.array.isRequired,
-  langtag : React.PropTypes.string.isRequired
-};
+
+  }
+
+
+}
 
 
 let DeleteRowOverlayBody = (props) => {
-  const {dependency, row, langtag} = props;
+  const {row, langtag} = props;
   const firstCell = row.cells.at(0);
   const rowDisplayLabel = RowConcatHelper.getRowConcatStringWithFallback(firstCell.value, firstCell.column, langtag);
 
-  const builtDependentView = <DependentTablesAndRows dependency={dependency} langtag={langtag}/>;
+  const builtDependentView = <DependentTablesAndRows row={row} langtag={langtag}/>;
   return (
     <div>
       <div className="delete-row-question">
         <h1>{i18n.t('table:confirm_delete_row', {rowName : rowDisplayLabel})}</h1>
-        {builtDependentView ?
-          <p>{i18n.t('table:delete_row_dependent_text')}</p> :
-          <p>{i18n.t('table:delete_row_no_dependent_rows')}</p>}
       </div>
       {builtDependentView}
     </div>
@@ -86,8 +133,7 @@ DeleteRowOverlayBody.propTypes = {
   dependency : React.PropTypes.array
 };
 
-function openDependantOverlay(row, langtag, dependency) {
-  console.log("openDependantOverlay got dependentContent:", dependency);
+function openDependantOverlay(row, langtag) {
 
   const onYesRowDelete = () => {
     //removeRow(row.tableId, row.getId());
@@ -97,21 +143,24 @@ function openDependantOverlay(row, langtag, dependency) {
 
   openOverlay({
     head : <span>{i18n.t('table:delete_row')}</span>,
-    body : <DeleteRowOverlayBody row={row} dependency={dependency} langtag={langtag}/>,
+    body : <DeleteRowOverlayBody row={row} langtag={langtag}/>,
     footer : <DeleteRowOverlayFooter onYes={onYesRowDelete}/>,
     type : "normal"
   });
 }
 
 export function confirmDeleteRow(row, langtag) {
+
+  openDependantOverlay(row, langtag);
+
   //check dependent rows
-  row.dependent(
-    (error)=> {
-      console.log("confirmaDeleteRow dependent error:", error);
-    },
-    (res) => {
-      openDependantOverlay(row, langtag, res);
-    }
-  );
+  /*row.dependent(
+   (error)=> {
+   console.log("confirmaDeleteRow dependent error:", error);
+   },
+   (res) => {
+   openDependantOverlay(row, langtag, res);
+   }
+   );*/
 }
 
