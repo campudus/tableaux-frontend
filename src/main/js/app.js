3633a1a503a8a77bc10acf3d0e0abe7a2b40ea41
Alexander Vetter <vettera@gmail.com>
2017-01-16 17:46:53 +0100

    cleanup app js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,19 +1,14 @@
 import App from 'ampersand-app';
 import Router from './router';
-import Dispatcher from './dispatcher/Dispatcher';
-import apiUrl from './helpers/apiUrl';
-import multiLanguage from './helpers/multiLanguage';
 import TableauxConstants from './constants/TableauxConstants';
 import {getAllLangtagsFromServer} from './helpers/serverSettingsHelper';
 import {initDevelopmentAccessCookies} from './helpers/accessManagementHelper';
-import _ from 'lodash';
+import '../index.html';
+import '../scss/main.scss';
 
 if (process.env.NODE_ENV != 'production') {
   window.Perf = require('react-addons-perf');
 }
-
-import '../index.html';
-import '../scss/main.scss';
 
 App.extend({
 
@@ -26,9 +21,9 @@ App.extend({
     window.GLOBAL_TABLEAUX = {};
 
     //init all available langtags from server before continuing
-    getAllLangtagsFromServer((err)=> {
+    getAllLangtagsFromServer((err) => {
       console.warn("error:", err);
-    }, (languages)=> {
+    }, (languages) => {
       TableauxConstants.initLangtags(languages);
       this.router = new Router();
       this.router.history.start();
