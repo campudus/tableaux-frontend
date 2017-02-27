be27914f708e7e4008d5e4d531e6c39a7f4d9047
Hermann Pauly <hp@campudus.com>
2017-02-27 13:39:54 +0100

    Make overlays stackable

--- src/main/js/components/Tableaux.jsx
+++ src/main/js/components/Tableaux.jsx
@@ -9,6 +9,7 @@ import {I18nextProvider} from "react-i18next";
 import ActionCreator from "../actions/ActionCreator";
 import Spinner from "./header/Spinner.jsx";
 import Toast from "./overlay/Toast.jsx";
+import * as f from "lodash/fp";
 
 const ActionTypes = TableauxConstants.ActionTypes;
 
@@ -60,6 +61,16 @@ export default class Tableaux extends React.Component {
           isLoading: false
         });
       });
+
+    this.state = {
+      currentView: this.props.initialViewName,
+      currentViewParams: this.props.initialParams,
+      activeOverlays: [],
+      isLoading: true,
+      toast: null
+    };
+
+    this.toastTimer = null;
   }
 
   componentWillUnmount() {
@@ -91,8 +102,7 @@ export default class Tableaux extends React.Component {
   }
 
   openOverlay(content) {
-    var newViewParams = Object.assign({}, this.state.currentViewParams);
-    newViewParams.overlayOpen = true;
+    const {currentViewParams, activeOverlays} = this.state;
     this.setState({
       activeOverlay: content,
       currentViewParams: newViewParams
@@ -100,20 +110,22 @@ export default class Tableaux extends React.Component {
   }
 
   closeOverlay() {
-    var newViewParams = Object.assign({}, this.state.currentViewParams);
-    newViewParams.overlayOpen = false;
+    const {currentViewParams, activeOverlays} = this.state;
     this.setState({
       activeOverlay: null,
       currentViewParams: newViewParams
     });
   }
 
-  renderActiveOverlay() {
-    let overlay = this.state.activeOverlay;
-    if (overlay) {
+  renderActiveOverlays() {
+    let overlays = this.state.activeOverlays;
+    if (f.isEmpty(overlays)) {
+      return null;
+    }
+    return overlays.map((overlay, idx) => {
       return (
         <GenericOverlay
-          key="genericoverlay"
+          key={`genericoverlay-${idx}`}
           head={overlay.head}
           footer={overlay.footer}
           type={overlay.type}
@@ -122,14 +134,14 @@ export default class Tableaux extends React.Component {
           {overlay.body}
         </GenericOverlay>
       );
-    }
+    });
   }
 
   renderToast() {
     const {toast} = this.state;
     if (toast) {
       console.log("render toast");
-      return (<Toast content={toast}/>);
+      return (<Toast content={toast} />);
     }
   }
 
@@ -162,8 +174,8 @@ export default class Tableaux extends React.Component {
     } else {
       return <I18nextProvider i18n={i18n}>
         <div id="tableaux-view">
-          <ViewRenderer viewName={this.state.currentView} params={this.state.currentViewParams}/>
-          {this.renderActiveOverlay()}
+          <ViewRenderer viewName={this.state.currentView} params={this.state.currentViewParams} />
+          {this.renderActiveOverlays()}
           {this.renderToast()}
         </div>
       </I18nextProvider>;
