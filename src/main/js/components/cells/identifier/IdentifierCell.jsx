7c9d4dd05b8f097d8298cd643e7372e91ad53e2d
Hermann Pauly <hp@campudus.com>
2017-06-20 09:46:44 +0200

    Make GroupCell open editor on enter

--- src/main/js/components/cells/identifier/IdentifierCell.jsx
+++ src/main/js/components/cells/identifier/IdentifierCell.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, {PropTypes} from "react";
 import {openEntityView} from "../../overlay/EntityViewOverlay";
 import connectToAmpersand from "../../helperComponents/connectToAmpersand";
+import {isLocked} from "../../../helpers/annotationHelper";
 
 const IdentifierCell = (props) => {
-  const {cell, langtag, selected} = props;
+  const {cell, editing, langtag, selected, setCellKeyboardShortcuts} = props;
+  const openEditor = () => ((selected || editing) && !isLocked(cell.row))
+    ? openEntityView(cell.row, langtag, null, null, cell.column)
+    : function () {};
+  setCellKeyboardShortcuts({
+    enter: openEditor
+  });
   return (
-    <div className='cell-content' onClick={
-      () => (selected)
-        ? openEntityView(cell.row, langtag, null, null, cell.column)
-        : function () {
-        }
-    }>
+    <div className='cell-content' onClick={openEditor}>
       {cell.displayValue[langtag]}
     </div>
   );
 };
 
 IdentifierCell.propTypes = {
-  langtag: React.PropTypes.string.isRequired,
-  cell: React.PropTypes.object.isRequired,
-  editing: React.PropTypes.bool.isRequired,
-  selected: React.PropTypes.bool.isRequired
+  langtag: PropTypes.string.isRequired,
+  cell: PropTypes.object.isRequired,
+  editing: PropTypes.bool.isRequired,
+  selected: PropTypes.bool.isRequired,
+  setCellKeyboardShortcuts: PropTypes.func.isRequired
 };
 
 module.exports = connectToAmpersand(IdentifierCell);
