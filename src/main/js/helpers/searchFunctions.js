cc8506dddad6fa5d25366d3e9533257b89d50af7
Hermann Pauly <hp@campudus.com>
2017-10-04 11:13:02 +0200

    Minor refactor _fp usage

--- src/main/js/helpers/searchFunctions.js
+++ src/main/js/helpers/searchFunctions.js
@@ -1,4 +1,4 @@
-import * as f from "lodash/fp";
+import f from "lodash/fp";
 import {FilterModes} from "../constants/TableauxConstants";
 
 const clean = f.flow(f.toLower, f.trim); // normalise string
@@ -8,9 +8,8 @@ const clean = f.flow(f.toLower, f.trim); // normalise string
 const SearchFunctions = {
   [FilterModes.CONTAINS]: f.curry(
     (stringOfFilters, str) => {
-      const fcontains = a => b => f.contains(b)(a);
       return f.every(
-        fcontains(clean(str)),
+        f.contains(f, clean(str)),
         f.words(clean(stringOfFilters)));
     }),
   [FilterModes.STARTS_WITH]: f.curry(
