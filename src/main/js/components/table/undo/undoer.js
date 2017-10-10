1d7d6dd1d4f03e37a8211c6cf3df6f112e91810e
Hermann Pauly <hp@campudus.com>
2017-10-10 11:34:56 +0200

    Add undo functionality

---
+++ src/main/js/components/table/undo/undoer.js
@@ -0,0 +1,51 @@
+/**
+ * A LIFO event listener that watches cell changes
+ */
+
+import f from "lodash/fp";
+import Dispatcher from "../../../dispatcher/Dispatcher";
+import {ActionTypes} from "../../../constants/TableauxConstants";
+import {changeCell} from "../../../models/Tables";
+
+const MAX_UNDO_STEPS = 50;
+
+const undoStacks = {};
+let currentTable;
+
+Dispatcher.on(ActionTypes.SWITCH_TABLE, setCurrentTable);
+
+const setCurrentTable = (tableId) => {
+  currentTable = tableId;
+};
+
+const getCurrentStack = f.getOr([], currentTable, undoStacks);
+
+const push = (data) => {
+  undoStacks[currentTable] = f.flow(
+    (s) => [...s, data],
+    f.takeRight(MAX_UNDO_STEPS)
+  )(getCurrentStack());
+};
+
+const pop = () => {
+  const stack = getCurrentStack();
+  undoStacks[currentTable] = f.dropRight(1, stack);
+  return f.last(stack);
+};
+
+export const peek = () => f.last(getCurrentStack());
+
+export async function undo() {
+  const {cell, value} = peek();
+  await changeCell(cell, value, f.noop, {type: "UNDO"});
+  return pop();
+}
+
+export const remember = ({cell, value}) => {
+  const {tableId} = cell;
+  setCurrentTable(tableId);
+  push({
+    cell,
+    value
+  });
+};
