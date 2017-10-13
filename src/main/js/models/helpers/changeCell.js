e0e9cc78a9da121ce3a1ddba4c09c6167665e86d
Hermann Pauly <hp@campudus.com>
2017-10-13 11:01:19 +0200

    Fix link reorder function extracting bad ids

--- src/main/js/models/helpers/changeCell.js
+++ src/main/js/models/helpers/changeCell.js
@@ -19,7 +19,10 @@ async function changeCell({cell, value, options = {}}) {
   window.devLog(`Changing ${cell.kind} cell ${cell.id} from`, cell.value, "to", (value.value || value));
   Raven.captureBreadcrumb({
     message: `Change cell ${cell.id}`,
-    data: value,
+    data: {
+      before: cell.value,
+      after: value
+    },
     category: (f.matchesProperty("type", "UNDO") ? "undo" : "direct change")
   });
 
@@ -153,9 +156,11 @@ async function changeLinkCell({cell, value}) {
 
 async function reorderLinks({cell, value}) {
   const swappers = f.flow(
-    f.reject(([a, b]) => f.get("id", a) === f.get("id", b)),
-    f.map([0, "id"])
-  )(f.zip(value, cell.value));
+    f.reject(([a, b]) => f.get("id", a) === f.get("id", b)), // remove all elements which kept their position
+    f.map(f.dropRight(1)),                                   // reduce remaining tuples to single entries in two steps
+    f.flatten,                                               //   thus only the swapped elements remain
+    f.map("id"),                                             // retrieve ids of swapped elements in order after swap
+  )(f.zip(value, cell.value)); // Input: tuples of element at index i before and after swap
   cell.set({value: f.map(f.pick(["id", "value"]), value)}); // don't store LinkOverlay's display value
   const sortUrl = `/tables/${cell.tableId}/columns/${cell.column.id}/rows/${cell.row.id}/link/${f.first(swappers)}/order`;
   await request
