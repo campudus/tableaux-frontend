c83cd601cbf311cd4cd1da76ffd866fef13ba783
Hermann Pauly <hp@campudus.com>
2017-12-18 14:27:36 +0100

    Make sure date picker is always in browser window

--- src/main/js/components/cells/date/DateEditCell.jsx
+++ src/main/js/components/cells/date/DateEditCell.jsx
@@ -1,15 +1,40 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import f from "lodash/fp";
 import Datetime from "react-datetime";
-import {compose, lifecycle, withStateHandlers} from "recompose";
+import {compose, lifecycle, withHandlers, withStateHandlers} from "recompose";
 
 const enhance = compose(
   withStateHandlers(
     () => ({
-      shiftUp: false
+      shiftUp: false,
+      domNode: null
     }),
-    {}
+    {
+      checkPosition: ({domNode, shiftUp}) => (node = domNode) => {
+        if (f.isNil(node)) {
+          return;
+        }
+
+        const rect = ReactDOM.findDOMNode(node).getBoundingClientRect();
+        const needsShiftUp = rect.bottom + 265 >= window.innerHeight;
+        if (needsShiftUp !== shiftUp) {
+          return {
+            shiftUp: needsShiftUp,
+            domNode: node
+          };
+        } else if (f.isNil(domNode)) {
+          return {domNode: node};
+        }
+      }
+    }
   ),
+  withHandlers({
+    getStyle: ({shiftUp}) => () => ({
+      position: "absolute",
+      top: (shiftUp) ? -265 : "100%"
+    })
+  }),
   lifecycle({
     componentWillUnmount() {
       this.props.saveChanges();
@@ -17,20 +42,24 @@ const enhance = compose(
   })
 );
 
-const DateEditCell = ({value, Formats, handleChange, clearDate, showTime}) => (
-  <React.Fragment>
+const DateEditCell = ({value, Formats, handleChange, clearDate, showTime, checkPosition, getStyle}) => (
+  <div ref={checkPosition}>
     {(f.isEmpty(value)) ? "" : value.format(Formats.formatForUser)}
     <i className="fa fa-ban"
        onClick={clearDate}
     />
 
-    <Datetime onChange={handleChange}
-              open
-              input={false}
-              value={value}
-              timeFormat={showTime}
-    />
-  </React.Fragment>
+    <div className="time-picker-wrapper"
+         style={getStyle()}
+    >
+      <Datetime onChange={handleChange}
+                open
+                input={false}
+                value={value}
+                timeFormat={showTime}
+      />
+    </div>
+  </div>
 );
 
 export default enhance(DateEditCell);
