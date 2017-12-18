58ba05c74865349d1d915f8322c1aaf34944ee3e
Hermann Pauly <hp@campudus.com>
2017-12-18 13:37:33 +0100

    Refactor DateCell and DateTimeCell

--- src/main/js/components/cells/date/DateCell.jsx
+++ src/main/js/components/cells/date/DateCell.jsx
@@ -1,88 +1,56 @@
-/*
- * Cell representing a date value; displays a DateEditCell when editing === true
- * Will not look for internationalisation, as date does not depend on timezone.
- */
 import React from "react";
-import DateEditCell from "./DateEditCell";
-import {DateFormats} from "../../../constants/TableauxConstants";
+import {branch, compose, pure, renderComponent, withProps, withStateHandlers} from "recompose";
 import Moment from "moment";
+import f from "lodash/fp";
+import {ColumnKinds, DateFormats, DateTimeFormats} from "../../../constants/TableauxConstants";
 import ActionCreator from "../../../actions/ActionCreator";
-import {either} from "../../../helpers/functools";
-import PropTypes from "prop-types";
+import DateEditCell from "./DateEditCell";
 
-class DateCell extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {value: this.getSavedMoment()};
-  }
-
-  getSavedMoment = () => {
-    const {cell, langtag} = this.props;
-    const momStr = (cell.isMultiLanguage)
-      ? cell.value[langtag]
-      : cell.value;
-    return (momStr) ? Moment(momStr) : null;
-  };
-
-  componentWillReceiveProps(newProps) {
-    if (!this.props.editing && newProps.editing) {
-      this.setState({value: this.getSavedMoment()});
+const enhance = compose(
+  pure,
+  withProps(
+    (props) => {
+      const showTime = f.get("kind", props.cell) === ColumnKinds.datetime;
+      return {
+        showTime,
+        Formats: (showTime)
+          ? DateTimeFormats
+          : DateFormats
+      };
     }
-  }
+  ),
+  withStateHandlers(
+    (props) => ({value: (f.isEmpty(props.value)) ? null : Moment(props.value)}),
+    {
+      handleChange: () => (moment) => ({value: moment}),
+      clearDate: () => () => ({value: null}),
+      saveChanges: (state, props) => () => {
+        const {Formats, cell, langtag} = props;
+        const curVal = (f.isEmpty(state.value))
+          ? null
+          : state.value.format(Formats.formatForServer);
+        if (curVal === props.value) {
+          return;
+        }
 
-  momentToString = moment => {
-    return (moment && moment.isValid())
-      ? moment.format(DateFormats.formatForUser)
-      : "";
-  };
-
-  finishedEditing = save => {
-    if (save) {
-      const {cell, langtag} = this.props;
-      const inputDate = this.state.value;
-      const savedDateString = this.momentToString(this.getSavedMoment());
-      if (savedDateString !== this.momentToString(inputDate)) {
-        const newValue = either(inputDate)
-          .map(m => m.format(DateFormats.formatForServer))
-          .getOrElse(null);
-        ActionCreator.changeCell(cell, (cell.isMultiLanguage) ? {[langtag]: newValue} : newValue);
+        const valueToSave = (cell.isMultiLanguage)
+          ? {[langtag]: curVal}
+          : curVal;
+        ActionCreator.changeCell(cell, valueToSave);
       }
-    } else {
-      this.setState({value: this.getSavedMoment()});
     }
-  };
+  )
+);
 
-  handleDateUpdate = (moment, cb) => {
-    (cb)
-      ? this.setState({value: moment}, cb)
-      : this.setState({value: moment});
-  };
+const DateCell = branch(
+  (props) => props.editing,
+  renderComponent(DateEditCell)
+)(
+  ({value, Formats}) => (
+    <div className="cell-content">
+      {(f.isEmpty(value)) ? "" : value.format(Formats.formatForUser)}
+    </div>
+  )
+);
 
-  render = () => {
-    const {cell, editing} = this.props;
-    if (!editing) {
-      return (
-        <div className="cell-content">
-          {this.momentToString(this.getSavedMoment())}
-        </div>
-      );
-    } else {
-      return (
-        <DateEditCell setCellKeyboardShortcuts={this.props.setCellKeyboardShortcuts}
-          toDisplayValue={this.momentToString}
-          handleDateUpdate={this.handleDateUpdate}
-          handleEditFinished={this.finishedEditing}
-          value={this.state.value || new Moment()}
-          cell={cell} />
-      );
-    }
-  }
-}
-
-DateCell.propTypes = {
-  editing: PropTypes.bool.isRequired,
-  setCellKeyboardShortcuts: PropTypes.func.isRequired,
-  langtag: PropTypes.string.isRequired
-};
-
-module.exports = DateCell;
+export default enhance(DateCell);
--- src/main/js/components/cells/date/DateEditCell.jsx
+++ src/main/js/components/cells/date/DateEditCell.jsx
@@ -1,98 +1,36 @@
-/*
- * Allows to edit value of a DateCell in a Datetime date picker (react-datetime)
- */
 import React from "react";
-import listensToClickOutside from "react-onclickoutside";
-import ActionCreator from "../../../actions/ActionCreator";
+import f from "lodash/fp";
 import Datetime from "react-datetime";
-import {Directions} from "../../../constants/TableauxConstants";
-import PropTypes from "prop-types";
+import {compose, lifecycle, withStateHandlers} from "recompose";
 
-@listensToClickOutside
-class DateEditCell extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      moment: this.props.value
-    };
-  }
+const enhance = compose(
+  withStateHandlers(
+    () => ({
+      shiftUp: false
+    }),
+    {}
+  ),
+  lifecycle({
+    componentWillUnmount() {
+      this.props.saveChanges();
+    }
+  })
+);
 
-  componentDidMount = () => {
-    this.props.setCellKeyboardShortcuts(this.getKeyboardShortcuts);
-  };
+const DateEditCell = ({value, Formats, handleChange, clearDate, showTime}) => (
+  <React.Fragment>
+    {(f.isEmpty(value)) ? "" : value.format(Formats.formatForUser)}
+    <i className="fa fa-ban"
+       onClick={clearDate}
+    />
 
-  componentWillUnmount = () => {
-    this.props.setCellKeyboardShortcuts({});
-  };
+    <Datetime onChange={handleChange}
+              open
+              input={false}
+              value={value}
+              timeFormat={showTime}
+    />
+  </React.Fragment>
+);
 
-  handleClickOutside = () => {
-    this.props.handleEditFinished(true);
-    ActionCreator.toggleCellEditing(false);
-  };
-
-  handleClickClearDate = event => {
-    this.handleChange(null, () => this.props.handleEditFinished(true));
-    ActionCreator.toggleCellEditing(true);
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  handleChange = (moment, cb) => {
-    this.setState({moment});
-    this.props.handleDateUpdate(moment, cb);
-  };
-
-  getKeyboardShortcuts = () => {
-    const editFinished = this.props.handleEditFinished;
-    return {
-      tab: function () {
-        editFinished(true);
-        ActionCreator.toggleCellEditing(true);
-        ActionCreator.selectNextCell(Directions.RIGHT);
-      },
-      enter: function () {
-        editFinished(true);
-        ActionCreator.toggleCellEditing(false);
-        ActionCreator.selectNextCell(Directions.DOWN);
-      },
-      escape: function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        editFinished(true);
-        ActionCreator.toggleCellEditing(false);
-      },
-      always: function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-    };
-  };
-
-  render = () => {
-    const {moment} = this.state;
-    return (
-      <div className="cell-content">
-        {this.props.toDisplayValue(moment)}
-        <i className="fa fa-ban cell-content"
-          style={{float: "right"}}
-          onClick={this.handleClickClearDate} />
-        <Datetime onChange={this.handleChange}
-          open={true}
-          input={false}
-          value={moment}
-          timeFormat={false} />
-      </div>
-    );
-  }
-}
-
-DateEditCell.propTypes = {
-  setCellKeyboardShortcuts: PropTypes.func.isRequired,
-  handleDateUpdate: PropTypes.func.isRequired,
-  toDisplayValue: PropTypes.func.isRequired,
-  handleEditFinished: PropTypes.func.isRequired,
-  cell: PropTypes.object.isRequired,
-  value: PropTypes.object.isRequired
-};
-
-module.exports = DateEditCell;
+export default enhance(DateEditCell);
