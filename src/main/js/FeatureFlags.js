bbe1c0f6f767afad0c394007a59900aefba23cf3
Hermann Pauly <hp@campudus.com>
2018-02-20 11:33:12 +0100

    Add feature flag to greet user by name
    defaults to false

--- src/main/js/FeatureFlags.js
+++ src/main/js/FeatureFlags.js
@@ -1,2 +1,3 @@
 export const ENABLE_DASHBOARD = false;
+export const SHOW_DASHBOARD_USER_NAME = false;
 export const KEYBOARD_TABLE_HISTORY = false;
--- src/main/js/components/dashboard/greeter/GreeterWidget.jsx
+++ src/main/js/components/dashboard/greeter/GreeterWidget.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import i18n from "i18next";
-import {compose, pure, withProps} from "recompose";
+import {branch, compose, pure, renderNothing, withProps} from "recompose";
 import f from "lodash/fp";
 import Raven from "raven-js";
 import getMotd from "./Messages";
+import {SHOW_DASHBOARD_USER_NAME} from "../../../FeatureFlags";
 
 const GreeterWidget = ({userName, motd}) => (
   <div className="greeter tile wide">
     <div className="heading">
       <span className="default-text">{i18n.t("dashboard:greeter.hello")}</span>
-      <span className="user-name">{userName},</span>
+      <UserName userName={userName}/>
+      ,
     </div>
     <div className="default-text">{motd}</div>
     <div className="info-text">{i18n.t("dashboard:greeter.info")}</div>
   </div>
+);
+
+// User name component dependent on feature flag
+const UserName = branch(
+  () => !SHOW_DASHBOARD_USER_NAME,
+  renderNothing
+)(
+  ({userName}) => <span className="user-name">{userName}</span>
 );
 
 const enhance = compose(
