8be436b45a6698a07fd3c0ebc7718a251cdddd17
Hermann Pauly <hp@campudus.com>
2018-02-22 09:27:45 +0100

    Hide "Add row" button in settings tables

--- src/main/js/components/rows/NewRow.jsx
+++ src/main/js/components/rows/NewRow.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import ActionCreator from "../../actions/ActionCreator";
 import {translate} from "react-i18next";
-import {compose, pure, withHandlers, setPropTypes} from "recompose";
+import {compose, pure, withHandlers, setPropTypes, renderNothing, branch} from "recompose";
+import f from "lodash/fp";
 
 const withFunctionality = compose(
   pure,
@@ -23,14 +24,27 @@ const withFunctionality = compose(
   translate(["table"])
 );
 
+// By extracting the row button, we make sure an empty cell with proper CSS gets rendered in settings tables
+const RowButton = branch(
+  (props) => f.getOr(false, ["table", "type"], props) === "settings",
+  renderNothing
+)(
+  ({t, addNewRow}) => (
+    <a href="#" className="button new-row-inner" onClick={addNewRow}>
+      <i className="fa fa-plus-circle" />
+      <span>{t("add_new_row")}</span>
+    </a>
+  )
+);
+
 const NewRowButton = (props) => {
-  const {t, addNewRow} = props;
+  const {t, addNewRow, table} = props;
   return (
     <div className="new-row">
-      <a href="#" className="button new-row-inner" onClick={addNewRow}>
-        <i className="fa fa-plus-circle" />
-        <span>{t("add_new_row")}</span>
-      </a>
+      <RowButton t={t}
+                 addNewRow={addNewRow}
+                 table={table}
+      />
     </div>
   );
 };
