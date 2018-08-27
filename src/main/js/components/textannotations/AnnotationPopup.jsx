407ac2f053bf2729f4a34f56c3c8f7a1a033ea8c
thimo <schneider.thimo@protonmail.com>
2018-08-27 10:46:58 +0200

    fixed error when adding comments

--- src/main/js/components/textannotations/AnnotationPopup.jsx
+++ src/main/js/components/textannotations/AnnotationPopup.jsx
@@ -2,6 +2,7 @@ import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import listenToClickOutside from "react-onclickoutside";
 import f from "lodash/fp";
+import _ from "lodash";
 import {DefaultLangtag} from "../../constants/TableauxConstants";
 import Empty from "../helperComponents/emptyEntry";
 import AnnotationEntry from "./AnnotationEntry";
@@ -100,8 +101,10 @@ class AnnotationPopup extends PureComponent {
 
   setArrowPosition = (fromBottom = 0) => {
     const arrowSelector = ".annotation-popup.shift-up::before";
+    // The first element doesn't have the key "cssRules", leading to an error.
+    const sheets = _.tail(document.styleSheets);
 
-    const arrowRule = doto(document.styleSheets,
+    const arrowRule = doto(sheets,
       f.map("cssRules"),
       f.map(f.find(f.matchesProperty("selectorText", arrowSelector))),
       f.flatten,
