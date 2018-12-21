d42ff8cd5467b3faba2936d9a6d52ccfd8797470
Hermann Pauly <hp@campudus.com>
2018-12-21 16:43:14 +0100

    Add reducer and routes routes for cell updates

--- src/app/helpers/apiRoutes.js
+++ src/app/helpers/apiRoutes.js
@@ -1,10 +1,28 @@
+import { flow, join, map, toString } from "lodash/fp";
+const urlTrim = url => url.match(/\.*\/?(.*)\/?/)[1];
+const joinUrlParts = (...args) =>
+  flow(
+    map(
+      flow(
+        toString,
+        urlTrim
+      )
+    ),
+
+    join("/")
+  )(args);
+
 const getAllTables = () => "/tables";
 const getAllColumnsForTable = tableId => "/tables/" + tableId + "/columns";
 const getAllRowsForTable = tableId => "/tables/" + tableId + "/rows";
+const toCell = ({ tableId, rowId, columnId }) =>
+  "/" + joinUrlParts("tables", tableId, "columns", columnId, "rows", rowId);
 
 const API_ROUTES = {
   getAllTables: getAllTables,
   getAllColumnsForTable,
-  getAllRowsForTable
-}
+  getAllRowsForTable,
+  toCell
+};
+
 export default API_ROUTES;
--- src/app/redux/reducers/rows.js
+++ src/app/redux/reducers/rows.js
@@ -1,18 +1,24 @@
 import actionTypes from "../actionTypes";
-import {toObjectById} from '../../helpers/funcHelpers';
+import f from "lodash/fp";
+import { doto } from "../../helpers/functools";
 
 const {
   ALL_ROWS_LOADING_DATA,
   ALL_ROWS_DATA_LOADED,
-  ALL_ROWS_DATA_LOAD_ERROR
+  ALL_ROWS_DATA_LOAD_ERROR,
+  CELL_SET_VALUE,
+  CELL_ROLLBACK_VALUE
 } = actionTypes;
 
 const initialState = {};
 
-const rows = (state = initialState, action) => {
+const rows = (state = initialState, action, completeState) => {
   switch (action.type) {
     case ALL_ROWS_LOADING_DATA:
-      return {...state, [action.tableId]: {error: false, finishedLoading: false}};
+      return {
+        ...state,
+        [action.tableId]: { error: false, finishedLoading: false }
+      };
     case ALL_ROWS_DATA_LOADED:
       return {
         ...state,
@@ -23,10 +29,46 @@ const rows = (state = initialState, action) => {
         }
       };
     case ALL_ROWS_DATA_LOAD_ERROR:
-      return {...state, [action.tableId]: {error: action.error, finishedLoading: true}};
+      return {
+        ...state,
+        [action.tableId]: { error: action.error, finishedLoading: true }
+      };
+    case CELL_SET_VALUE: {
+      const { columnIdx, rowIdx } = idsToIndices(action, {
+        state,
+        completeState
+      });
+      const rowSelector = [action.tableId, "data", rowIdx, "values"];
+      return f.update(rowSelector, f.assoc(columnIdx, action.newValue), state);
+    }
+    case CELL_ROLLBACK_VALUE: {
+      const { columnIdx, rowIdx } = idsToIndices(action, {
+        state,
+        completeState
+      });
+      const rowSelector = [action.tableId, "data", rowIdx, "values"];
+      return f.update(rowSelector, f.assoc(columnIdx, action.oldValue), state);
+    }
     default:
       return state;
   }
+};
+
+const idsToIndices = (
+  { columnId, tableId, rowId },
+  { state, completeState } = {}
+) => {
+  const columnIdx = doto(
+    completeState,
+    f.prop(["columns", tableId, "data"]),
+    f.findIndex(f.propEq("id", columnId))
+  );
+  const rowIdx = doto(
+    state,
+    f.prop([tableId, "data"]),
+    f.findIndex(f.propEq("id", rowId))
+  );
+  return { columnIdx, rowIdx };
 };
 
 export default rows;
