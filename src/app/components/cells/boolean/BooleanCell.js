112430d359e2aab17c90153f97763a091519a820
Hermann Pauly <hp@campudus.com>
2019-01-08 15:04:03 +0100

    Make boolean cells toggleable

--- src/app/components/cells/boolean/BooleanCell.js
+++ src/app/components/cells/boolean/BooleanCell.js
@@ -1,40 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import '../../../../scss/main.scss';
+import "../../../../scss/main.scss";
 
-const BooleanCell = (props) => {
-  const {value,column, langtag, selected, setCellKeyboardShortcuts, toggleCheckboxValue, displayValue} = props;
+const BooleanCell = props => {
+  const {
+    actions,
+    value,
+    table,
+    row,
+    column,
+    langtag,
+    setCellKeyboardShortcuts,
+    selected
+  } = props;
 
-  // const handleEditDone = (newValue) => {
-  //   const valueToSave = (cell.isMultiLanguage)
-  //     ? {[langtag]: newValue}
-  //     : newValue;
-  //   ActionCreator.changeCell(cell, valueToSave);
-  // };
-
-  const getCheckboxValue = () => {
-    return !!((column.isMultiLanguage) ? value[langtag] : value);
+  const handleEditDone = newValue => {
+    const valueToSave = column.isMultiLanguage
+      ? { [langtag]: newValue }
+      : newValue;
+    actions.changeCellValue({
+      tableId: table.id,
+      rowId: row.id,
+      columnId: column.id,
+      oldValue: value,
+      newValue: valueToSave,
+      kind: column.kind
+    });
   };
 
-  // const toggleCheckboxValue = () => {
-  //   if (isLocked(cell.row)) {
-  //     askForSessionUnlock(cell.row);
-  //   } else if (selected) {
-  //     handleEditDone(!getCheckboxValue());
-  //   }
-  // };
+  const getCheckboxValue = () => {
+    return !!(column.isMultiLanguage ? value[langtag] : value);
+  };
 
-  // setCellKeyboardShortcuts({
-  //   enter: evt => {
-  //     toggleCheckboxValue();
-  //   }
-  // });
-  
+  const toggleCheckboxValue = () => {
+    // FIXME: Respect locking status
+    // if (isLocked(cell.row)) {
+    //   askForSessionUnlock(cell.row);
+    // } else if (selected) {
+    //   handleEditDone(!getCheckboxValue());
+    // }
+    handleEditDone(!getCheckboxValue());
+  };
+
+  setCellKeyboardShortcuts({
+    enter: evt => {
+      selected && toggleCheckboxValue();
+    }
+  });
+
   return (
-    <div className={"cell-content"} onClick={toggleCheckboxValue} >
-      <input className="checkbox" type="checkbox"
+    <div className={"cell-content"} onClick={toggleCheckboxValue}>
+      <input
+        className="checkbox"
+        type="checkbox"
         checked={getCheckboxValue()}
-        readOnly="readOnly" />
+        readOnly="readOnly"
+      />
     </div>
   );
 };
