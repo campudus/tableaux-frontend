baf4a5d717bbd4b31a63d89b6bda9b28779e6212
Hermann Pauly <hp@campudus.com>
2019-01-28 15:21:58 +0100

    Make numeric view editable

--- src/app/components/entityView/numeric/NumericView.jsx
+++ src/app/components/entityView/numeric/NumericView.jsx
@@ -1,57 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {compose, lifecycle, pure, withHandlers, withStateHandlers} from "recompose";
+import {
+  compose,
+  lifecycle,
+  pure,
+  withHandlers,
+  withStateHandlers
+} from "recompose";
 import f from "lodash/fp";
-import {contentChanged} from "../../cells/Cell";
-// import ActionCreator from "../../../actions/ActionCreator";
 import i18n from "i18next";
 import KeyboardShortcutsHelper from "../../../helpers/KeyboardShortcutsHelper";
-import {maybe} from "../../../helpers/functools";
+import { maybe } from "../../../helpers/functools";
 
 const enhance = compose(
   pure,
   withStateHandlers(
-    ({value}) => {
-      return ({
-        value: maybe(value).map(parseFloat).getOrElse(0)
-      });
+    ({ value, cell, langtag }) => {
+      const oldValue = cell.column.multilanguage ? { [langtag]: value } : value;
+      return {
+        oldValue,
+        value: maybe(value)
+          .map(parseFloat)
+          .getOrElse(0)
+      };
     },
     {
-      registerInput: (state, {funcs}) => (node) => {
+      registerInput: (state, { funcs }) => node => {
         funcs.register(node);
       },
-      handleChange: () => (event) => {
+      handleChange: () => event => {
         const inputString = event.target.value.replace(/,/g, ".");
-        const normalized = (inputString.split(".").length > 2)
-          ? inputString.substr(0, inputString.length - 1)
-          : inputString;
-        return {value: normalized};
+        const value =
+          inputString.split(".").length > 2
+            ? inputString.substr(0, inputString.length - 1)
+            : inputString;
+        return { value };
       },
-      saveChanges: (state, props) => () => {
-        const origVal = props.value;
+      saveChanges: ({ oldValue, value }, props) => () => {
+        const { actions, cell, langtag } = props;
         // value might have been converted to string in the meantime; this is necessary to be able to
         // add a decimal seperator with the editor still behaving naturally
-        const currVal = parseFloat(state.value);
-        if (origVal === currVal) {
-          return;
-        }
+        const _value = parseFloat(value);
+        const newValue = cell.column.multiLanguage
+          ? { [langtag]: _value }
+          : _value;
 
-        const {cell, langtag} = props;
-        // ActionCreator.changeCell(
-        //   cell,
-        //   ((cell.isMultiLanguage) ? {[langtag]: currVal} : currVal),
-        //   contentChanged(cell, langtag, origVal)
-        // );
+        actions.changeCellValue({
+          tableId: cell.table.id,
+          columnId: cell.column.id,
+          rowId: cell.row.id,
+          oldValue,
+          newValue
+        });
       }
     }
   ),
   withHandlers({
-    getKeyboardShortcuts: ({saveChanges}) => () => {
+    getKeyboardShortcuts: ({ saveChanges }) => () => {
       const captureEventAnd = fn => event => {
         event.stopPropagation();
         event.preventDefault();
-        (fn || function () {
-        })(event);
+        (fn || function() {})(event);
       };
 
       return {
@@ -63,7 +72,21 @@ const enhance = compose(
   withHandlers({
     isKeyAllowed: () => event => {
       const numbers = f.map(f.toString, f.range(0, 10));
-      const allowedKeys = [...numbers, ".", ",", "ArrowLeft", "ArrowRight", "Enter", "Return", "Escape", "Backspace", "Delete", "Tab", "ArrowUp", "ArrowDown"];
+      const allowedKeys = [
+        ...numbers,
+        ".",
+        ",",
+        "ArrowLeft",
+        "ArrowRight",
+        "Enter",
+        "Return",
+        "Escape",
+        "Backspace",
+        "Delete",
+        "Tab",
+        "ArrowUp",
+        "ArrowDown"
+      ];
       if (!f.contains(event.key, allowedKeys)) {
         event.preventDefault();
         event.stopPropagation();
@@ -73,7 +96,7 @@ const enhance = compose(
     }
   }),
   withHandlers({
-    handleKeyDown: ({isKeyAllowed, getKeyboardShortcuts}) => (event) => {
+    handleKeyDown: ({ isKeyAllowed, getKeyboardShortcuts }) => event => {
       if (isKeyAllowed(event)) {
         KeyboardShortcutsHelper.onKeyboardShortcut(getKeyboardShortcuts)(event);
       }
@@ -86,18 +109,25 @@ const enhance = compose(
   })
 );
 
-const NumericView = ({value, registerInput, handleChange, thisUserCantEdit, children, handleKeyDown, saveChanges}) => (
-  <div className="item-content shorttext"
-       tabIndex={1}
-  >
-    <input type="text"
-           ref={registerInput}
-           disabled={thisUserCantEdit}
-           value={value}
-           placeholder={i18n.t("table:empty.number")}
-           onChange={handleChange}
-           onKeyDown={handleKeyDown}
-           onBlur={saveChanges}
+const NumericView = ({
+  value,
+  registerInput,
+  handleChange,
+  thisUserCantEdit,
+  children,
+  handleKeyDown,
+  saveChanges
+}) => (
+  <div className="item-content shorttext" tabIndex={1}>
+    <input
+      type="text"
+      ref={registerInput}
+      disabled={thisUserCantEdit}
+      value={value}
+      placeholder={i18n.t("table:empty.number")}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      onBlur={saveChanges}
     />
     {children}
   </div>
