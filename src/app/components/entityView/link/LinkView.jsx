85c8cbf286eabfde33590ec106a72f893a7e3154
Hermann Pauly <hp@campudus.com>
2019-01-30 10:01:55 +0100

    Adapt LinkView to new data format

--- src/app/components/entityView/link/LinkView.jsx
+++ src/app/components/entityView/link/LinkView.jsx
@@ -1,68 +1,75 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {openLinkOverlay} from "../../cells/link/LinkOverlay";
+import { openLinkOverlay } from "../../cells/link/LinkOverlay";
 import LinkList from "../../helperComponents/LinkList";
-// import ActionCreator from "../../../actions/ActionCreator";
 import i18n from "i18next";
 import * as f from "lodash/fp";
+import { reportUpdateReasons } from "../../../helpers/devWrappers";
+import Empty from "../../helperComponents/emptyEntry";
+import { withProps } from "recompose";
 
 class LinkView extends Component {
   static propTypes = {
     langtag: PropTypes.string.isRequired,
     cell: PropTypes.object.isRequired,
-    thisUserCantEdit: PropTypes.bool
+    thisUserCantEdit: PropTypes.bool,
+    actions: PropTypes.object.isRequired
   };
 
+  componentDidUpdate = reportUpdateReasons("link").bind(this);
+
   openOverlay = () => {
-    const {cell, langtag} = this.props;
-    openLinkOverlay(cell, langtag);
+    const { cell, langtag, actions } = this.props;
+    openLinkOverlay({ cell, langtag, actions });
   };
 
   removeLink = id => () => {
     if (this.props.thisUserCantEdit) {
       return;
     }
-    const {cell} = this.props;
+    const { cell, actions, value } = this.props;
     const newValue = f.pullAt(id, cell.value);
-    // ActionCreator.changeCell(cell, newValue);
+    actions.changeCellValue({ cell, oldValue: value, newValue });
   };
 
   mkLinkList = (cell, langtag) => {
-    return cell.value.map(
-      (link, idx) => {
-        return {
-          displayName: f.get([idx, langtag], cell.displayValue || ""),
-          linkTarget: {
-            tables: cell.tables,
-            tableId: cell.column.toTable,
-            rowId: link.id
-          }
-        };
-      }
-    );
+    return cell.value.map((link, idx) => {
+      return {
+        displayName: f.get([idx, langtag], this.props.displayValues) || (
+          <Empty langtag={langtag} />
+        ),
+        linkTarget: {
+          tables: cell.tables,
+          tableId: cell.column.toTable,
+          rowId: link.id
+        }
+      };
+    });
   };
 
   render() {
-    const {cell, langtag} = this.props;
+    const { cell, langtag, value } = this.props;
     const links = this.mkLinkList(cell, langtag);
 
-    return (f.isEmpty(links)
-      ? (
-        <div className="item-description">
-          {i18n.t("table:empty.links")}
-          {this.props.children}
-        </div>
-      )
-      : (
-        <div><LinkList links={links}
-          langtag={langtag}
-          unlink={this.removeLink}
-        />
+    return f.isEmpty(links) ? (
+      <div className="item-description">
+        {i18n.t("table:empty.links")}
         {this.props.children}
-        </div>
-      )
+      </div>
+    ) : (
+      <div>
+        <LinkList links={links} langtag={langtag} unlink={this.removeLink} />
+        {this.props.children}
+      </div>
     );
   }
 }
 
-export default LinkView;
+export default withProps(({ value, grudData, cell }) => {
+  const displayValues = grudData.displayValues[cell.column.toTable];
+  const linkDisplayValues = value.map(({ id }) => {
+    const displayValueObject = f.find(f.propEq("id", id), displayValues);
+    return displayValueObject.values[0];
+  });
+  return { displayValues: linkDisplayValues };
+})(LinkView);
--- src/app/components/helperComponents/LinkList.jsx
+++ src/app/components/helperComponents/LinkList.jsx
@@ -4,18 +4,19 @@
  *        links: [{displayName, linkTarget: url-string},...]
  */
 
-import React, {Component} from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import i18n from "i18next";
 import * as f from "lodash/fp";
-import {loadAndOpenEntityView} from "../overlay/EntityViewOverlay";
-import {List} from "react-virtualized";
+import { loadAndOpenEntityView } from "../overlay/EntityViewOverlay";
+import { List } from "react-virtualized";
 import SvgIcon from "../helperComponents/SvgIcon";
+import { reportUpdateReasons } from "../../helpers/devWrappers";
 
 const MAX_DISPLAYED_LINKS = 4;
 
-class LinkList extends Component {
+class LinkList extends PureComponent {
   static propTypes = {
     langtag: PropTypes.string.isRequired,
     links: PropTypes.array.isRequired,
@@ -30,7 +31,9 @@ class LinkList extends Component {
     };
   }
 
-  toggleExpand = () => this.setState({expanded: !this.state.expanded});
+  componentDidUpdate = reportUpdateReasons("LinkList").bind(this);
+
+  toggleExpand = () => this.setState({ expanded: !this.state.expanded });
 
   proceedTo = linkTarget => evt => {
     if (f.isEmpty(linkTarget)) {
@@ -44,38 +47,44 @@ class LinkList extends Component {
     }
   };
 
-  renderLink = ({index, key = index, style}) => {
-    const {displayName, linkTarget} = this.props.links[index];
+  renderLink = ({ index, key = index, style }) => {
+    const { displayName, linkTarget } = this.props.links[index];
     return (
       <div className="link-label-wrapper-2" key={key} style={style}>
-        <div className="link-label-wrapper" onClick={this.proceedTo(linkTarget)}>
+        <div
+          className="link-label-wrapper"
+          onClick={this.proceedTo(linkTarget)}
+        >
           <a className="link-label" href="#">
             {displayName}
-            {(f.isEmpty(linkTarget)) ? null : <i className="fa fa-long-arrow-right" />}
+            {f.isEmpty(linkTarget) ? null : (
+              <i className="fa fa-long-arrow-right" />
+            )}
           </a>
         </div>
       </div>
     );
   };
 
-  renderInteractiveLink = ({index, key = index, style}) => {
-    const {links, unlink} = this.props;
-    const {displayName, linkTarget} = links[index];
+  renderInteractiveLink = ({ index, key = index, style }) => {
+    const { links, unlink } = this.props;
+    const { displayName, linkTarget } = links[index];
     const hovered = this.state.hovered === index;
     const cssClass = classNames("link-label-wrapper has-buttons", {
       "show-buttons": hovered
     });
     const setHoverState = () => {
-      this.setState({hovered: index});
+      this.setState({ hovered: index });
     };
 
     return (
       <div className="link-label-wrapper-2" style={style} key={key}>
-        <div className={cssClass}
+        <div
+          className={cssClass}
           onMouseEnter={setHoverState}
           onMouseLeave={() => {
             if (hovered) {
-              this.setState({hovered: null});
+              this.setState({ hovered: null });
             }
           }}
         >
@@ -83,74 +92,68 @@ class LinkList extends Component {
             <a href="#">
               <div className="text-wrapper">{displayName}</div>
             </a>
-            {(hovered) ? <i className="fa fa-long-arrow-right" /> : null}
+            {hovered ? <i className="fa fa-long-arrow-right" /> : null}
           </div>
-          {(hovered)
-            ? (<div className="unlink-button" onClick={unlink(index)}>
+          {hovered ? (
+            <div className="unlink-button" onClick={unlink(index)}>
               <a href="#">
                 <SvgIcon icon="cross" containerClasses="color-primary" />
               </a>
             </div>
-            )
-            : null }
+          ) : null}
         </div>
       </div>
     );
   };
 
   renderPreview = () => {
-    const {links, unlink} = this.props;
+    const { links, unlink } = this.props;
     const nLinks = links.length;
     const canExpand = nLinks > MAX_DISPLAYED_LINKS;
-    const renderFn = (unlink) ? this.renderInteractiveLink : this.renderLink;
-    const cssClass = classNames("item-content", {"can-expand": canExpand});
+    const renderFn = unlink ? this.renderInteractiveLink : this.renderLink;
+    const cssClass = classNames("item-content", { "can-expand": canExpand });
     return (
       <div className={cssClass}>
-        {f.range(0, f.min([nLinks, MAX_DISPLAYED_LINKS]))
-          .map(index => renderFn({index}))
-        }
+        {f
+          .range(0, f.min([nLinks, MAX_DISPLAYED_LINKS]))
+          .map(index => renderFn({ index }))}
       </div>
     );
   };
 
   renderAll = () => {
-    const {links, unlink} = this.props;
+    const { links, unlink } = this.props;
     const nLinks = links.length;
 
     return (
-      <List width={window.innerWidth * 0.6 - 100}
+      <List
+        width={window.innerWidth * 0.6 - 100}
         height={430}
         rowCount={nLinks}
         rowHeight={42}
-        rowRenderer={(unlink) ? this.renderInteractiveLink : this.renderLink}
+        rowRenderer={unlink ? this.renderInteractiveLink : this.renderLink}
         hovered={this.state.hovered}
       />
     );
   };
 
   render() {
-    const {links} = this.props;
+    const { links } = this.props;
     const nLinks = links.length;
-    const {expanded} = this.state;
+    const { expanded } = this.state;
     const canExpand = nLinks > MAX_DISPLAYED_LINKS;
 
     return (
       <div className="link-list">
-        {(expanded)
-          ? this.renderAll()
-          : this.renderPreview()
-        }
-        {(canExpand)
-          ? (<a className="expand-button" href="#" onClick={this.toggleExpand}>
-            <i className={(expanded) ? "fa fa-angle-up" : "fa fa-angle-down"} />
-            {(expanded)
+        {expanded ? this.renderAll() : this.renderPreview()}
+        {canExpand ? (
+          <a className="expand-button" href="#" onClick={this.toggleExpand}>
+            <i className={expanded ? "fa fa-angle-up" : "fa fa-angle-down"} />
+            {expanded
               ? i18n.t("table:show_less")
-              : i18n.t("table:show_all_items", {nItems: nLinks})
-            }
+              : i18n.t("table:show_all_items", { nItems: nLinks })}
           </a>
-          )
-          : null
-        }
+        ) : null}
       </div>
     );
   }
