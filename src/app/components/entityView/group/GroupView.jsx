ab15583edf6998753646e15b49a2bf3a2664dc3f
Hermann Pauly <hp@campudus.com>
2019-01-30 15:04:30 +0100

    Adapt GroupView to use redux flow

--- src/app/components/entityView/group/GroupView.jsx
+++ src/app/components/entityView/group/GroupView.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {openEntityView} from "../../overlay/EntityViewOverlay";
+import { openEntityView } from "../../overlay/EntityViewOverlay";
 import Empty from "../../helperComponents/emptyEntry";
-import {isEmpty, trim} from "lodash/fp";
+import { isEmpty, trim } from "lodash/fp";
 
-const GroupView = (props) => {
-  const {langtag, cell, thisUserCantEdit, funcs} = props;
+const GroupView = props => {
+  const { langtag, cell, thisUserCantEdit, funcs } = props;
 
   const clickHandler = evt => {
     if (thisUserCantEdit) {
       return;
     }
     evt.stopPropagation();
-    openEntityView(cell.row, langtag, null, [], cell.column);
+    openEntityView({
+      langtag,
+      table: cell.table,
+      row: cell.row,
+      filterColumn: cell.column
+    });
   };
 
   const value = trim(cell.displayValue[langtag]);
 
   return (
-    <div className="item-content group"
+    <div
+      className="item-content group"
       tabIndex="1"
-      ref={el => { funcs.register(el); }}
+      ref={el => {
+        funcs.register(el);
+      }}
       onClick={clickHandler}
     >
-      {(isEmpty(value)
-        ? <Empty/>
-        : value)}
+      {isEmpty(value) ? <Empty /> : value}
       {props.children}
     </div>
   );
