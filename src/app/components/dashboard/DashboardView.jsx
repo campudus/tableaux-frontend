754e159631db8563c1c9bd77b65fdb53a5a7dc07
Richard Wohlgemuth <woffrek@outlook.com>
2019-02-05 13:38:16 +0100

    enable dashboard and make it render again

--- src/app/components/dashboard/DashboardView.jsx
+++ src/app/components/dashboard/DashboardView.jsx
@@ -1,82 +1,102 @@
+import { compose, pure, withHandlers, withProps } from "recompose";
 import React from "react";
+import i18n from "i18next";
+
 import PropTypes from "prop-types";
-import {compose, pure, withHandlers} from "recompose";
+
+import FetchStatusData from "./RequestStatusData";
+import FlagWidget from "./flagwidget/FlagWidget";
+import GreeterWidget from "./greeter/GreeterWidget";
+import LanguageSwitcher from "../header/LanguageSwitcher";
 import Navigation from "../header/Navigation";
 import PageTitle from "../header/PageTitle";
-import LanguageSwitcher from "../header/LanguageSwitcher";
-import GreeterWidget from "./greeter/GreeterWidget";
-import TranslationStatusWidget from "./translationstatus/TranslationStatusWidget";
 import SupportWidget from "./support/SupportWidget";
-import FlagWidget from "./flagwidget/FlagWidget";
-import i18n from "i18next";
-import App from "ampersand-app";
-import FetchStatusData from "./RequestStatusData";
+import TableauxConstants from "../../constants/TableauxConstants";
+import TableauxRouter from "../../router/router";
+import TranslationStatusWidget from "./translationstatus/TranslationStatusWidget";
+import needsApiData from "../helperComponents/needsAPIData";
 
-const WidgetColletion = ({langtag, requestedData}) => (
+const WidgetColletion = ({ langtag, requestedData }) => (
   <div className="widgets-wrapper">
     <div className="widgets">
       <GreeterWidget langtag={langtag} />
-      <FlagWidget langtag={langtag}
-                  flag="comments"
-                  requestedData={requestedData}
+      <FlagWidget
+        langtag={langtag}
+        flag="comments"
+        requestedData={requestedData}
       />
-      <FlagWidget langtag={langtag}
-                  flag="important"
-                  requestedData={requestedData}
+      <FlagWidget
+        langtag={langtag}
+        flag="important"
+        requestedData={requestedData}
       />
-      <FlagWidget langtag={langtag}
-                  flag="needs-translation"
-                  requestedData={requestedData}
+      <FlagWidget
+        langtag={langtag}
+        flag="needs-translation"
+        requestedData={requestedData}
       />
-      <FlagWidget langtag={langtag}
-                  flag="check-me"
-                  requestedData={requestedData}
+      <FlagWidget
+        langtag={langtag}
+        flag="check-me"
+        requestedData={requestedData}
       />
-      <FlagWidget langtag={langtag}
-                  flag="postpone"
-                  requestedData={requestedData}
+      <FlagWidget
+        langtag={langtag}
+        flag="postpone"
+        requestedData={requestedData}
       />
     </div>
-    <TranslationStatusWidget langtag={langtag}
-                             requestedData={requestedData}
-    />
+    <TranslationStatusWidget langtag={langtag} requestedData={requestedData} />
   </div>
-
 );
 
-const DashboardView = ({langtag, handleLanguageSwitch}) => (
-  <React.Fragment>
-    <header>
-      <Navigation langtag={langtag} />
-      <div className="header-separator" />
-      <PageTitle titleKey="Dashboard" />
-      <LanguageSwitcher langtag={langtag} onChange={handleLanguageSwitch} />
-    </header>
-    <div id="dashboard-view" className={"wrapper"}>
+const DashboardView = props => {
+  const { langtag, handleLanguageSwitch, requestedData } = props;
 
-      <FetchStatusData>
-        <WidgetColletion langtag={langtag} />
-      </FetchStatusData>
+  if (requestedData) {
+    TableauxConstants.initLangtags(requestedData.value);
+  }
 
-      <SupportWidget langtag={langtag} />
-      <footer>
-        <div className="footer-text">{i18n.t("dashboard:footer-text")}</div>
-      </footer>
-    </div>
-  </React.Fragment>
-);
+  return requestedData ? (
+    <React.Fragment>
+      <header>
+        <Navigation langtag={langtag} />
+        <div className="header-separator" />
+        <PageTitle titleKey="Dashboard" />
+        <LanguageSwitcher langtag={langtag} onChange={handleLanguageSwitch} />
+      </header>
+      <div id="dashboard-view" className={"wrapper"}>
+        <FetchStatusData>
+          <WidgetColletion langtag={langtag} />
+        </FetchStatusData>
+
+        <SupportWidget langtag={langtag} />
+        <footer>
+          <div className="footer-text">{i18n.t("dashboard:footer-text")}</div>
+        </footer>
+      </div>
+    </React.Fragment>
+  ) : null;
+};
 
 DashboardView.propTypes = {
-  langtag: PropTypes.string.isRequired
+  langtag: PropTypes.string.isRequired,
+  requestedData: PropTypes.object
 };
 
 const enhance = compose(
   pure,
   withHandlers({
-    handleLanguageSwitch: () => (langtag) => {
-      App.router.history.navigate(`/${langtag}/dashboard`, {trigger: true});
+    handleLanguageSwitch: () => newLangtag => {
+      const history = TableauxRouter.history;
+      i18n.changeLanguage(newLangtag);
+      history.navigate(`/${newLangtag}/dashboard`);
     }
-  })
+  }),
+  withProps(() => {
+    return { requestUrl: "/api/system/settings/langtags" };
+  }),
+  needsApiData
 );
 
 export default enhance(DashboardView);
