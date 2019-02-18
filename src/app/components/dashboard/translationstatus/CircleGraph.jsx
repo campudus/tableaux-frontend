18465d6e6b456b7be28369ad83a70c9e40514435
Hermann Pauly <hp@campudus.com>
2019-02-18 15:30:45 +0100

    Use gradient instead of breakpoints for translation percentage

--- src/app/components/dashboard/translationstatus/CircleGraph.jsx
+++ src/app/components/dashboard/translationstatus/CircleGraph.jsx
@@ -1,66 +1,105 @@
-import React from "react";
+import { pure } from "recompose";
+import React, { useRef, useState } from "react";
+
 import PropTypes from "prop-types";
 import f from "lodash/fp";
-import {pure} from "recompose";
 
-const getColorForPercentage = f.cond([
-  [f.lt(90), f.always("#56d1a6")],
-  [f.lt(80), f.always("#f8e71c")],
-  [f.lt(50), f.always("#f5a623")],
-  [f.stubTrue, f.always("#f35a5a")]
-]);
+const getColorForPercentage = (perc, gradient) => {
+  if (!gradient) return null;
+  const percCoord = (perc / 100.0) * gradient.height;
+  const x = 0;
+  const y = Math.min(percCoord, gradient.height - 1);
+  const [r, g, b] = gradient.getContext("2d").getImageData(x, y, 1, 1).data;
+  return `rgb(${r},${g},${b})`;
+};
 
-const toCartesian = (radians, radius) => (
-  {
-    x: radius + radius * Math.cos(radians),
-    y: radius + radius * Math.sin(radians)
-  }
-);
+const toCartesian = (radians, radius) => ({
+  x: radius + radius * Math.cos(radians),
+  y: radius + radius * Math.sin(radians)
+});
 
 const mkArcSvg = (p, radius) => {
   const angle = 3.6 * p;
-  const rAngle = (angle - 90) * Math.PI / 180.0;
+  const rAngle = ((angle - 90) * Math.PI) / 180.0;
 
   const start = toCartesian(rAngle, radius);
   const end = toCartesian(-Math.PI / 2, radius);
 
-  const arcSweep = (angle > 180) ? 1 : 0;
+  const arcSweep = angle > 180 ? 1 : 0;
 
   return [
-    "M", start.x, start.y,
-    "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+    "M",
+    start.x,
+    start.y,
+    "A",
+    radius,
+    radius,
+    0,
+    arcSweep,
+    0,
+    end.x,
+    end.y
   ].join(" ");
 };
 
-const CircleGraph = ({percent, radius = 50}) => {
+const CircleGraph = ({ percent, radius = 50 }) => {
+  const gradientImg = useRef();
+  const gradientCnv = useRef();
+  const [perc, setPercAndRender] = useState(0);
+
+  const initGradient = () => {
+    const cnv = gradientCnv.current;
+    const img = gradientImg.current;
+    cnv.height = img.height;
+    !f.isNil(cnv) &&
+      cnv.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
+    setPercAndRender(percent);
+  };
+
   const percentage = f.clamp(0, 100, percent);
+
   return (
-    <div className="circle-chart">
-      <svg style={
-        {
-          width: 2 * radius,
-          height: 2 * radius
-        }
-      }
-      >
-        <circle cx={radius}
-                cy={radius}
-                r={radius}
-                fill="transparent"
-                strokeWidth={1}
+    <React.Fragment>
+      <div className="gradient" style={{ display: "none" }}>
+        <img
+          ref={gradientImg}
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAABkCAYAAABHLFpgAAAACXBIWXMAAAsSAAALEgHS3X78AAAAhElEQVQYlY2PwQ7CMAxDHXdo/Bl/wf9fOO9EMyUh7RqBxMXyqx1rw+vxVEIAAop0AEUGusOKMitTMo2A5aIE6wqXKZnIX7f87y2maqXnW7m4YFYuaTO5pWg4+0F320Bz6rglfmRgA6i+Zy6W3cVnAHyjgUe/g4fuJu4GRrCmvfS+lHXHCaPMaZUc/VrSAAAAAElFTkSuQmCC"
+          onLoad={initGradient}
         />
-        <path stroke={getColorForPercentage(percentage)}
-              strokeWidth={1}
-              d={mkArcSvg(percentage, radius)}
-              fill="transparent"
-        />
-      </svg>
-      <div style={{color: getColorForPercentage(percentage)}}
-           className="label">
-        <span>{((percentage * 10) | 0) / 10}</span>
-        <span className="percent-sign">%</span>
+        <canvas ref={gradientCnv} />
       </div>
-    </div>
+      <div className="circle-chart">
+        <svg
+          style={{
+            width: 2 * radius,
+            height: 2 * radius
+          }}
+        >
+          <circle
+            cx={radius}
+            cy={radius}
+            r={radius}
+            fill="transparent"
+            strokeWidth={1}
+          />
+          <path
+            stroke={getColorForPercentage(perc, gradientCnv.current)}
+            strokeWidth={1}
+            d={mkArcSvg(Math.min(perc, 99.99), radius)}
+            fill="transparent"
+          />
+        </svg>
+        <div
+          style={{
+            color: getColorForPercentage(percentage, gradientCnv.current)
+          }}
+          className="label"
+        >
+          <span>{((percentage * 10) | 0) / 10}</span>
+          <span className="percent-sign">%</span>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
