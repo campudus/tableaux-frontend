a003969713bdc912d5c17d0bc98f2dc4a6b3da36
Hermann Pauly <hp@campudus.com>
2019-02-28 11:02:24 +0100

    Fix redux store init

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 
+import { maybe } from "../helpers/functools";
 import rootReducer from "./reducers/rootReducer";
 import thunkMiddleware from "./thunkMiddleware";
 
+const composeEnhancers = maybe(
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+).getOrElse(compose);
+
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunkMiddleware()),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunkMiddleware()))
 );
 export default store;
