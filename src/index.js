7909c7cf7c5a1efbe28340c94094143076d47be4
Hermann Pauly <hp@campudus.com>
2019-03-06 17:33:06 +0100

    Integrate Sentry with new backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "../node_modules/react-select/dist/react-select.css";
 import "./app/router/router";
+import Sentry from "@sentry/browser";
+import Cookies from "js-cookie";
+import { makeRequest } from "./app/helpers/apiHelper";
+import route from "./app/helpers/apiRoutes";
+
+async function initSentry(noDryRun = true) {
+  const username = Cookies.get("userName") || "Unknown User";
+  const dsn = (await makeRequest({ apiRoute: route.toSetting("sentryUrl") }))
+    .value;
+  if (noDryRun) {
+    Sentry.init({
+      dsn,
+      release: process.env.BUILD_ID
+    });
+    Sentry.configureScope(scope => scope.setUser({ username }));
+    Sentry.captureMessage("Sentry initialised");
+  } else {
+    console.log("Project Sentry url:", dsn);
+  }
+}
 
 console.log("Campudus GRUD frontend", process.env.BUILD_ID);
+initSentry(process.env.NODE_ENV === "production");
