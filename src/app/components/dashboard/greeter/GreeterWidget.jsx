7b2e39bd79fc4cf6e2cbb7cb680a85421db45af9
Hermann Pauly <hp@campudus.com>
2019-03-07 10:22:04 +0100

    Use userName helper in greeter widget

--- src/app/components/dashboard/greeter/GreeterWidget.jsx
+++ src/app/components/dashboard/greeter/GreeterWidget.jsx
@@ -3,20 +3,24 @@ import PropTypes from "prop-types";
 import i18n from "i18next";
 import { branch, compose, pure, renderNothing, withProps } from "recompose";
 import f from "lodash/fp";
-import Sentry from "@sentry/browser";
 import getMotd from "./Messages";
 import { SHOW_DASHBOARD_USER_NAME } from "../../../FeatureFlags";
+import { getUserName } from "../../../helpers/userNameHelper";
 
-const GreeterWidget = ({ userName, motd }) => (
-  <div className="greeter tile wide">
-    <div className="heading">
-      <span className="default-text">{i18n.t("dashboard:greeter.hello")}</span>
-      <UserName userName={userName} />,
+const GreeterWidget = ({ userName, motd }) => {
+  return (
+    <div className="greeter tile wide">
+      <div className="heading">
+        <span className="default-text">
+          {i18n.t("dashboard:greeter.hello")}
+        </span>
+        <UserName userName={userName} />,
+      </div>
+      <div className="default-text">{motd}</div>
+      <div className="info-text">{i18n.t("dashboard:greeter.info")}</div>
     </div>
-    <div className="default-text">{motd}</div>
-    <div className="info-text">{i18n.t("dashboard:greeter.info")}</div>
-  </div>
-);
+  );
+};
 
 // User name component dependent on feature flag
 const UserName = branch(() => !SHOW_DASHBOARD_USER_NAME, renderNothing)(
@@ -26,7 +30,7 @@ const UserName = branch(() => !SHOW_DASHBOARD_USER_NAME, renderNothing)(
 const enhance = compose(
   pure,
   withProps(props => ({
-    userName: f.getOr("GRUDling", ["user", "id"], Sentry.getContext()),
+    userName: f.getOr("GRUDling", ["user", "id"], getUserName()),
     motd: getMotd(props.langtag)
   }))
 );
