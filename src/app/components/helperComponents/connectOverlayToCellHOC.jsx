e47ea90fb09f79a983432f00691d0a1fabd7848d
Richard Wohlgemuth <woffrek@outlook.com>
2019-03-14 11:45:06 +0100

    connectOverlayToCellHOC now returns cell and row

--- src/app/components/helperComponents/connectOverlayToCellHOC.jsx
+++ src/app/components/helperComponents/connectOverlayToCellHOC.jsx
@@ -5,11 +5,17 @@ export const connectOverlayToCellValue = withProps(props => {
   const { cell, grudData } = props;
   try {
     const { rows } = grudData;
+    const { column, row, table } = cell;
     const [rowIdx, columnIdx] = idsToIndices(
-      { columnId: cell.column.id, rowId: cell.row.id, tableId: cell.table.id },
+      { columnId: column.id, rowId: row.id, tableId: table.id },
       grudData
     );
-    return { value: rows[cell.table.id].data[rowIdx].values[columnIdx] };
+    const linkedRow = rows[table.id];
+
+    return {
+      value: linkedRow.data[rowIdx].values[columnIdx],
+      row: linkedRow.data[0]
+    };
   } catch (err) {
     console.error("Error connecting element:", err);
     return {};
