b9659755367eb54d4f3f6242f5328528ac2d0f13
Richard Wohlgemuth <woffrek@outlook.com>
2019-03-20 10:32:36 +0100

    fix displayValue-problems in clipboard-menu

--- src/app/components/header/PasteCellPreview.jsx
+++ src/app/components/header/PasteCellPreview.jsx
@@ -15,6 +15,7 @@ import i18n from "i18next";
 import { openInNewTab } from "../../helpers/apiUrl";
 import store from "../../redux/store";
 import actions from "../../redux/actionCreators";
+import getDisplayValue from "../../helpers/getDisplayValue";
 
 const ClearCellButton = props => (
   <div className="clear-pasted-button button neutral">
@@ -68,6 +69,7 @@ const CellPreview = props => {
   const langtag = props.pasteOriginCellLang;
 
   const CellType = cellRenderers[cell.kind];
+  const cellDisplayValue = getDisplayValue(cell.column, cell.value);
 
   return (
     <div className="cell-preview">
@@ -77,7 +79,7 @@ const CellPreview = props => {
           langtag={langtag}
           selected={false}
           editing={false}
-          value={cell.displayValue[langtag] || ""}
+          value={cellDisplayValue || ""}
           contentChanged={f.noop}
           setCellKeyboardShortcuts={f.noop}
           displayValue={cell.displayValue}
