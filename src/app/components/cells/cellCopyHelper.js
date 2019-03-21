81180066137b880a573d120a0a7bda5bcff26232
Richard Wohlgemuth <woffrek@outlook.com>
2019-03-21 14:07:54 +0100

    add missing translation for language-access-error-toast

--- src/app/components/cells/cellCopyHelper.js
+++ src/app/components/cells/cellCopyHelper.js
@@ -249,7 +249,7 @@ const pasteCellValue = function(
   }
 
   if (!dst.column.multilanguage && !hasUserAccessToLanguage(dstLang)) {
-    showErrorToast("table:translation.cant_access_language");
+    showErrorToast("common:access_management.cant_access_language");
     return;
   }
 
