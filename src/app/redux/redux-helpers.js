f1ca9ae5f1186500e904b41b73b33d38fb97d7fb
Hermann Pauly <hp@campudus.com>
2019-04-03 13:40:48 +0200

    Add helper to match group member columns with their group

--- src/app/redux/redux-helpers.js
+++ src/app/redux/redux-helpers.js
@@ -1,14 +1,17 @@
 import f from "lodash/fp";
 
 import { ColumnKinds } from "../constants/TableauxConstants";
-import { merge } from "../helpers/functools";
+import { doto, memoizeWith, merge, when } from "../helpers/functools";
 import getDisplayValue from "../helpers/getDisplayValue";
 
 /**
  * @params { tableId, columnId, rowId }
  * @returns [rowIndex, columnIndex, displayValueColumnIndex]
  */
-export const idsToIndices = ({ tableId, columnId, rowId }, completeState) => {
+export const idsToIndices = (
+  { tableId, columnId, rowId } = {},
+  completeState
+) => {
   try {
     const rowIdx = f.findIndex(
       row => row.id === rowId,
@@ -37,8 +40,73 @@ export const idsToIndices = ({ tableId, columnId, rowId }, completeState) => {
   }
 };
 
+export const tableColumnKey = arg => {
+  const [tableId, columnId] = f.props(["tableId", ["column", "id"]], arg);
+  return f.every(f.isNil, [tableId, columnId])
+    ? null
+    : `${tableId}-${columnId}`;
+};
+
+export const isGroupMember = memoizeWith(
+  tableColumnKey,
+  ({ tableId, column, completeState }) => {
+    const columns = completeState.columns[tableId].data;
+    const groupMemberIds = doto(
+      columns,
+      f.map(
+        f.flow(
+          f.prop("groups"),
+          f.map("id")
+        )
+      ),
+      f.reject(f.isEmpty),
+      f.flatten
+    );
+    return f.contains(column.id, groupMemberIds);
+  }
+);
+
+export const getLookupMap = memoizeWith(
+  f.prop("tableId"),
+  ({ tableId, completeState }) => {
+    const columns = completeState.columns[tableId].data;
+
+    // column[] -> { [groupColumnId]: groupMemberId[] }
+    const groups = doto(
+      columns,
+      f.groupBy("id"),
+      f.mapValues(
+        f.flow(
+          f.first,
+          f.prop("groups"),
+          f.map("id")
+        )
+      ),
+      f.pickBy(f.complement(f.isEmpty))
+    );
+
+    // { [groupColumnId]: groupMemberId[] } -> { [groupMemberId]: groupColumnId }
+    return doto(
+      groups,
+      f.toPairs,
+      f.reduce((theMap, [groupColumnId, groupMemberIds]) => {
+        groupMemberIds.forEach(memberId => (theMap[memberId] = groupColumnId));
+        return theMap;
+      }, {})
+    );
+  }
+);
+
+export const getGroupColumn = (data, completeState) =>
+  when(
+    f.isString,
+    f.parseInt(10),
+    f.propOr(null, data.column.id, getLookupMap({ ...data, completeState }))
+  );
+
 export const calcConcatValues = (action, completeState) => {
   const { tableId, columnId, column } = action;
+  // eslint-disable-next-line no-unused-vars
   const [rowIdx, columnIdx, dvRowIdx] = idsToIndices(action, completeState);
   const columns = completeState.columns[tableId].data;
   const rows = completeState.rows[tableId].data;
