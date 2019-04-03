360811f77dd1cde9e0843a86da9346630d2861ea
Hermann Pauly <hp@campudus.com>
2019-04-03 16:48:34 +0200

    Change edit mode of currency cells
    Languages don't automatically match countries, so we keep language
    cells' edit mode disabled while their row is expanded.

--- src/app/redux/reducers/tableView.js
+++ src/app/redux/reducers/tableView.js
@@ -170,18 +170,29 @@ const toggleExpandedRow = (state, action) => {
 };
 
 const toggleCellEditing = (state, action, completeState) => {
-  const { currentTable, selectedCell: { rowId } = {} } = state;
+  const { currentTable, selectedCell: { rowId, columnId } = {} } = state;
   const tableId = parseInt(currentTable);
   const row = f.find(f.propEq("id", rowId), completeState.rows[tableId].data);
   if (action.editing !== false && row && isLocked(row)) {
     askForSessionUnlock(row);
     return state;
   } else {
-    return f.update(
-      "editing",
-      wasEditing => action.editing !== false && !wasEditing,
-      state
+    const column = f.find(
+      f.propEq("id", columnId),
+      completeState.columns[tableId].data
     );
+    // languages don't automatically match countries, so country cells should not switch to edit mode when expanded
+    const shouldStayClosed =
+      column.multilanguage &&
+      column.languageType === "country" &&
+      f.contains(rowId, state.expandedRowIds);
+    return shouldStayClosed
+      ? state
+      : f.update(
+          "editing",
+          wasEditing => action.editing !== false && !wasEditing,
+          state
+        );
   }
 };
 
