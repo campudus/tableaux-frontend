8a69687ad0418236bbafc8705ab9bb4751a27474
Hermann Pauly <hp@campudus.com>
2019-04-03 14:00:03 +0200

    Add OS detection to json helper script

--- js2json.sh
+++ js2json.sh
@@ -1,10 +1,16 @@
-if [ $# -lt 2 ]; then
-    echo "Usage: js2json.sh <inputFile> <outputFile>"
+if [ $# -lt 1 ]; then
+    echo "Usage: js2json.sh <inputFile>"
     exit 1;
 fi
 
+if [ "$(uname -s)" = "Darwin" ]
+then
+    REGEX_PARAM="-E"
+else
+    REGEX_PARAM="-r"
+fi
+
 sed -r 's/(\w+): /"\1": /g' "$1" | \
-    sed -r "s/'(.*?)'/\"\1\"/g" | \
-    sed -r "s/\\\\'/'/g" | \
-    sed -r "s/undefined/null/g" \
-        > "$2"
+    sed "$REGEX_PARAM" "s/'(.*?)'/\"\1\"/g" | \
+    sed "$REGEX_PARAM" "s/\\\\'/'/g" | \
+    sed "$REGEX_PARAM" "s/undefined/null/g"
