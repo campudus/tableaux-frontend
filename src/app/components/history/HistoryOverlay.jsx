11c1b4ad3be4dee92f5e92632e185faa34ef55af
Hermann Pauly <hp@campudus.com>
2019-04-17 17:01:27 +0200

    Use simple header in history overlay

--- src/app/components/history/HistoryOverlay.jsx
+++ src/app/components/history/HistoryOverlay.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import i18n from "i18next";
 
-import Header from "../overlay/Header";
+import { SimpleHeader } from "../overlay/Header";
 import HistoryBody from "./HistoryBody";
 import action from "../../redux/actionCreators";
 import store from "../../redux/store";
@@ -10,7 +10,12 @@ export const openHistoryOverlay = ({ cell, langtag }) => {
   console.log({ cell, langtag });
   store.dispatch(
     action.openOverlay({
-      head: <Header langtag={langtag} />,
+      head: (
+        <SimpleHeader
+          langtag={langtag}
+          title={i18n.t("history:history-view")}
+        />
+      ),
       body: <HistoryBody cell={cell} langtag={langtag} />,
       cell,
       context: i18n.t("history:header-context"),
