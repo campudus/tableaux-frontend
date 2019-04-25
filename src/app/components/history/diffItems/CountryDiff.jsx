c59f1b517baf5006ff64ff48276d626614ecb370
Hermann Pauly <hp@campudus.com>
2019-04-25 15:24:44 +0200

    Add currency codes to currency history

--- src/app/components/history/diffItems/CountryDiff.jsx
+++ src/app/components/history/diffItems/CountryDiff.jsx
@@ -4,10 +4,13 @@ import f from "lodash/fp";
 import classNames from "classnames";
 
 import { doto } from "../../../helpers/functools";
-import { getLanguageOrCountryIcon } from "../../../helpers/multiLanguage";
+import {
+  getCurrencyCode,
+  getLanguageOrCountryIcon
+} from "../../../helpers/multiLanguage";
 
 const CountryDiff = props => {
-  const { diff } = props;
+  const { diff, noCurrency } = props;
   const countries = f.groupBy("country", diff);
   console.log("countries:", countries);
   return doto(
@@ -15,19 +18,28 @@ const CountryDiff = props => {
     f.keys,
     f.map(country => (
       <div key={country} className="counry-diff-group">
-        {getLanguageOrCountryIcon(country)}
-        {countries[country].map(({ add, del, value }, idx) => {
-          console.log("For", country, { add, del, value, idx });
-          const cssClass = classNames("content-diff", {
-            "content-diff--added": add,
-            "content-diff--deleted": del
-          });
-          return (
-            <div key={idx} className={cssClass}>
-              {value}
-            </div>
-          );
-        }, countries)}
+        <div className="country-diff__sub-header">
+          {getLanguageOrCountryIcon(country)}
+          {!noCurrency && (
+            <div className="country-diff-sub-header__currency">{`[${getCurrencyCode(
+              country
+            )}]`}</div>
+          )}
+        </div>
+        <div className="country-diff__group">
+          {countries[country].map(({ add, del, value }, idx) => {
+            console.log("For", country, { add, del, value, idx });
+            const cssClass = classNames("content-diff", {
+              "content-diff--added": add,
+              "content-diff--deleted": del
+            });
+            return (
+              <div key={idx} className={cssClass}>
+                {value}
+              </div>
+            );
+          }, countries)}
+        </div>
       </div>
     ))
   );
