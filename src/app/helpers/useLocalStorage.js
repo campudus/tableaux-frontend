43486e4f21ca73f5ea794be20018de391536e7f3
Hermann Pauly <hp@campudus.com>
2019-05-15 15:27:13 +0200

    Add useLocalStorage hook
    Add a `useLocalStorage` hook that can be used like `useState`, but
    also persists changes and dispatches them to all `useLocalStorage`
    instances that access the same key.
    
    Instead of naively using `window.localStorage` we use Mozilla's
    `localforage` library. It automatically selects the best performing
    API available, provides stable fallback for all browsers and accesses
    the storage asynchronously; thus taking performance pressure from
    components.

---
+++ src/app/helpers/useLocalStorage.js
@@ -0,0 +1,74 @@
+import React from "react";
+import f from "lodash/fp";
+import localStore from "localforage";
+import * as rx from "rxjs";
+
+import { either, ifElse } from "./functools";
+
+export const safetyPrefix = "for(;;);";
+export const matchSafetyPrefix = /^for\(;;\);/;
+
+localStore.config({ name: "Campudus", storeName: "GRUD" });
+
+export const toWriteable = raw =>
+  raw
+    ? f.identity
+    : value => {
+        const str = JSON.stringify(value);
+        return safetyPrefix + str; // avoid cross-site attacks
+      };
+
+export const readValue = raw =>
+  raw
+    ? f.identity
+    : value =>
+        either(value)
+          .exec("replace", matchSafetyPrefix, "")
+          .map(JSON.parse)
+          .getOrElse(null);
+
+const streams = {};
+const registerStream = (key, raw, initialValue) => {
+  const subject = new rx.BehaviorSubject();
+  streams[key] = subject;
+  localStore
+    .getItem(key)
+    .then(ifElse(f.isNil, () => initialValue, readValue(raw)))
+    .then(x => subject.next(x))
+    .then(() => {
+      subject.subscribe(value => {
+        localStore.setItem(key, toWriteable(raw)(value));
+      });
+    })
+    .catch(console.error);
+
+  return subject;
+};
+
+export const useLocalStorage = (key, initialValue = null, raw = false) => {
+  streams[key] = streams[key] || registerStream(key, raw, initialValue);
+  const stream = streams[key];
+  const [value, setValue] = React.useState(null);
+
+  React.useEffect(() => {
+    const id = stream.subscribe(setValue);
+    return () => {
+      stream.unsubscribe(id);
+      if (stream && !stream.observers) {
+        streams[key] = null;
+      }
+    };
+  }, []);
+
+  return [value, x => stream.next(x)];
+};
+
+export const withLocalStorage = ({
+  key,
+  initialValue,
+  raw
+}) => Component => props => {
+  const [value, setValue] = useLocalStorage(key, initialValue, raw);
+  const storage = { ...(props.storage || {}), [key]: { value, setValue } };
+  return <Component storage={storage} {...props} />;
+};
