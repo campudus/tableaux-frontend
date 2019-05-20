59252acc423b1a6c199c61800a98bb15107fe292
Hermann Pauly <hp@campudus.com>
2019-05-20 10:13:41 +0200

    Extract header to separate component
    Replace the header found in each major view with a shared styled
    component that automatically includes a navigation menu, a page title,
    and a connections status widget.
    Subscribe this widget to connection status and service registry, to
    stay DRY.

---
+++ src/app/components/GrudHeader.jsx
@@ -0,0 +1,53 @@
+import { connect } from "react-redux";
+import React from "react";
+import f from "lodash/fp";
+
+import PropTypes from "prop-types";
+
+import { ConnectionStatus } from "./header/ConnectionStatus";
+import { filterMainMenuEntries } from "../frontendServiceRegistry/frontendServices";
+import LanguageSwitcher from "./header/LanguageSwitcher";
+import Navigation from "./header/Navigation";
+import PageTitle from "./header/PageTitle";
+
+const mapStateToProps = state => {
+  const services = state.frontendServices || [];
+  const connectedToBackend = f.prop(["grudStatus", "connectedToBackend"])(
+    state
+  );
+
+  return {
+    services,
+    connectedToBackend
+  };
+};
+
+const GrudHeader = ({
+  children,
+  handleLanguageSwitch,
+  langtag,
+  pageTitle,
+  connectedToBackend,
+  services
+}) => (
+  <div className="grud-header-wrapper">
+    <header className="grud-header">
+      <Navigation
+        langtag={langtag}
+        services={services.filter(filterMainMenuEntries)}
+      />
+      {children || <div className="header-separator" />}
+      <PageTitle titleKey={pageTitle} />
+      <LanguageSwitcher langtag={langtag} onChange={handleLanguageSwitch} />
+      <ConnectionStatus isConnected={connectedToBackend} />
+    </header>
+  </div>
+);
+
+export default connect(mapStateToProps)(GrudHeader);
+
+GrudHeader.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  langtag: PropTypes.string.isRequired,
+  handleLanguageSwitch: PropTypes.func.isRequired
+};
