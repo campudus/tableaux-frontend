5e7ccf065a1fbf9a1721851aa52ed63cedec6899
Hermann Pauly <hp@campudus.com>
2019-05-27 11:32:52 +0200

    Add formatting toolbar

--- src/app/components/markdownEditor/EditorPanel.jsx
+++ src/app/components/markdownEditor/EditorPanel.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import PropTypes from "prop-types";
 import {
   Editor,
   EditorState,
@@ -8,7 +6,12 @@ import {
   RichUtils
 } from "draft-js";
 import { markdownToDraft, draftToMarkdown } from "markdown-draft-js";
+import React from "react";
 import f from "lodash/fp";
+
+import PropTypes from "prop-types";
+
+import StyleControls from "./StyleControls";
 
 const markdownToState = f.compose(
   convertFromRaw,
@@ -20,7 +23,7 @@ const stateToMarkdown = f.compose(
   convertToRaw
 );
 
-const EditorPanel = ({ initialMarkdown, onChange }) => {
+const EditorPanel = ({ initialMarkdown, onChange, hideToolbar }) => {
   const [editorState, setEditorState] = React.useState(
     EditorState.createWithContent(markdownToState(initialMarkdown))
   );
@@ -39,14 +42,39 @@ const EditorPanel = ({ initialMarkdown, onChange }) => {
     return "not-handled";
   });
 
+  const toggleBlockType = React.useCallback(typeToToggle => {
+    const stateWithToggledBlock = RichUtils.toggleBlockType(
+      editorState,
+      typeToToggle
+    );
+    handleChange(stateWithToggledBlock);
+  });
+
+  const toggleInlineStyle = React.useCallback(styleToToggle => {
+    const stateWithToggledStyle = RichUtils.toggleInlineStyle(
+      editorState,
+      styleToToggle
+    );
+    handleChange(stateWithToggledStyle);
+  });
+
   console.log({ initialMarkdown, editorState });
 
   return (
-    <Editor
-      editorState={editorState}
-      onChange={handleChange}
-      handleKeyCommand={handleKeyCommand}
-    />
+    <>
+      {!hideToolbar && (
+        <StyleControls
+          toggleBlockType={toggleBlockType}
+          toggleInlineStyle={toggleInlineStyle}
+          editorState={editorState}
+        />
+      )}
+      <Editor
+        editorState={editorState}
+        onChange={handleChange}
+        handleKeyCommand={handleKeyCommand}
+      />
+    </>
   );
 };
 
@@ -54,5 +82,6 @@ export default EditorPanel;
 
 EditorPanel.propTypes = {
   initialMarkdown: PropTypes.string,
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  hideToolbar: PropTypes.bool
 };
