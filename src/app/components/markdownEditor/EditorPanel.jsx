96af2fd28b9b0109e90aff76119f4e2924039418
Hermann Pauly <hp@campudus.com>
2019-06-26 13:41:26 +0200

    Add ability to set links visually
    This is a dirty WIP state; it works but needs refactoring.

--- src/app/components/markdownEditor/EditorPanel.jsx
+++ src/app/components/markdownEditor/EditorPanel.jsx
@@ -1,4 +1,5 @@
 import {
+  CompositeDecorator,
   Editor,
   EditorState,
   convertFromRaw,
@@ -9,9 +10,15 @@ import { markdownToDraft, draftToMarkdown } from "markdown-draft-js";
 import React from "react";
 import f from "lodash/fp";
 import i18n from "i18next";
+import listensToClickOutside from "react-onclickoutside";
 
 import PropTypes from "prop-types";
 
+import {
+  either,
+  preventDefault,
+  stopPropagation
+} from "../../helpers/functools";
 import StyleControls from "./StyleControls";
 
 const markdownToState = f.compose(
@@ -24,73 +31,234 @@ const stateToMarkdown = f.compose(
   convertToRaw
 );
 
-const EditorPanel = (
-  { controlButtons, initialMarkdown, onChange, hideToolbar, readOnly },
-  ref
-) => {
-  const [editorState, setEditorState] = React.useState(
-    EditorState.createWithContent(markdownToState(initialMarkdown))
-  );
-  const editorRef = React.useRef();
-  React.useImperativeHandle(ref, () => ({
-    focus: editorRef.current && editorRef.current.focus()
-  }));
+const LinkEditor = ({ editorState, setEditorState, handleClickOutside }) => {
+  const getUrlAtPoint = selection => {
+    if (!selection.isCollapsed()) {
+      // Adapted from DraftJS link example
+      // https://github.com/facebook/draft-js/blob/ceaeebf1f50fee452d92d71c5e2008e3d4fb6d9f/examples/draft-0-10-0/link/link.html#L76
+      const content = editorState.getCurrentContent();
+      const startKey = selection().getStartKey();
+      const startOffset = selection.getStartOffset();
+      const blockWithLinkAtBeginning = content.getBlockForKey(startKey);
+      const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset);
 
-  const handleChange = React.useCallback(newState => {
-    setEditorState(newState);
-    onChange(stateToMarkdown(newState.getCurrentContent()));
-  });
-
-  const handleKeyCommand = React.useCallback((command, transientState) => {
-    const newState = RichUtils.handleKeyCommand(transientState, command);
-    if (newState) {
-      setEditorState(newState);
-      return "handled";
+      const url = linkKey ? content.getEntity(linkKey).getData().url : "";
+      return url;
+    } else {
+      return "";
     }
-    return "not-handled";
+  };
+
+  const [url, setUrl] = React.useState(
+    either(editorState.getSelection())
+      .map(getUrlAtPoint)
+      .getOrElse("")
+  );
+
+  const inputRef = React.useRef();
+
+  const handleUrlChange = React.useCallback(event => {
+    setUrl(event.target.value);
+    // forceTheGoddamnFocus();
   });
 
-  const toggleBlockType = React.useCallback(typeToToggle => {
-    const stateWithToggledBlock = RichUtils.toggleBlockType(
-      editorState,
-      typeToToggle
+  const removeLink = React.useCallback(event => {
+    preventDefault(event);
+    const selection = editorState.getSelection();
+    if (!selection.isCollapsed()) {
+      setEditorState(RichUtils.toggleLink(editorState, selection, null));
+    }
+  });
+
+  const setLinkUrl = React.useCallback(event => {
+    preventDefault(event);
+    const content = editorState.getCurrentContent();
+    const contentWithNewLink = content.createEntity("LINK", "MUTABLE", { url });
+    const entityKey = contentWithNewLink.getLastCreatedEntityKey();
+    const newEditorState = EditorState.set(editorState, {
+      currentContent: contentWithNewLink
+    });
+    setEditorState(
+      RichUtils.toggleLink(
+        newEditorState,
+        newEditorState.getSelection(),
+        entityKey
+      )
     );
-    handleChange(stateWithToggledBlock);
+    handleClickOutside();
   });
-
-  const toggleInlineStyle = React.useCallback(styleToToggle => {
-    const stateWithToggledStyle = RichUtils.toggleInlineStyle(
-      editorState,
-      styleToToggle
-    );
-    handleChange(stateWithToggledStyle);
-  });
-
-  console.log({ initialMarkdown, editorState });
 
   return (
-    <>
-      {!hideToolbar && (
-        <StyleControls
-          toggleBlockType={toggleBlockType}
-          toggleInlineStyle={toggleInlineStyle}
-          editorState={editorState}
-          additionalButtons={controlButtons}
-        />
-      )}
-      <Editor
-        readOnly={readOnly}
-        ref={editorRef}
-        editorState={editorState}
-        onChange={handleChange}
-        handleKeyCommand={handleKeyCommand}
-        placeholder={i18n.t("table:empty.text")}
+    <div
+      className="link-editor"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <button
+        className="link-editor__cancel-button button"
+        onClick={handleClickOutside}
+      >
+        {i18n.t("common:cancel")}
+      </button>
+      <input
+        placeholder={i18n.t("common:url")}
+        className="link-editor__input"
+        type="text"
+        value={url}
+        onChange={handleUrlChange}
+        ref={inputRef}
       />
-    </>
+      <button
+        className="link-editor__confirm-button button positive"
+        onClick={setLinkUrl}
+      >
+        {i18n.t("common:ok")}
+      </button>
+    </div>
   );
 };
 
-export default React.forwardRef(EditorPanel);
+LinkEditor.propTypes = {
+  handleClickOutside: PropTypes.func.isRequired,
+  setEditorState: PropTypes.func.isRequired,
+  editorState: PropTypes.object.isRequired
+};
+
+// {editLink ? (
+//     <SelfClosingLinkEditor
+//         handleClickOutside={closeLinkOverlay}
+//         editorState={editorState}
+//         setEditorState={setEditorState}
+//     />
+// ) : (
+//     <button onClick={openLinkOverlay}>open link overlay</button>
+// )}
+
+class EditorPanel extends React.PureComponent {
+  static Link = props => {
+    const { url } = props.contentState.getEntity(props.entityKey).getData();
+    return (
+      <a className="markdown-editor__link" href={url}>
+        {props.children}
+      </a>
+    );
+  };
+
+  static findLinkEntities = (contentBlock, callback, contentState) => {
+    contentBlock.findEntityRanges(character => {
+      const entityKey = character.getEntity();
+      return (
+        !f.isNil(entityKey) &&
+        contentState.getEntity(entityKey).getType() === "LINK"
+      );
+    }, callback);
+  };
+
+  static decorator = new CompositeDecorator([
+    { strategy: EditorPanel.findLinkEntities, component: EditorPanel.Link }
+  ]);
+
+  constructor(props) {
+    super(props);
+    const { initialMarkdown } = props;
+    this.state = {
+      editorState: EditorState.createWithContent(
+        markdownToState(initialMarkdown, EditorPanel.decorator)
+      )
+    };
+  }
+
+  handleChange = (newState, callback) => {
+    this.setEditorState(newState, callback);
+    this.props.onChange(stateToMarkdown(newState.getCurrentContent()));
+  };
+
+  handleKeyCommand = (command, transientState) => {
+    const newState = RichUtils.handleKeyCommand(transientState, command);
+    if (newState) {
+      this.setEditorState(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+
+  toggleStyle = toggleFunction => styleToToggle => {
+    const newState = toggleFunction(this.state.editorState, styleToToggle);
+    this.handleChange(newState, this.focus);
+  };
+
+  toggleBlockType = this.toggleStyle(RichUtils.toggleBlockType);
+  toggleInlineStyle = this.toggleStyle(RichUtils.toggleInlineStyle);
+
+  keepMouseFocus = event => {
+    stopPropagation(event);
+  };
+
+  focus = () => this.editorRefElement && this.editorRefElement.focus();
+  editorRef = element => (this.editorRefElement = element);
+  linkEditorRef = element => (this.linkEditorRefElement = element);
+
+  setEditorState = (editorState, callback) => {
+    this.setState({ editorState }, state => {
+      callback && callback(state);
+    });
+  };
+
+  render() {
+    const { hideToolbar, readOnly, controlButtons } = this.props;
+    const { editorState } = this.state;
+    const {
+      keepMouseFocus,
+      toggleBlockType,
+      toggleInlineStyle,
+      setEditorState,
+      editorRef,
+      handleChange,
+      handleKeyCommand
+    } = this;
+
+    return (
+      <>
+        <div onMouseDown={stopPropagation} onClick={stopPropagation}>
+          {!hideToolbar && (
+            <StyleControls
+              toggleBlockType={toggleBlockType}
+              toggleInlineStyle={toggleInlineStyle}
+              editorState={editorState}
+              additionalButtons={controlButtons}
+            />
+          )}
+        </div>
+
+        <div
+          ref={this.linkEditorRef}
+          className="focus-keeper"
+          onClick={keepMouseFocus}
+          onMouseDown={keepMouseFocus}
+          onMouseUp={keepMouseFocus}
+        >
+          <LinkEditor
+            handleClickOutside={() => null}
+            editorState={editorState}
+            setEditorState={setEditorState}
+          />
+        </div>
+
+        <div onMouseDown={stopPropagation} onClick={stopPropagation}>
+          <Editor
+            readOnly={readOnly}
+            ref={editorRef}
+            editorState={editorState}
+            onChange={handleChange}
+            handleKeyCommand={handleKeyCommand}
+            placeholder={i18n.t("table:empty.text")}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+export default EditorPanel;
 
 EditorPanel.propTypes = {
   initialMarkdown: PropTypes.string,
