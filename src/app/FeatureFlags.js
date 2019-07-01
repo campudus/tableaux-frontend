b86f77798c162b079ccbe2e22742d037e0b1e97e
Hermann Pauly <hp@campudus.com>
2019-07-01 16:52:57 +0200

    Add helpers for authentication and user information

--- src/app/FeatureFlags.js
+++ src/app/FeatureFlags.js
@@ -1,11 +1,6 @@
 export const ENABLE_DASHBOARD = true;
-export const SHOW_DASHBOARD_USER_NAME = false;
+export const SHOW_DASHBOARD_USER_NAME = true;
 export const KEYBOARD_TABLE_HISTORY = true;
 export const ENABLE_HISTORY = process.env.ENABLE_HISTORY !== "false";
 export const SHOW_TABLE_DROPDOWN = process.env.SHOW_TABLE_DROPDOWN !== "false";
-
-console.log(
-  "Environment:",
-  process.env.ENABLE_HISTORY,
-  process.env.SHOW_TABLE_DROPDOWN
-);
+export const NO_AUTH_IN_DEV_MODE = false;
--- src/app/helpers/userNameHelper.js
+++ src/app/helpers/userNameHelper.js
@@ -1,3 +1,17 @@
-import Cookie from "js-cookie";
+import { NO_AUTH_IN_DEV_MODE } from "../FeatureFlags";
+import { firstValidPropOr } from "./functools";
+import { getLogin } from "./authenticate";
 
-export const getUserName = () => Cookie.get("userName") || "John Doe";
+export const getUserName = (onlyFirstName = false) => {
+  const keycloak = getLogin();
+
+  const fallbackUserName =
+    process.env.NODE_ENV === "production" ? "John Doe" : "GRUDling";
+  return process.env.NODE_ENV === "development" && NO_AUTH_IN_DEV_MODE
+    ? fallbackUserName
+    : firstValidPropOr(
+        fallbackUserName,
+        [onlyFirstName ? "given_name" : "name", "preferred_username"],
+        keycloak.idTokenParsed
+      );
+};
