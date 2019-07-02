148deb9a37fdd376cedca84310071e4d4efdaabc
Hermann Pauly <hp@campudus.com>
2019-07-02 08:45:34 +0200

    Centralise disabling of auth for developers

--- src/app/helpers/authenticate.jsx
+++ src/app/helpers/authenticate.jsx
@@ -22,41 +22,53 @@ const keycloakSettings = {
 
 const keycloakInitOptions = { onLoad: "login-required" };
 
-// () => bool
+// (state) => bool
 // react-redux@7 selector
 export const authSelector = f.propOr(false, ["grudStatus", "authenticated"]);
 
-// () => Keycloak
-// Side effects: Will login on first load and memoize the result
-export const getLogin = f.memoize(() => {
-  const keycloak = Keycloak(keycloakSettings);
-  keycloak
-    .init(keycloakInitOptions)
-    .success(status => {
-      store.dispatch(actions.setUserAuthenticated({ status }));
-    })
-    .error(err => {
-      console.error("Error authenticating user:", err);
-      store.dispatch(actions.setUserAuthenticated({ status: false }));
-    });
-
-  keycloak.onAuthLogout = () => {
-    store.dispatch(actions.setUserAuthenticated({ status: false }));
-  };
-
-  keycloak.onTokenExpired = () => {
-    keycloak.updateToken();
-  };
-
-  return keycloak;
+export const noAuthNeeded = f.memoize(() => {
+  const result = process.env.NODE_ENV === "development" && NO_AUTH_IN_DEV_MODE;
+  console.log("No auth needed?", {
+    NODE_ENV: process.env.NODE_ENV,
+    NO_AUTH_IN_DEV_MODE,
+    result
+  });
+  return result;
 });
 
-const ignoreAuth =
-  process.env.NODE_ENV === "development" && NO_AUTH_IN_DEV_MODE;
+// () => Keycloak
+// Side effects: Will login on first load and memoize the result
+export const getLogin = f.memoize(
+  noAuthNeeded()
+    ? f.always({})
+    : () => {
+        const keycloak = Keycloak(keycloakSettings);
+        keycloak
+          .init(keycloakInitOptions)
+          .success(status => {
+            store.dispatch(actions.setUserAuthenticated({ status }));
+          })
+          .error(err => {
+            console.error("Error authenticating user:", err);
+            store.dispatch(actions.setUserAuthenticated({ status: false }));
+          });
 
-export const withUserAuthentication = ignoreAuth
+        keycloak.onAuthLogout = () => {
+          store.dispatch(actions.setUserAuthenticated({ status: false }));
+        };
+
+        keycloak.onTokenExpired = () => {
+          keycloak.updateToken();
+        };
+
+        return keycloak;
+      }
+);
+
+export const withUserAuthentication = noAuthNeeded()
   ? f.identity
   : Component => props => {
+      console.log("Wrapping app with authentication");
       const keycloakRef = React.useRef(getLogin());
       const keycloak = keycloakRef.current;
       const isLoggedIn = useSelector(authSelector);
