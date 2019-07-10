4010fe40283c689bf54c6570db9323b62a2d04ee
Hermann Pauly <hp@campudus.com>
2019-07-10 14:55:59 +0200

    Read runtime config once before loading GRUD
    This way, we have all options that can be configured at server launch
    available when we mount GRUD.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "babel-polyfill";
 import "../node_modules/react-select/dist/react-select.css";
 import "./app/helpers/connectionWatcher";
+import { initConfig } from "./app/constants/TableauxConstants";
 
 import { Provider } from "react-redux";
 import React from "react";
@@ -11,9 +12,14 @@ import store from "./app/redux/store";
 
 console.log("Campudus GRUD frontend", process.env.BUILD_ID);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <GRUDRouter />
-  </Provider>,
-  document.querySelector("#tableaux")
-);
+fetch("/config.json")
+  .then(response => response.json())
+  .then(initConfig)
+  .then(() => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <GRUDRouter />
+      </Provider>,
+      document.querySelector("#tableaux")
+    );
+  });
