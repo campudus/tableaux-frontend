5d82d99c844a1075f7fbc71e85478d9f79b44110
Coffee_Ginger <schneider.thimo@protonmail.com>
2021-02-25 10:28:45 +0100

    link button css BEM

--- src/app/components/cells/link/LinkItem.jsx
+++ src/app/components/cells/link/LinkItem.jsx
@@ -50,7 +50,7 @@ const LinkItem = props => {
           className={
             canUserSeeTable(props.cell.column.toTable)
               ? linkButtonClass
-              : linkButtonClass + " disabled"
+              : linkButtonClass + " " + linkButtonClass + "--disabled"
           }
           draggable={false}
           onClick={() => {
