cda3e4e4ce0a5d197904beda4d2206c1090c7f05
Simon Haugen <simon.haugen@campudus.com>
2021-06-28 16:41:56 +0200

    refactored MetaCellHeader

--- src/app/components/cells/MetaCellHeader.jsx
+++ src/app/components/cells/MetaCellHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import PropTypes from "prop-types";
 import {
@@ -7,73 +7,60 @@ import {
 } from "../columns/ColumnHeaderFragments";
 import classNames from "classnames";
 
-class MetaCellHeader extends React.Component {
-  static propTypes = {
-    displayName: PropTypes.string.isRequired,
-    column: PropTypes.object.isRequired
-  };
+const MetaCellHeader = ({ displayName, column }) => {
+  const [ctxCoords, setCtxCoords] = useState(null);
+  const menuOpen = !!ctxCoords;
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      contextMenu: null
-    };
-  }
-
-  openContextMenu = evt => {
+  const openContextMenu = evt => {
     if (!evt) {
       return;
     }
     const parentNode = evt.target.parentNode;
     const rect = parentNode.getBoundingClientRect();
-    this.setState({
-      ctxCoords: {
-        x: rect.left,
-        y: rect.bottom
-      }
+    setCtxCoords({
+      x: rect.left,
+      y: rect.bottom
     });
   };
 
-  closeContextMenu = () => {
-    this.setState({ ctxCoords: null });
+  const closeContextMenu = () => {
+    setCtxCoords(null);
   };
 
-  toggleContextMenu = evt => {
-    this.state.ctxCoords ? this.closeContextMenu() : this.openContextMenu(evt);
+  const toggleContextMenu = evt => {
+    ctxCoords ? closeContextMenu() : openContextMenu(evt);
     evt.preventDefault();
   };
 
-  render() {
-    const { displayName, column } = this.props;
-    const menuOpen = this.state.ctxCoords;
-    const contextMenuClass = classNames("column-contextmenu-button fa ", {
-      "fa-angle-up ignore-react-onclickoutside": menuOpen,
-      "fa-angle-down": !menuOpen
-    });
-
-    return (
-      <div
-        className={classNames("meta-cell-head", {
-          "context-menu-open": menuOpen
+  return (
+    <div
+      className={classNames("meta-cell-head", {
+        "context-menu-open": menuOpen
+      })}
+    >
+      {displayName}
+      <ContextMenuButton
+        contextMenuClass={classNames("column-contextmenu-button fa ", {
+          "fa-angle-up ignore-react-onclickoutside": menuOpen,
+          "fa-angle-down": !menuOpen
         })}
-      >
-        {displayName}
-        <ContextMenuButton
-          contextMenuClass={contextMenuClass}
-          toggleContextMenu={this.toggleContextMenu}
-        />
-        <ContextMenu
-          menuOpen={menuOpen}
-          column={column}
-          closeHandler={this.closeContextMenu}
-          isId={true}
-          rect={this.state.ctxCoords}
-          alignRight={true}
-        />
-      </div>
-    );
-  }
-}
+        toggleContextMenu={toggleContextMenu}
+      />
+      <ContextMenu
+        menuOpen={menuOpen}
+        column={column}
+        closeHandler={closeContextMenu}
+        isId={true}
+        rect={ctxCoords}
+        alignRight={true}
+      />
+    </div>
+  );
+};
+
+MetaCellHeader.propTypes = {
+  displayName: PropTypes.string.isRequired,
+  column: PropTypes.object.isRequired
+};
 
 export default MetaCellHeader;
