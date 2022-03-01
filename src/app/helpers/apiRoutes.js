ce400067d123837ef0c7a24422a11b474955478a
Hermann <hp@campudus.com>
2022-03-01 13:57:52 +0100

    Adapt service url helper

--- src/app/helpers/apiRoutes.js
+++ src/app/helpers/apiRoutes.js
@@ -6,6 +6,20 @@ const cleanUrlPart = f.flow(
 );
 const joinUrlParts = (...args) => args.map(cleanUrlPart).join("/");
 
+const toQSValue = (key, value) => `${key}=${encodeURIComponent(value)}`;
+export const toQueryString = params =>
+  f.compose(
+    f.reduce(
+      (query, [key, value]) =>
+        `${query}&` +
+        (Array.isArray(value)
+          ? value.map(v => toQSValue(key, v)).join("&")
+          : toQSValue(key, value)),
+      ""
+    ),
+    f.toPairs
+  )(params);
+
 const getAllTables = () => "/tables";
 
 const getAllColumnsForTable = tableId => "/tables/" + tableId + "/columns";
@@ -36,8 +50,19 @@ const toFile = (fileId, langtag) =>
   (langtag ? "?langtag=" + langtag : "");
 
 const toServiceRegistry = () => "/system/services";
-const toFrontendServiceView = (id, langtag) =>
-  "/" + langtag + "/services/" + id;
+const toFrontendServiceView = (id, langtag, params) => {
+  const baseUrl = `/${langtag}/services/${id}`;
+  return f.compose(
+    str => `${str}?${toQueryString(params)}`,
+    f.join("/"),
+    f.compact
+  )([
+    baseUrl,
+    params.tableId && `tables/${params.tableId}`,
+    params.columnId && `columns/${params.columnId}`,
+    params.rowId && `rows/${params.rowId}`
+  ]);
+};
 
 const toCellHistory = ({ tableId, rowId, columnId }) =>
   toCell({ tableId, rowId, columnId }) + "/history";
