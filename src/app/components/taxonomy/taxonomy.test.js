fbf2f69bb435b2c0fb17bbafc01e1b194b6c77ba
Hermann Pauly <hp@campudus.com>
2022-10-10 13:28:54 +0200

    Add helpers to build trees and toggle expanded state

---
+++ src/app/components/taxonomy/taxonomy.test.js
@@ -0,0 +1,156 @@
+import * as t from "./taxonomy";
+import { performance } from "perf_hooks";
+import { range } from "lodash/fp";
+
+const time = fn => (...args) => {
+  const start = performance.now();
+  // Randomly generated trees might contains stack-blowingly deep parent nestings.
+  // We prefer that way over uglymising the code for artificial tests.
+  try {
+    fn(...args);
+  } catch {
+    // pass
+  }
+  return performance.now() - start;
+};
+
+describe("taxonomy helpers", () => {
+  const nodes = [
+    { id: 1, parent: null },
+    { id: 2, parent: 1 },
+    { id: 3, parent: 1 },
+    { id: 4, parent: null },
+    { id: 5, parent: 4 },
+    { id: 6, parent: 5 },
+    { id: 7, parent: 6 }
+  ];
+
+  describe("getPathToNode", () => {
+    it("should find the correct path to specific nodes", () => {
+      const startNode = nodes.find(node => node.id === 7);
+      const path = t.getPathToNode(nodes)(startNode);
+      expect(path.map(node => node.id)).toEqual([4, 5, 6]);
+    });
+    it("should return an empty path if root node is selected", () => {
+      const startNode = nodes[0];
+      expect(t.getPathToNode(nodes)(startNode)).toEqual([]);
+    });
+    it("should return an empty path if no node is given", () => {
+      expect(t.getPathToNode(nodes)(null)).toEqual([]);
+    });
+    it("should return an empty path if node list is corrupt", () => {
+      expect(t.getPathToNode(null)(nodes[0])).toEqual([]);
+    });
+  });
+
+  describe("buildTree", () => {
+    it("should create a tree from nodes", () => {
+      const tree = t.buildTree({})(nodes);
+      expect(tree).toEqual([
+        {
+          children: [
+            { children: [], expanded: false, id: 2, onPath: false, parent: 1 },
+            { children: [], expanded: false, id: 3, onPath: false, parent: 1 }
+          ],
+          expanded: false,
+          id: 1,
+          onPath: false,
+          parent: null
+        },
+        {
+          children: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      children: [],
+                      expanded: false,
+                      id: 7,
+                      onPath: false,
+                      parent: 6
+                    }
+                  ],
+                  expanded: false,
+                  id: 6,
+                  onPath: false,
+                  parent: 5
+                }
+              ],
+              expanded: false,
+              id: 5,
+              onPath: false,
+              parent: 4
+            }
+          ],
+          expanded: false,
+          id: 4,
+          onPath: false,
+          parent: null
+        }
+      ]);
+    });
+    it("should mark nodes correctly as expanded", () => {
+      const tree = t.buildTree({ expandedNodeId: 6 })(nodes);
+      expect(tree).toEqual([
+        {
+          children: [
+            { children: [], expanded: false, id: 2, onPath: false, parent: 1 },
+            { children: [], expanded: false, id: 3, onPath: false, parent: 1 }
+          ],
+          expanded: false,
+          id: 1,
+          onPath: false,
+          parent: null
+        },
+        {
+          children: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      children: [],
+                      expanded: false,
+                      id: 7,
+                      onPath: false,
+                      parent: 6
+                    }
+                  ],
+                  expanded: true,
+                  id: 6,
+                  onPath: false,
+                  parent: 5
+                }
+              ],
+              expanded: false,
+              id: 5,
+              onPath: true,
+              parent: 4
+            }
+          ],
+          expanded: false,
+          id: 4,
+          onPath: true,
+          parent: null
+        }
+      ]);
+    });
+    it("should build large trees performantly", () => {
+      const size = 2000;
+      const manyNodes = range(0, size).map((_, idx) => {
+        const isRootNode = Math.random() < 0.05;
+        return {
+          id: idx,
+          parent: isRootNode ? null : Math.round(Math.random() * size)
+        };
+      });
+      const buildUnexpanded = t.buildTree({});
+      const buildExpanded = t.buildTree({
+        expandedNodeId: Math.round(Math.random() * size)
+      });
+      expect(time(buildUnexpanded)(manyNodes)).toBeLessThan(250);
+      expect(time(buildExpanded)(manyNodes)).toBeLessThan(250);
+    });
+  });
+});
