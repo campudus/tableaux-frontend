e4cf30d137f1b6dbf6958a19d382961ad0c3d3b1
Hermann Pauly <hp@campudus.com>
2022-10-12 09:45:16 +0200

    Refactor TreeView as reusable component

--- src/app/components/taxonomy/TaxonomyTable.jsx
+++ src/app/components/taxonomy/TaxonomyTable.jsx
@@ -1,67 +1,14 @@
-import React, { useCallback, useState } from "react";
+import f from "lodash/fp";
+import React from "react";
 import { useSelector } from "react-redux";
 import * as t from "./taxonomy";
-import f from "lodash/fp";
-import { retrieveTranslation } from "../../helpers/multiLanguage";
-
-const TreeEntry = ({ node, onClick, children, langtag }) => {
-  const handleClick = useCallback(
-    event => {
-      event.stopPropagation();
-      onClick(node);
-    },
-    [node.id, node.expanded]
-  );
-  const childCount = `(${node.children.length})`;
-  const childrenToRender = node.expanded || node.onPath ? node.children : null;
-
-  return (
-    <li onClick={handleClick} style={{ paddingLeft: "24px" }}>
-      <span>{retrieveTranslation(langtag, node.displayValue)}</span>
-      {t.isLeaf(node) ? null : <span>{childCount}</span>}
-      {childrenToRender ? (
-        <ul>
-          {childrenToRender.map(n => (
-            <TreeEntry
-              key={n.id}
-              node={n}
-              onClick={onClick}
-              langtag={langtag}
-            />
-          ))}
-        </ul>
-      ) : null}
-    </li>
-  );
-};
+import TreeView from "./TreeView";
 
 const TaxonomyTable = ({ langtag, tableId }) => {
   const rows = useSelector(f.propOr([], ["rows", tableId, "data"]));
-  const [expandedNodeId, setExpandedNodeId] = useState(null);
+  const nodes = t.tableToTreeNodes({ rows });
 
-  const tree = f.compose(
-    t.buildTree({ expandedNodeId }),
-    t.tableToTreeNodes
-  )({ rows });
-
-  const toggleExpandNode = node => {
-    setExpandedNodeId(expandedNodeId === node.id ? node.parent : node.id);
-  };
-
-  return (
-    <section className="taxonomy-tree">
-      <ul>
-        {tree.map(node => (
-          <TreeEntry
-            key={node.id}
-            node={node}
-            onClick={toggleExpandNode}
-            langtag={langtag}
-          />
-        ))}
-      </ul>
-    </section>
-  );
+  return <TreeView nodes={nodes} langtag={langtag} />;
 };
 
 TaxonomyTable.displayName = "TaxonomyTable";
