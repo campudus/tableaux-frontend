819786edf31e51767c4908c71499ba75754ce7c7
Hermann Pauly <hp@campudus.com>
2022-10-13 12:04:40 +0200

    Add popup menu for taxonomy table view

--- src/app/components/taxonomy/TaxonomyTable.jsx
+++ src/app/components/taxonomy/TaxonomyTable.jsx
@@ -1,13 +1,122 @@
+import i18n from "i18next";
 import f from "lodash/fp";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { outsideClickEffect } from "../../helpers/useOutsideClick";
 import * as t from "./taxonomy";
 import TreeView from "./TreeView";
+import SvgIcon from "../helperComponents/SvgIcon";
+import { buildClassName } from "../../helpers/buildClassName";
 
 const shouldShowAction = ({ node, expandedNodeId }) =>
   (!node.parent && !expandedNodeId) || node.parent === expandedNodeId;
 
-const TableEditor = () => <span>X</span>;
+// EditorEntryDescription :
+//   { faIcon: String
+//   , titleKey: String
+//   , onClick: { node: TreeNode } => ()
+//   , isVisible: { node: TreeNode } => Boolean
+//   }
+
+// EditorEntry : { entry: EditorEntryDescription, node: TreeNode } -> React.Element
+const EditorEntry = ({ entry, node }) => (
+  <li
+    className="tree-node__menu-popup__item"
+    onClick={() => entry.onClick(node)}
+  >
+    <span className="item__icon">
+      <i className={`fa ${entry.faIcon}`} />
+    </span>
+    <span className="item-title">{i18n.t(entry.titleKey)}</span>
+  </li>
+);
+
+// EditorEntryGroup : { entries: List EditorEntryDescription, node: TreeNode } -> React.Element
+const EditorEntryGroup = ({ entries, node }) => (
+  <li className="tree-node__menu-popup__item-group">
+    <ul>
+      {entries.map((entry, idx) => (
+        <EditorEntry key={idx} entry={entry} node={node} />
+      ))}
+    </ul>
+  </li>
+);
+
+// mkTableEditor : List (List EditorEntryDescription) -> { node: TreeNode } -> React.Element
+const mkTableEditor = entryGroups => ({ node }) => {
+  const groups = entryGroups
+    .map(group =>
+      group.filter(
+        entry => !f.isFunction(entry.isVisible) || entry.isVisible(node)
+      )
+    )
+    .filter(group => !f.isEmpty(group));
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleTogglePopup = useCallback(
+    event => {
+      event.stopPropagation();
+      setIsOpen(!isOpen);
+    },
+    [isOpen]
+  );
+  const handleClosePopup = useCallback(() => setIsOpen(false), []);
+  const containerRef = useRef(null);
+
+  useEffect(
+    outsideClickEffect({
+      shouldListen: isOpen,
+      onOutsideClick: handleClosePopup,
+      containerRef
+    }),
+    [isOpen, containerRef.current]
+  );
+
+  const buttonClass = buildClassName("tree-node__menu-button", {
+    open: isOpen
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={handleTogglePopup}
+      ref={containerRef}
+    >
+      <SvgIcon
+        containerClasses="tree-node__menu-button-icon"
+        icon="vdots"
+        center
+      />
+      {isOpen ? (
+        <div className="tree-node__menu-popup">
+          <ul className="tree--node__menu-popup__items-list">
+            {groups.map((entries, idx) => (
+              <EditorEntryGroup key={idx} entries={entries} node={node} />
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </button>
+  );
+};
+
+const TableEditor = mkTableEditor([
+  [
+    {
+      faIcon: "fa-level-down flip-lr",
+      titleKey: "table:taxonomy.add-sibling-below"
+    },
+    { faIcon: "fa-level-up rotate-90", titleKey: "table:taxonomy.add-child" }
+  ],
+  [
+    { faIcon: "fa-pencil", titleKey: "table:taxonomy.edit" },
+    {
+      faIcon: "fa-trash",
+      titleKey: "table:taxonomy.delete",
+      isVisible: t.isLeaf
+    }
+  ]
+]);
 
 const TaxonomyTable = ({ langtag, tableId }) => {
   const rows = useSelector(f.propOr([], ["rows", tableId, "data"]));
