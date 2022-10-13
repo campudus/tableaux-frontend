c0cc6b5545073cf8252a27ea9c56089da1cf9602
Hermann Pauly <hp@campudus.com>
2022-10-13 12:02:12 +0200

    Add helper for outside clicks in function components

---
+++ src/app/helpers/useOutsideClick.jsx
@@ -0,0 +1,35 @@
+/*
+ * react-onclickoutside won't work with functional components.
+ * This is a replacement as suggested by the react-onclickoutside library.
+ * It is meant to be used in an useEffect hook, like
+ *
+ * useEffect(outsideClickEffect({
+ *   shouldListen: popupOpen,
+ *   containerRef: myRef,
+ *   onOutsideClick: closePopup
+ * }), [popupOpen, myRef.current])
+ *
+ * The useEffect's cleanup mechanism will take care of removing event listeners.
+ */
+
+const outsideClickEffect = ({
+  shouldListen, // Boolean
+  containerRef, // React.leagacyRef
+  onOutsideClick // Html.Event -> ()
+}) => () => {
+  if (!shouldListen || !containerRef.current) return;
+  else {
+    const handleOutsideClick = event => {
+      if (!containerRef.current || containerRef.current.contains(event.target))
+        return;
+      onOutsideClick(event);
+    };
+    const handleCleanUp = () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return handleCleanUp;
+  }
+};
+
+export { outsideClickEffect };
