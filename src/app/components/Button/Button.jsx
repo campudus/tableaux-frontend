131d3f2127c71057048c5d7c83454ff4436e9ef1
Hermann Pauly <hp@campudus.com>
2022-11-16 08:50:57 +0100

    Allow button to show "waiting" spinner

--- src/app/components/Button/Button.jsx
+++ src/app/components/Button/Button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { buildClassName } from "../../helpers/buildClassName";
+import Spinner from "../header/Spinner";
 
 export const Button = ({
   onClick,
@@ -7,17 +8,34 @@ export const Button = ({
   classNames,
   children,
   text,
-  alt
-}) => (
-  <button
-    className={`${buildClassName("button", { disabled })} ${classNames || ""}`}
-    onClick={onClick}
-    disabled={!!disabled}
-    title={alt}
-  >
-    {text || children}
-  </button>
-);
+  alt,
+  waiting
+}) => {
+  const content = text ? (
+    <span className="button__text">{text}</span>
+  ) : (
+    children
+  );
+
+  const handleClick = waiting ? () => null : onClick;
+
+  return (
+    <button
+      className={`${buildClassName("button", {
+        disabled,
+        waiting
+      })} ${classNames || ""}`}
+      onClick={handleClick}
+      disabled={!!disabled}
+      title={alt}
+    >
+      <div className="button__spinner">
+        <Spinner isLoading={waiting} />
+      </div>
+      {content}
+    </button>
+  );
+};
 
 Button.displayName = "Button";
 export default Button;
