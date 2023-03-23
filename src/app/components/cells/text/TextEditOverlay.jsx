cad9319546f143e4018f034664ddb3615041a3fa
Thimo <schneider.thimo@protonmail.com>
2023-03-23 18:47:56 +0100

    Limit Text Length in TextEditOverlay

--- src/app/components/cells/text/TextEditOverlay.jsx
+++ src/app/components/cells/text/TextEditOverlay.jsx
@@ -1,21 +1,53 @@
 import React from "react";
 import { compose, lifecycle, withStateHandlers } from "recompose";
 import i18n from "i18next";
+import { columnHasMaxLength, columnHasMinLength, isTextTooShort, isTextTooLong, getTextLength } from "../../../helpers/limitTextLength";
 
 const TextEditOverlay = props => {
-  const { editedValue, setValue, saveEdits, readOnly } = props;
-
+  const { editedValue, setValue, saveEdits, readOnly, cell: { column } } = props;
+  const { minLength, maxLength } = column
+  const [clickedOutside, setClickedOutside] = React.useState(false);
+  const minLengthText = columnHasMinLength(column) ? i18n.t("table:text-length:min-length-full", { minLength }) : ""
+  const maxLengthText = columnHasMaxLength(column) ? `${getTextLength(editedValue)}/${maxLength}` : ""
+  const textTooShort = isTextTooShort(column, editedValue)
+  const shouldCatchOutsideClick = textTooShort
+  const errorCssClass = (clickedOutside && textTooShort) ? "markdown-editor_error" : ""
+  const onOutsideClick = evt => {
+    setClickedOutside(true);
+    evt.stopPropagation();
+    evt.preventDefault();
+  }
+  const onChange = (evt) => {
+    setClickedOutside(false)
+    const value = evt.target.value
+    if (isTextTooLong(column, value)) {
+      return
+    }
+    setValue(evt)
+  }
+  const onBlur = () => {
+    if (shouldCatchOutsideClick) {
+      return
+    }
+    saveEdits()
+  }
   return (
     <div className="content-items richtext-cell-editor">
       <div className="item">
-        <div className="item-content shorttext" tabIndex={1}>
+        {shouldCatchOutsideClick && (<div className="catchOutsideClick" onClick={onOutsideClick} />)}
+        <div className="item-content shorttext textarea_wrapper" tabIndex={1}>
           <textarea
+            className={errorCssClass}
             disabled={!!readOnly}
             value={editedValue}
             placeholder={i18n.t("table:empty.text")}
-            onChange={setValue}
-            onBlur={saveEdits}
+            onChange={onChange}
+            onBlur={onBlur}
           />
+        </div>
+        <div className="length-limits" >
+          <div className={`min-length ${errorCssClass}`}>{minLengthText} </div>
+          <div className="max-length">{maxLengthText} </div>
         </div>
       </div>
     </div>
