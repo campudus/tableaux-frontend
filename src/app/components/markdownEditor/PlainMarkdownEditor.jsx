2a525581c893d8c34e99e053a3cf052d2a59ba20
Thimo <schneider.thimo@protonmail.com>
2023-03-23 18:48:46 +0100

    Limit Text Length in MarkdownEditor

--- src/app/components/markdownEditor/PlainMarkdownEditor.jsx
+++ src/app/components/markdownEditor/PlainMarkdownEditor.jsx
@@ -2,9 +2,12 @@ import classNames from "classnames";
 import "codemirror/mode/markdown/markdown";
 import PropTypes from "prop-types";
 import React from "react";
+// import { Controlled as CodeMirror } from "react-codemirror2";
 import CodeMirror from "react-codemirror";
 import ReactMarkdown from "react-markdown";
 import { StyleIcon } from "./StyleControls";
+import { columnHasMinLength, columnHasMaxLength, isTextTooShort, getTextLength } from "../../helpers/limitTextLength";
+import i18n from "i18next";
 
 const PreviewModes = {
   HORIZONTAL: "HORIZONTAL",
@@ -13,12 +16,16 @@ const PreviewModes = {
 };
 
 const PlainMarkdownEditor = (
-  { controlButtons, initialMarkdown, className, onChange, readOnly },
+  { controlButtons, initialMarkdown, className, onChange, readOnly, cell: { column } },
   ref
 ) => {
   const [markdown, setMarkdown] = React.useState(initialMarkdown || "");
+  const [clickedOutside, setClickedOutside] = React.useState(false);
   const editorRef = React.useRef();
 
+  const { minLength, maxLength } = column;
+  const minLengthText = columnHasMinLength(column) ? i18n.t("table:text-length:min-length-full", { minLength }) : ""
+  const maxLengthText = columnHasMaxLength(column) ? `${getTextLength(markdown)}/${maxLength}` : ""
   // FIXME: Other preview modes' display components problematic with current
   // CodeMirror versions
   const markdownPreview = PreviewModes.HORIZONTAL;
@@ -37,17 +44,35 @@ const PlainMarkdownEditor = (
     lineNumbers: false,
     mode: "markdown",
     lineWrapping: true,
-    readOnly
+    readOnly: false,
   };
 
   React.useImperativeHandle(ref, () => ({
     focus: () => editorRef.current && editorRef.current.focus()
   }));
 
-  const handleChange = React.useCallback(newValue => {
+  const handleChange = (newValue) => {
+    setClickedOutside(false)
+    if (columnHasMaxLength(column) && (getTextLength(newValue) > column.maxLength)) {
+      //We have to manually set the value of the internal codemirror instance
+      //as this is not a true controlled component. Not doing this WILL lead
+      //to different states.
+      const codeMirrorDoc = editorRef.current.getCodeMirror().getDoc()
+      const currentCursor = codeMirrorDoc.getCursor()
+      const newCursor = { ...currentCursor, ch: currentCursor.ch - 1 }
+      codeMirrorDoc.setValue(markdown)
+      //set cursor to previous location
+      codeMirrorDoc.setCursor(newCursor)
+
+      return
+    }
     setMarkdown(newValue);
-    onChange(newValue);
-  });
+    if (isTextTooShort(newValue)) {
+      onChange(initialMarkdown);
+    } else {
+      onChange(newValue)
+    }
+  }
 
   const previewSelectorControls = [
     // FIXME: see above
@@ -74,6 +99,15 @@ const PlainMarkdownEditor = (
     // }
   ];
 
+  const onOutsideClick = evt => {
+    evt.stopPropagation();
+    setClickedOutside(true);
+  }
+
+  const textTooShort = isTextTooShort(column, markdown)
+  const shouldCatchOutsideClick = textTooShort
+  const errorCssClass = (clickedOutside && textTooShort) ? "markdown-editor_error" : ""
+
   return (
     <>
       <header className="richtext-toggle-style__bar">
@@ -84,13 +118,20 @@ const PlainMarkdownEditor = (
         {controlButtons}
       </header>
       <div className={cssClass}>
-        <CodeMirror
-          ref={editorRef}
-          value={markdown}
-          onChange={handleChange}
-          options={editorOptions}
-          autoFocus={true}
-        />
+        {shouldCatchOutsideClick && (<div className="catchOutsideClick" onClick={onOutsideClick} />)}
+        <div className={`cm-wrapper ${errorCssClass}`}>
+          <CodeMirror
+            ref={editorRef}
+            value={markdown}
+            onChange={handleChange}
+            options={editorOptions}
+            autoFocus={true}
+          />
+          <div className="length-limits" >
+            <div className={`min-length ${errorCssClass}`}>{minLengthText} </div>
+            <div className="max-length">{maxLengthText} </div>
+          </div>
+        </div>
         <div className="plain-markdown-editor__preview">
           <div className="plain-markdown-editor-preview__react-markdown">
             <ReactMarkdown source={markdown} />
