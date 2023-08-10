28e719e244afe6c9c028af525683707ee236e3e1
Hermann Pauly <hp@campudus.com>
2023-08-10 16:25:32 +0200

    Refactor filter rows

--- src/app/components/header/filter/FilterRow.jsx
+++ src/app/components/header/filter/FilterRow.jsx
@@ -1,5 +1,5 @@
 import { translate } from "react-i18next";
-import React, { PureComponent } from "react";
+import React, { useCallback, useRef } from "react";
 import Select from "react-select";
 import f from "lodash/fp";
 import i18n from "i18next";
@@ -14,158 +14,146 @@ import SvgIcon from "../../helperComponents/SvgIcon";
 export const BOOL = "boolean";
 export const TEXT = "text";
 
-@translate(["table", "filter"])
-class FilterRow extends PureComponent {
-  static propTypes = {
-    searchableColumns: PropTypes.array.isRequired,
-    valueRenderer: PropTypes.func.isRequired,
-    onChangeColumn: PropTypes.func.isRequired,
-    onChangeValue: PropTypes.func.isRequired,
-    onChangeMode: PropTypes.func.isRequired,
-    filter: PropTypes.object.isRequired,
-    onAddFilter: PropTypes.func,
-    onRemoveFilter: PropTypes.func,
-    applyFilters: PropTypes.func.isRequired
+const FilterRow = ({
+  applyFilters,
+  filter,
+  onAddFilter,
+  onChangeColumn,
+  onChangeMode,
+  onChangeValue,
+  onRemoveFilter,
+  searchableColumns,
+  valueRenderer,
+  t
+}) => {
+  const { columnId } = filter;
+  const clearValue = () => {
+    if (onChangeValue) onChangeValue({});
   };
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  getKeyboardShortcuts = () => {
-    const { applyFilters } = this.props;
-    return {
-      enter: () => {
-        applyFilters();
+  const clearColumn = () => {
+    if (onChangeColumn) onChangeColumn();
+  };
+  const getKeyboardShortcuts = useCallback(
+    () => (
+      {
+        enter: () => {
+          applyFilters();
+        },
+        escape: () => {
+          clearValue();
+        }
       },
-      escape: () => {
-        const { onChangeValue } = this.props;
-        onChangeValue && onChangeValue({});
-      }
-    };
+      [applyFilters, clearValue]
+    )
+  );
+
+  const filterInputRef = useRef(null);
+  const focusFilterInput = () => {
+    filterInputRef.current?.focus();
   };
-
-  setFilterInputRef = node => {
-    this.filterInput = node;
+  const clearFilter = () => {
+    clearValue();
+    clearColumn();
   };
-
-  focusFilterInput = () => {
-    this.filterInput && this.filterInput.focus();
+  const clearOrRemoveFilter = () => {
+    if (onRemoveFilter) onRemoveFilter();
+    else clearFilter();
   };
+  const handleChangeFilterMode = mode => onChangeMode(mode.value);
 
-  clearFilter = () => {
-    const { onChangeValue, onChangeColumn } = this.props;
-    onChangeValue && onChangeValue({});
-    onChangeColumn && onChangeColumn({});
-  };
+  const isFilterColumnSelected =
+    f.isInteger(parseInt(columnId)) ||
+    (f.isString(columnId) && !f.isEmpty(columnId));
+  const filterModeOptions =
+    SearchFunctions
+    |> f.keys
+    |> f.map(key => ({
+      value: key,
+      label: i18n.t(SearchFunctions[key].displayName)
+    }));
 
-  clearOrRemoveFilter = () => {
-    const { onRemoveFilter } = this.props;
-    onRemoveFilter ? onRemoveFilter() : this.clearFilter();
-  };
+  return (
+    <div className="filter-row">
+      <button
+        className="filter-array-button col-one"
+        onClick={clearOrRemoveFilter}
+      >
+        <i className="fa fa-trash" />
+      </button>
 
-  handleFilterModeChange = mode => {
-    this.props.onChangeMode(mode.value);
-  };
-
-  render() {
-    const {
-      filter,
-      onAddFilter,
-      onChangeColumn,
-      onChangeValue,
-      searchableColumns,
-      t,
-      valueRenderer
-    } = this.props;
-    const { columnId } = this.props.filter;
-    const filterColumnSelected =
-      f.isInteger(parseInt(columnId)) ||
-      (f.isString(columnId) && !f.isEmpty(columnId));
-
-    const filterModeOptions =
-      SearchFunctions
-      |> f.keys
-      |> f.map(key => ({
-        value: key,
-        label: i18n.t(SearchFunctions[key].displayName)
-      }));
-
-    return (
-      <div className="filter-row">
-        <button
-          className="filter-array-button col-one"
-          onClick={this.clearOrRemoveFilter}
-        >
-          <i className="fa fa-trash" />
-        </button>
-
-        <Select
-          className="filter-select col-two"
-          options={searchableColumns}
-          searchable={true}
-          clearable={false}
-          openOnFocus
-          value={columnId}
-          onChange={onChangeColumn}
-          placeholder={t("filter:input.filter")}
-          valueRenderer={valueRenderer}
-          noResultsText={t("input.noResult")}
-        />
-        {filter.columnKind !== BOOL &&
-          !f.isNil(filter.columnId) &&
-          Number(filter.columnId) >= 0 &&
-          filter.mode !== "STATUS" && (
-            <Select
-              className="filter-row__mode-select col-three"
-              searchable={false}
-              clearable={false}
-              openOnFocus
-              value={filter.mode}
-              options={filterModeOptions}
-              onChange={this.handleFilterModeChange}
-            />
-          )}
-
-        {filter.columnKind === BOOL || filter.mode === "STATUS" ? (
-          <BoolInput
-            value={this.props.filter.value}
-            onChangeValue={this.props.onChangeValue}
+      <Select
+        className="filter-select col-two"
+        options={searchableColumns}
+        searchable={true}
+        clearable={false}
+        openOnFocus
+        value={columnId}
+        onChange={onChangeColumn}
+        placeholder={t("filter:input.filter")}
+        valueRenderer={valueRenderer}
+        noResultsText={t("input.noResult")}
+      />
+      {filter.columnKind !== BOOL &&
+        !f.isNil(filter.columnId) &&
+        Number(filter.columnId) >= 0 &&
+        filter.mode !== "STATUS" && (
+          <Select
+            className="filter-row__mode-select col-three"
+            searchable={false}
+            clearable={false}
+            openOnFocus
+            value={filter.mode}
+            options={filterModeOptions}
+            onChange={handleChangeFilterMode}
           />
-        ) : (
-          <span className="filter-mode-wrapper col-four">
-            <input
-              value={f.isString(filter.value) ? filter.value : ""}
-              type="text"
-              className="filter-input"
-              disabled={!filterColumnSelected}
-              ref={this.setFilterInputRef}
-              onChange={onChangeValue}
-              onKeyDown={KeyboardShortcutsHelper.onKeyboardShortcut(
-                this.getKeyboardShortcuts
-              )}
-              onClick={this.focusFilterInput}
-            />
-            {!f.isNil(columnId) && (
-              <button
-                onClick={() => onChangeValue({ target: { value: "" } })}
-                className="filter-input__clear-button col-five"
-              >
-                <SvgIcon icon="cross" />
-              </button>
+        )}
+
+      {filter.columnKind === BOOL || filter.mode === "STATUS" ? (
+        <BoolInput value={filter.value} onChangeValue={onChangeValue} />
+      ) : (
+        <span className="filter-mode-wrapper col-four">
+          <input
+            value={f.isString(filter.value) ? filter.value : ""}
+            type="text"
+            className="filter-input"
+            disabled={!isFilterColumnSelected}
+            ref={filterInputRef}
+            onChange={onChangeValue}
+            onKeyDown={KeyboardShortcutsHelper.onKeyboardShortcut(
+              getKeyboardShortcuts
             )}
-          </span>
-        )}
+            onClick={focusFilterInput}
+          />
+          {!f.isNil(columnId) && (
+            <button
+              onClick={() => onChangeValue({ target: { value: "" } })}
+              className="filter-input__clear-button col-five"
+            >
+              <SvgIcon icon="cross" />
+            </button>
+          )}
+        </span>
+      )}
 
-        {onAddFilter && (
-          <button className="filter-array-button" onClick={onAddFilter}>
-            <i className="fa fa-plus" />
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+      {onAddFilter && (
+        <button className="filter-array-button" onClick={onAddFilter}>
+          <i className="fa fa-plus" />
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default FilterRow;
+FilterRow.propTypes = {
+  applyFilters: PropTypes.func.isRequired,
+  filter: PropTypes.object.isRequired,
+  onAddFilter: PropTypes.func,
+  onChangeColumn: PropTypes.func.isRequired,
+  onChangeMode: PropTypes.func.isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  onRemoveFilter: PropTypes.func,
+  searchableColumns: PropTypes.array.isRequired,
+  valueRenderer: PropTypes.func.isRequired
+};
+
+export default translate(["table", "filter"])(FilterRow);
