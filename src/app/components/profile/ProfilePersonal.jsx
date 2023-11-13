b0ace9ae50317eb64ec2d00363742c761d29c9c0
smnhgn <simon.haugen@campudus.com>
2023-11-13 15:42:38 +0100

    feat: add profile personal tab

--- src/app/components/profile/ProfilePersonal.jsx
+++ src/app/components/profile/ProfilePersonal.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { t } from "i18next";
+import { buildClassName as cn } from "../../helpers/buildClassName";
 import Breadcrumbs from "../helperComponents/Breadcrumbs";
+import { getLogin } from "../../helpers/authenticate";
+import UserIcon from "./UserIcon";
 
 export default function ProfilePersonal({ langtag }) {
+  const login = getLogin();
+  const info = login.tokenParsed;
+
   return (
-    <div className="profile-personal">
+    <div className={cn("profile-tab", { personal: true })}>
       <Breadcrumbs
         links={[
           {
@@ -17,6 +23,35 @@ export default function ProfilePersonal({ langtag }) {
           }
         ]}
       />
+
+      <div className="profile-tab__header">
+        <h1 className="profile-tab__title">
+          {t("profile:personal-data.title")}
+        </h1>
+        <h2 className="profile-tab__subtitle">
+          {t("profile:personal-data.subtitle")}
+        </h2>
+      </div>
+
+      <div className="profile-tab__section">
+        <UserIcon className="profile-tab__icon" />
+        {!!info && (
+          <div className="profile-tab__info">
+            <div className="profile-tab__label">
+              {t("profile:personal-data.display-name")}
+            </div>
+            <div className="profile-tab__value">{info.preferred_username}</div>
+            <div className="profile-tab__label">
+              {t("profile:personal-data.user-name")}
+            </div>
+            <div className="profile-tab__value">{info.name}</div>
+            <div className="profile-tab__label">
+              {t("profile:personal-data.email-address")}
+            </div>
+            <div className="profile-tab__value">{info.email}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
