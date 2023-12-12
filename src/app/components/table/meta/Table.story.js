5d8a848fa07c7525e232988c2e504744dfa7aa62
Hermann Pauly <hp@campudus.com>
2023-12-12 14:25:08 +0100

    Remove unused files

--- src/app/components/table/meta/Table.story.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/react";
-
-import React from "react";
-import Table from "../Table";
-
-const props = { table: { columns: [] }, rows: {} };
-
-storiesOf("Table", module).add("default", () => <Table {...props} />);
