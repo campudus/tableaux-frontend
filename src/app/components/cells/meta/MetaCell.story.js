766d03b6b8d5e88e24967f0bbcd372bbd06630af
Hermann Pauly <hp@campudus.com>
2023-12-12 14:25:08 +0100

    Remove unused files

--- src/app/components/cells/meta/MetaCell.story.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from "@storybook/react";
-
-import React from "react";
-import MetaCell from "../MetaCell";
-
-storiesOf("MetaCell", module)
-  .add("default", () => <MetaCell row={{ id: 3 }} langtag="de" />)
-  .add("expanded", () => <MetaCell row={{ id: 3 }} langtag="de" expanded />);
--- src/app/components/header/meta/Spinner.story.js
+++
@@ -1,6 +0,0 @@
-import { storiesOf } from "@storybook/react";
-
-import React from "react";
-import Spinner from "../Spinner";
-
-storiesOf("Spinner", module).add("default", () => <Spinner isLoading />);
