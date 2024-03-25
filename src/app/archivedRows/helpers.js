1dcead8bf7be130dac6aecfe1477c29ace9c6c3f
Hermann Pauly <hp@campudus.com>
2024-03-25 14:04:07 +0100

    Add mock link archive helper

--- src/app/archivedRows/helpers.js
+++ src/app/archivedRows/helpers.js
@@ -1,1 +1,3 @@
 export const isRowArchived = _ => false; // TODO: implement once we decided on the data type
+
+export const isLinkArchived = _ => Math.random() < 0.5; // TODO: implement once we decided on the data type
