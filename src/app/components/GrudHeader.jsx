1cf99f66b5d474e2f4f7e71d901fd853970144a4
Hermann Pauly <hp@campudus.com>
2024-04-29 15:19:47 +0200

    Upgrade to react-router v6

--- src/app/components/GrudHeader.jsx
+++ src/app/components/GrudHeader.jsx
@@ -1,45 +1,38 @@
-import { connect } from "react-redux";
-import React from "react";
 import f from "lodash/fp";
-
 import PropTypes from "prop-types";
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
 import { ConnectionStatus } from "./header/ConnectionStatus";
 import LanguageSwitcher from "./header/LanguageSwitcher";
 import Navigation from "./header/Navigation";
 import PageTitle from "./header/PageTitle";
 import UserMenu from "./header/UserMenu";
 
-const mapStateToProps = state => {
-  const connectedToBackend = f.prop(["grudStatus", "connectedToBackend"])(
-    state
-  );
-
-  return {
-    connectedToBackend
-  };
-};
-
 const GrudHeader = ({
   children,
   handleLanguageSwitch,
   langtag,
-  pageTitleOrKey,
-  connectedToBackend
-}) => (
-  <div className="grud-header-wrapper">
-    <header className="grud-header">
-      <Navigation langtag={langtag} />
-      {children || <div className="header-separator" />}
-      <PageTitle titleKey={pageTitleOrKey} />
-      <LanguageSwitcher langtag={langtag} onChange={handleLanguageSwitch} />
-      <ConnectionStatus isConnected={connectedToBackend} />
-      <UserMenu langtag={langtag} />
-    </header>
-  </div>
-);
+  pageTitleOrKey
+}) => {
+  const connectedToBackend = useSelector(
+    f.prop(["grudStatus", "connectedToBackend"])
+  );
+  return (
+    <div className="grud-header-wrapper">
+      <header className="grud-header">
+        <Navigation langtag={langtag} />
+        {children || <div className="header-separator" />}
+        <PageTitle titleKey={pageTitleOrKey} />
+        <LanguageSwitcher langtag={langtag} onChange={handleLanguageSwitch} />
+        <ConnectionStatus isConnected={connectedToBackend} />
+        <UserMenu langtag={langtag} />
+      </header>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(GrudHeader);
+export default GrudHeader;
 
 GrudHeader.propTypes = {
   pageTitleOrKey: PropTypes.string.isRequired,
--- src/app/components/header/LanguageSwitcher.jsx
+++ src/app/components/header/LanguageSwitcher.jsx
@@ -1,46 +1,12 @@
 import f from "lodash/fp";
-import Select from "react-select";
-import React from "react";
 import PropTypes from "prop-types";
-import { compose, withHandlers, pure, setPropTypes } from "recompose";
-
-import { Langtags } from "./../../constants/TableauxConstants";
+import React, { useCallback } from "react";
+import Select from "react-select";
 import { getLanguageOrCountryIcon } from "../../helpers/multiLanguage";
-
-const enhance = compose(
-  setPropTypes({
-    langtag: PropTypes.string.isRequired,
-    onChange: PropTypes.func,
-    openOnTop: PropTypes.bool,
-    options: PropTypes.array,
-    disabled: PropTypes.bool,
-    limitLanguages: PropTypes.array
-  }),
-  withHandlers({
-    onChange: props => langObj => {
-      // prevents undefined language tag: we just want to switch the language when there is actually something selected
-      // if (!f.isEmpty(langObj)) {
-      const langtag = langObj.value;
-      if (props.onChange) {
-        props.onChange(langtag);
-      }
-      // }
-    },
-    renderOption: () => option =>
-      getLanguageOrCountryIcon(option.value, "language")
-  }),
-  pure
-);
+import { Langtags } from "./../../constants/TableauxConstants";
 
 const LanguageSwitcher = props => {
-  const {
-    limitLanguages,
-    disabled,
-    langtag,
-    onChange,
-    renderOption,
-    openOnTop
-  } = props;
+  const { disabled, langtag, limitLanguages, onChange, openOnTop } = props;
   const languages = f.isNil(props.languages) ? Langtags : props.languages;
   // Inside select box show user just the languages he has access to
   const languagesToDisplay =
@@ -53,6 +19,21 @@ const LanguageSwitcher = props => {
       label: langtag
     }));
 
+  const handleChange = useCallback(
+    langObj => {
+      const newLangtag = langObj?.value;
+      if (onChange && newLangtag) {
+        onChange(newLangtag);
+      }
+    },
+    [onChange]
+  );
+
+  const renderOption = useCallback(
+    option => getLanguageOrCountryIcon(option.value, "language"),
+    []
+  );
+
   return (
     <div className="language-switcher">
       <Select
@@ -61,7 +42,7 @@ const LanguageSwitcher = props => {
         searchable={false}
         clearable={false}
         value={langtag}
-        onChange={onChange}
+        onChange={handleChange}
         optionRenderer={renderOption}
         valueRenderer={renderOption}
         disabled={disabled}
@@ -70,4 +51,13 @@ const LanguageSwitcher = props => {
   );
 };
 
-export default enhance(LanguageSwitcher);
+LanguageSwitcher.propTypes = {
+  langtag: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  openOnTop: PropTypes.bool,
+  options: PropTypes.array,
+  disabled: PropTypes.bool,
+  limitLanguages: PropTypes.array
+};
+
+export default LanguageSwitcher;
