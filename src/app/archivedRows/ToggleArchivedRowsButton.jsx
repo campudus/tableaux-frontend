1a9a27fd92f9f12063c5da1c2d09700274833851
Hermann Pauly <hp@campudus.com>
2024-05-15 08:33:45 +0200

    Fetch archived rows once when clicking "Show archived" button

--- src/app/archivedRows/ToggleArchivedRowsButton.jsx
+++ src/app/archivedRows/ToggleArchivedRowsButton.jsx
@@ -1,15 +1,22 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Action from "../redux/actionCreators";
 import { selectShowArchivedState } from "../redux/reducers/tableView";
 
 const ToggleArchivedRowsButton = ({ table }) => {
+  // prevent potential massive reload of archived rows
+  const [mustFetchArchivedRows, setMustFetchArchivedRows] = useState(true);
+
   const showArchived = useSelector(selectShowArchivedState);
   const dispatch = useDispatch();
   const className = `filter-wrapper ${showArchived ? "active" : ""}`;
   const toggleArchivedRows = useCallback(() => {
+    if (mustFetchArchivedRows) {
+      dispatch(Action.loadAllRows(table.id, true));
+      setMustFetchArchivedRows(false);
+    }
     dispatch(Action.setShowArchivedRows(table, !showArchived));
-  }, [showArchived]);
+  }, [showArchived, mustFetchArchivedRows]);
 
   return (
     <div className={className}>
