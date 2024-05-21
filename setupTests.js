20a69244532d7eab83ac41fbbdb30d286471dc4e
Manfred Zingl <mz@campudus.com>
2024-05-21 17:30:37 +0200

    fix issue with react-smooth-dnd

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,8 @@
 // babel polyfill: https://babeljs.io/docs/en/babel-polyfill
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// https://github.com/kutlugsahin/react-smooth-dnd/issues/48
+Object.defineProperty(global, "Node", {
+  value: { firstElementChild: "firstElementChild" }
+});
