8726b27cd491312687c5d8b95a4260779bbe6854
Hermann Pauly <hp@campudus.com>
2024-09-30 11:11:02 +0200

    Refactor attachment view, remove unused handler

--- src/app/components/entityView/attachment/AttachmentView.jsx
+++ src/app/components/entityView/attachment/AttachmentView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import LinkList from "../../helperComponents/LinkList";
 import { retrieveTranslation } from "../../../helpers/multiLanguage";
@@ -6,58 +6,42 @@ import i18n from "i18next";
 import apiUrl from "../../../helpers/apiUrl";
 import * as f from "lodash/fp";
 
-class AttachmentView extends Component {
-  constructor(props) {
-    super(props);
-    this.displayName = "AttachmentView";
-  }
+const AttachmentView = ({ actions, cell, children, langtag, value }) => {
+  const attachments =
+    f.size(value) <= f.size(cell.displayValue)
+      ? f.zip(value, cell.displayValue).map(([value, displayValue]) => ({
+          ...value,
+          id: value.uuid,
+          displayName: displayValue[langtag],
+          linkTarget: f.isPlainObject(value.url)
+            ? apiUrl(retrieveTranslation(langtag, value.url))
+            : ""
+        }))
+      : null;
 
-  static propTypes = {
-    langtag: PropTypes.string.isRequired,
-    cell: PropTypes.object.isRequired,
-    tabIdx: PropTypes.number
-  };
+  return f.isEmpty(attachments) ? (
+    <div className="item-description">{i18n.t("table:empty.attachments")}</div>
+  ) : (
+    <div className="item-content link">
+      <LinkList
+        links={attachments}
+        langtag={langtag}
+        sortable
+        cell={cell}
+        value={value}
+        actions={actions}
+        isAttachment
+      />
+      {children}
+    </div>
+  );
+};
 
-  removeAttachment = uuid => () => {
-    const { actions, cell } = this.props;
-    const newValue = cell.value.filter(el => el.uuid !== uuid);
-    actions.changeCellValue({ cell, newValue, oldValue: cell.value });
-  };
-
-  render() {
-    const { cell, langtag, value, actions } = this.props;
-
-    const attachments =
-      f.size(value) === f.size(cell.displayValue)
-        ? f.zip(value, cell.displayValue).map(([value, displayValue]) => ({
-            ...value,
-            id: value.uuid,
-            displayName: displayValue[langtag],
-            linkTarget: f.isPlainObject(value.url)
-              ? apiUrl(retrieveTranslation(langtag, value.url))
-              : ""
-          }))
-        : null;
-
-    return f.isEmpty(attachments) ? (
-      <div className="item-description">
-        {i18n.t("table:empty.attachments")}
-      </div>
-    ) : (
-      <div className="item-content link">
-        <LinkList
-          links={attachments}
-          langtag={langtag}
-          sortable
-          cell={cell}
-          value={value}
-          actions={actions}
-          isAttachment
-        />
-        {this.props.children}
-      </div>
-    );
-  }
-}
+AttachmentView.propTypes = {
+  cell: PropTypes.object.isRequired,
+  langtag: PropTypes.string.isRequired,
+  actions: PropTypes.object.isRequired,
+  value: PropTypes.array.isRequired
+};
 
 export default AttachmentView;
