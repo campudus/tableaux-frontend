f42c6aec4c31e40597c24c896e048169257d1115
Hermann Pauly <hp@campudus.com>
2024-12-18 12:47:24 +0100

    Remove unused prop

--- src/app/components/header/LanguageSwitcher.jsx
+++ src/app/components/header/LanguageSwitcher.jsx
@@ -1,12 +1,12 @@
 import f from "lodash/fp";
 import PropTypes from "prop-types";
 import React, { useCallback } from "react";
-import Select from "../GrudSelect";
 import { getLanguageOrCountryIcon } from "../../helpers/multiLanguage";
+import Select from "../GrudSelect";
 import { Langtags } from "./../../constants/TableauxConstants";
 
 const LanguageSwitcher = props => {
-  const { disabled, langtag, limitLanguages, onChange, openOnTop } = props;
+  const { disabled, langtag, limitLanguages, onChange } = props;
   const languages = f.isNil(props.languages) ? Langtags : props.languages;
   // Inside select box show user just the languages he has access to
   const languagesToDisplay =
