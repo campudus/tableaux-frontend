06dbfce4847779c61e4cdfb7cdf17eb49a8daa61
smnhgn <simon.haugen@campudus.com>
2025-03-05 10:34:13 +0100

    refactor: load config

--- server/config.js
+++ server/config.js
@@ -6,7 +6,22 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const baseDir = path.join(__dirname, "..");
 const outDir = path.join(baseDir, "out");
 
-const configDefaults = {
+const envParams = [
+  "outDir",
+  "apiHost",
+  "apiPort",
+  "host",
+  "port",
+  "webhookUrl",
+  "authClientId",
+  "authServerUrl",
+  "authRealm",
+  "enableHistory",
+  "showTableDropdown",
+  "disableAuth"
+];
+
+const configDefault = {
   enableHistory: true,
   showTableDropdown: true,
   outDir: outDir,
@@ -17,46 +32,34 @@ const configDefaults = {
 };
 
 const loadConfig = configPath => {
-  let config;
+  let configLocal;
 
   try {
     console.error("Config file path", configPath);
     const configJson = fs.readFileSync(configPath, "utf-8");
-    config = JSON.parse(configJson);
+    configLocal = JSON.parse(configJson);
   } catch (err) {
     console.error("Warning: Could not read config file, using defaults");
   }
 
-  for (const envParam of [
-    "outDir",
-    "apiHost",
-    "apiPort",
-    "host",
-    "port",
-    "webhookUrl",
-    "authClientId",
-    "authServerUrl",
-    "authRealm",
-    "enableHistory",
-    "showTableDropdown",
-    "disableAuth"
-  ]) {
-    const envNameSnake = envParam.replace(/([A-Z])/g, "_$1").toUpperCase();
-    const envNameUpper = envParam.toUpperCase();
+  const configEnv = envParams.reduce((config, param) => {
+    const envNameSnake = param.replace(/([A-Z])/g, "_$1").toUpperCase();
+    const envNameUpper = param.toUpperCase();
     const envValue = process.env[envNameSnake] || process.env[envNameUpper];
 
     if (envValue) {
-      console.error("Overriding", envParam, "with", envValue, "from env");
-
+      console.error("Overriding", param, "with", envValue, "from env");
       try {
-        config[envParam] = JSON.parse(envValue); // extract bools and numbers
-      } catch (err) {
-        config[envParam] = envValue;
+        config[param] = JSON.parse(envValue);
+      } catch {
+        config[param] = envValue;
       }
     }
-  }
 
-  return { ...configDefaults, ...config };
+    return config;
+  }, {});
+
+  return { ...configDefault, ...configLocal, ...configEnv };
 };
 
 export default loadConfig;
