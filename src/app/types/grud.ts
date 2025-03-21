83c361eabc6d98d6009a6012d8ae612da3fc93c5
smnhgn <simon.haugen@campudus.com>
2025-03-21 15:11:20 +0100

    fix: grud types

--- src/app/types/grud.ts
+++ src/app/types/grud.ts
@@ -1,34 +1,27 @@
 import {
-  Attachment,
+  Attachment as _Attachment,
   CellValue,
-  CellValueForColumn,
-  Column,
-  ISODateString,
-  LinkColumn,
-  MultilangValue,
-  MultilangCellValue,
-  SingleLangCellValue,
-  Table
+  Column as _Column,
+  Table as _Table
 } from "grud-devtools/types";
 
-// eslint-disable-next-line prettier/prettier
-export type {
-  Attachment,
-  CellValue,
-  CellValueForColumn,
-  Column,
-  ISODateString,
-  LinkColumn,
-  MultilangValue,
-  MultilangCellValue,
-  SingleLangCellValue,
-  Table
-};
+// upgrade prettier to v2 for support of new ts syntax
+// export type { Attachment, Column, Table };
+export type Attachment = _Attachment;
+export type Column = _Column;
+export type Table = _Table;
 
-export type RowValue = CellValue["value"];
+export type Annotation = {
+  uuid: string;
+  type: string;
+  value: string;
+  createdAt: string; // ISOString
+};
 
 export type Row = {
   id: number;
-  final: string;
-  values: RowValue[];
+  final?: boolean;
+  archived?: boolean;
+  values: CellValue["value"][];
+  annotations?: Annotation[][];
 };
