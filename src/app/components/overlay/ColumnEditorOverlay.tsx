04aecbc5d2f281db14f671d6389c1499958c1327
smnhgn <simon.haugen@campudus.com>
2025-03-28 17:35:19 +0100

    fix: ColumnEditorOverlay proper sharedData

--- src/app/components/overlay/ColumnEditorOverlay.tsx
+++ src/app/components/overlay/ColumnEditorOverlay.tsx
@@ -4,37 +4,58 @@ import {
   FocusEvent,
   ReactElement,
   useEffect,
-  useRef,
   useState
 } from "react";
 import Header from "./Header";
 import { Column, Table } from "../../types/grud";
-import { outsideClickEffect } from "../../helpers/useOutsideClick";
 
-type UpdateEvent<T> = ChangeEvent<T> | FocusEvent<T>;
+type ColumnData = Pick<Column, "displayName" | "description">;
 
 type ColumnEditorOverlayProps = {
   langtag: string;
   column: Column;
   table: Table;
-  actions: {
-    editColumn: (
-      columnId: number,
-      tableId: number,
-      data: Pick<Column, "displayName" | "description">
-    ) => void;
+  // provided through hoc
+  sharedData?: ColumnData;
+  updateSharedData?: (updateFn: (data?: ColumnData) => ColumnData) => void;
+  actions?: {
+    editColumn: (columnId: number, tableId: number, data: ColumnData) => void;
   };
-  onClose: () => void;
 };
 
-export default function ColumnEditorOverlay({
+type ColumnEditorOverlayHeaderProps = ColumnEditorOverlayProps;
+type ColumnEditorOverlayBodyProps = Omit<ColumnEditorOverlayProps, "table">;
+type UpdateEvent<T> = ChangeEvent<T> | FocusEvent<T>;
+
+export function ColumnEditorOverlayHeader(
+  props: ColumnEditorOverlayHeaderProps
+): ReactElement {
+  const { langtag, sharedData, actions, column, table } = props;
+
+  const handleUpdateColumn = () => {
+    if (sharedData && actions) {
+      actions.editColumn(column.id, table.id, sharedData);
+    }
+  };
+
+  return (
+    <Header
+      {...props}
+      context={i18n.t("table:editor.edit_column")}
+      title={column.displayName[langtag] || column.name}
+      buttonActions={{
+        positive: [i18n.t("common:save"), handleUpdateColumn],
+        neutral: [i18n.t("common:cancel"), null]
+      }}
+    />
+  );
+}
+
+export function ColumnEditorOverlayBody({
   langtag,
   column,
-  table,
-  actions: { editColumn },
-  onClose
-}: ColumnEditorOverlayProps): ReactElement {
-  const containerRef = useRef(null);
+  updateSharedData
+}: ColumnEditorOverlayBodyProps): ReactElement {
   const [displayName, setDisplayName] = useState(column.displayName[langtag]);
   const [description, setDescription] = useState(column.description[langtag]);
 
@@ -46,66 +67,40 @@ export default function ColumnEditorOverlay({
     setDescription(event.target.value);
   };
 
-  const handleUpdateColumn = () => {
-    editColumn(column.id, table.id, {
-      displayName: { ...column.displayName, [langtag]: displayName },
-      description: { ...column.description, [langtag]: description }
-    });
-    onClose();
-  };
-
-  useEffect(
-    outsideClickEffect({
-      shouldListen: true,
-      containerRef,
-      onOutsideClick: onClose
-    }),
-    [containerRef.current]
-  );
+  useEffect(() => {
+    if (updateSharedData) {
+      updateSharedData(() => ({
+        displayName: { ...column.displayName, [langtag]: displayName },
+        description: { ...column.description, [langtag]: description }
+      }));
+    }
+  }, [displayName, description]);
 
   return (
-    <div className="overlay open active">
-      <div className="overlay-wrapper" ref={containerRef}>
-        <Header
-          context={i18n.t("table:editor.edit_column")}
-          title={column.displayName[langtag] || column.name}
-          buttonActions={{
-            positive: [i18n.t("common:save"), handleUpdateColumn],
-            neutral: [i18n.t("common:cancel"), onClose]
-          }}
-        />
-        <div className="overlay-content">
-          <div className="content-items">
-            <div className="item">
-              <div className="item-header">
-                {i18n.t("table:editor.colname")}
-              </div>
-              <div className="item-description">
-                ({i18n.t("table:editor.sanity_info")})
-              </div>
-              <input
-                type="text"
-                autoFocus
-                className="item-content"
-                onChange={handleUpdateDisplayName}
-                onBlur={handleUpdateDisplayName}
-                value={displayName}
-              />
-            </div>
-            <div className="item">
-              <div className="item-header">
-                {i18n.t("table:editor.description")}
-              </div>
-              <textarea
-                className="item-content"
-                rows={6}
-                onChange={handleUpdateDescription}
-                onBlur={handleUpdateDescription}
-                value={description}
-              />
-            </div>
-          </div>
+    <div className="content-items">
+      <div className="item">
+        <div className="item-header">{i18n.t("table:editor.colname")}</div>
+        <div className="item-description">
+          ({i18n.t("table:editor.sanity_info")})
         </div>
+        <input
+          type="text"
+          autoFocus
+          className="item-content"
+          onChange={handleUpdateDisplayName}
+          onBlur={handleUpdateDisplayName}
+          value={displayName}
+        />
+      </div>
+      <div className="item">
+        <div className="item-header">{i18n.t("table:editor.description")}</div>
+        <textarea
+          className="item-content"
+          rows={6}
+          onChange={handleUpdateDescription}
+          onBlur={handleUpdateDescription}
+          value={description}
+        />
       </div>
     </div>
   );
