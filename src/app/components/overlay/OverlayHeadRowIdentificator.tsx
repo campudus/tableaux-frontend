e90862dab35c1b6fd3c543b9d0014872e36c08ad
smnhgn <simon.haugen@campudus.com>
2025-06-04 09:40:13 +0200

    fix: remove non-null assertion

--- src/app/components/overlay/OverlayHeadRowIdentificator.tsx
+++ src/app/components/overlay/OverlayHeadRowIdentificator.tsx
@@ -1,7 +1,7 @@
 import f from "lodash/fp";
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { Cell, Column, GRUDStore, Row } from "../../types/grud";
+import { Cell, CellValue, Column, GRUDStore } from "../../types/grud";
 import { retrieveTranslation } from "../../helpers/multiLanguage";
 import getDisplayValue from "../../helpers/getDisplayValue";
 
@@ -18,16 +18,18 @@ function OverlayHeadRowIdentificator({
     return null;
   }
 
-  const firstColumn = useSelector<GRUDStore, Column>(state => {
+  const firstColumn = useSelector<GRUDStore, Column | undefined>(state => {
     const columns = state.columns[cell.table.id]?.data;
-    return columns.at(0)!;
+    return columns.at(0);
   });
-  const firstRowValue = useSelector<GRUDStore, Row["values"][number]>(state => {
-    const rows = state.rows[cell.table.id]?.data;
-    const row = rows.find(r => r.id === cell.row.id) ?? cell.row;
-    return row.values.at(0)!;
-  });
-  const displayValue = getDisplayValue(firstColumn)(firstRowValue);
+  const firstCellValue = useSelector<GRUDStore, CellValue["value"] | undefined>(
+    state => {
+      const rows = state.rows[cell.table.id]?.data;
+      const row = rows.find(r => r.id === cell.row.id) ?? cell.row;
+      return row.values.at(0);
+    }
+  );
+  const displayValue = getDisplayValue(firstColumn)(firstCellValue);
   const title = retrieveTranslation(langtag)(displayValue);
   const columnName = retrieveTranslation(langtag)(cell.column?.displayName);
 
