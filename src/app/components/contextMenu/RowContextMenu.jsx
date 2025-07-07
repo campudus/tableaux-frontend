efd25e706ed724d256c1f12cf2f88a765e6920df
TimoGebendorfer <gb.timo@gmail.com>
2025-07-07 10:06:02 +0200

    feat: add preview link to row context menu

--- src/app/components/contextMenu/RowContextMenu.jsx
+++ src/app/components/contextMenu/RowContextMenu.jsx
@@ -31,6 +31,7 @@ import SvgIcon from "../helperComponents/SvgIcon";
 import { openHistoryOverlay } from "../history/HistoryOverlay";
 import AnnotationContextMenu from "./AnnotationContextMenu";
 import GenericContextMenu from "./GenericContextMenu";
+import { Link } from "react-router-dom";
 
 //  Distance between clicked coordinate and the left upper corner of the context menu
 const CLICK_OFFSET = 3;
@@ -259,6 +260,8 @@ class RowContextMenu extends React.Component {
       closeRowContextMenu
     } = this;
 
+    const previewUrl = `/${this.props.langtag}/preview/${table.id}/rows/${cell.row.id}`;
+
     const isSettingsTable = table.type === "settings";
 
     const isDeletingRowAllowed =
@@ -338,6 +341,10 @@ class RowContextMenu extends React.Component {
             ? this.mkItem(showDependency, "show_dependency", "code-fork")
             : null}
           {this.mkItem(showTranslations, "show_translation", "flag")}
+          <Link to={previewUrl}>
+            <i className="fa fa-eye" />
+            <div className="item-label">{"Überprüfen"}</div>
+          </Link>
           {this.setFinalItem()}
           {this.setArchivedItem()}
           {isDeletingRowAllowed || isDuplicatingRowAllowed ? (
