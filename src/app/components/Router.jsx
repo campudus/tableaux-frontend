0c08f5f296e4bbb93ac6ff364d45676386d5c261
TimoGebendorfer <gb.timo@gmail.com>
2025-07-07 14:04:38 +0200

    feat: load preview view data

--- src/app/components/Router.jsx
+++ src/app/components/Router.jsx
@@ -99,6 +99,12 @@ const GRUDRouter = React.memo(() => {
       tables
     );
 
+    if (!tableId || !rowId) {
+      return <Redirect to={`/${langtag}/dashboard`} />;
+    }
+
+    store.dispatch(actionCreators.loadPreviewView(tableId, rowId, columnId));
+
     return renderView(ViewNames.PREVIEW_VIEW)(routeProps);
   });
 
@@ -179,6 +185,10 @@ const GRUDRouter = React.memo(() => {
         <Route path="/media/*" render={() => <Redirect to={withLangtag} />} />
         <Route path="/media" render={() => <Redirect to={withLangtag} />} />
 
+        <CompatRoute
+          path="/:langtag/preview/:tableId/rows/:rowId"
+          render={renderPreviewView}
+        />
         <CompatRoute
           path="/:langtag/preview/:tableId/columns/:columnId/rows/:rowId"
           render={renderPreviewView}
