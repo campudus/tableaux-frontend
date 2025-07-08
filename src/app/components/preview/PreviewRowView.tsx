27df53d61267229a949eef0cb768e2dc270d2e6a
TimoGebendorfer <gb.timo@gmail.com>
2025-07-08 10:01:19 +0200

    feat: add preview row and detail view components

---
+++ src/app/components/preview/PreviewRowView.tsx
@@ -0,0 +1,55 @@
+import { ReactElement } from "react";
+import { Column, Row } from "../../types/grud";
+import Spinner from "../header/Spinner";
+import getDisplayValue from "../../helpers/getDisplayValue";
+
+type PreviewRowViewProps = {
+  langtag: string;
+  tableId: number;
+  columns: Column[] | undefined;
+  row: Row | undefined;
+};
+
+export default function PreviewRowView({
+  langtag,
+  tableId,
+  columns,
+  row
+}: PreviewRowViewProps): ReactElement {
+  return (
+    <div className="preview-row-view">
+      {columns && row ? (
+        <table>
+          <tbody>
+            {columns
+              ?.filter(column => column.id !== 0)
+              .map(column => {
+                const columnLink = `/${langtag}/tables/${tableId}/columns/${column.id}`;
+                const cellLink = `/${langtag}/tables/${tableId}/columns/${column.id}/rows/${row.id}`;
+                let value = getDisplayValue(column)(row?.values.at(column.id));
+
+                if (Array.isArray(value))
+                  value = value.map(v => v[langtag]).join(", ");
+                else {
+                  value = value[langtag];
+                }
+
+                return (
+                  <tr key={column.id}>
+                    <td className="preview-view__column-name">
+                      <a href={columnLink}>{column.displayName[langtag]}</a>
+                    </td>
+                    <td className="preview-view__column-value">
+                      <a href={cellLink}>{value || "n/a"}</a>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      ) : (
+        <Spinner isLoading />
+      )}
+    </div>
+  );
+}
