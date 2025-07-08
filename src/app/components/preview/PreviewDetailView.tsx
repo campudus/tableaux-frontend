afdfc73867a3637b8753f92e9d1feb45c43a4406
TimoGebendorfer <gb.timo@gmail.com>
2025-07-08 10:01:19 +0200

    feat: add preview row and detail view components

---
+++ src/app/components/preview/PreviewDetailView.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+export default function PreviewDetailView(): ReactElement {
+  return (
+    <div className="preview-detail-view">
+      <h4 className="preview-detail-view__title">Preview Detail View</h4>
+      <p>This is a placeholder for the detail view of a preview.</p>
+      {/* Additional content can be added here */}
+    </div>
+  );
+}
