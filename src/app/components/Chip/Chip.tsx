2320ba52e97a106395c7feb98c71489e507d6cef
TimoGebendorfer <gb.timo@gmail.com>
2025-07-15 12:34:34 +0200

    feat: add Chip component

---
+++ src/app/components/Chip/Chip.tsx
@@ -0,0 +1,33 @@
+import { ReactElement, ReactNode } from "react";
+import { buildClassName } from "../../helpers/buildClassName";
+
+type ChipProps = {
+  className?: string;
+  icon?: ReactNode;
+  label: string;
+  onClick?: () => void;
+  isActive?: boolean;
+};
+
+export default function Chip({
+  className,
+  icon,
+  label,
+  onClick,
+  isActive = false
+}: ChipProps): ReactElement {
+  return (
+    <div
+      className={buildClassName(
+        "chip",
+        { active: isActive, clickable: onClick },
+        className
+      )}
+      onClick={onClick}
+      role="button"
+    >
+      {icon && <span className="chip__icon">{icon}</span>}
+      <span className="chip__label">{label}</span>
+    </div>
+  );
+}
