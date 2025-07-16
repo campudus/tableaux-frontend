9611dd1dae97a4bf56311bdc44077b58f0cd2dec
TimoGebendorfer <gb.timo@gmail.com>
2025-07-16 16:36:14 +0200

    feat: add CellValueLink component

---
+++ src/app/components/preview/CellValueLink.tsx
@@ -0,0 +1,54 @@
+import { Column, Row } from "../../types/grud";
+import f from "lodash/fp";
+import getDisplayValue from "../../helpers/getDisplayValue";
+import BooleanCell from "./cells/BooleanCell";
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import ArrayCell from "./cells/ArrayCell";
+import { buildClassName } from "../../helpers/buildClassName";
+import { setEmptyClassName } from "./helper";
+
+type CellValueLinkProps = {
+  column: Column;
+  columnIndex: number;
+  row: Row;
+  link: string;
+  langtag: string;
+};
+
+export default function CellValueLink({
+  column,
+  columnIndex,
+  row,
+  link,
+  langtag
+}: CellValueLinkProps): ReactElement {
+  const rowValue =
+    row.values.length > 1
+      ? row?.values.at(columnIndex)
+      : row.values[columnIndex];
+
+  const value = getDisplayValue(column)(rowValue);
+
+  function renderCellValue(): ReactElement {
+    if (f.isBoolean(rowValue)) {
+      return <BooleanCell langtag={langtag} value={rowValue} />;
+    }
+
+    if (Array.isArray(value)) {
+      return <ArrayCell langtag={langtag} values={value} />;
+    }
+
+    return (
+      <span className={`text-cell ${setEmptyClassName(value[langtag])}`}>
+        {value[langtag] || "Leer"}
+      </span>
+    );
+  }
+
+  return (
+    <Link className="cell-value-link" to={link}>
+      {renderCellValue()}
+    </Link>
+  );
+}
