5d1197a33e67273738d4a1796629bcca92fe5c92
TimoGebendorfer <gb.timo@gmail.com>
2025-07-23 12:22:51 +0200

    feat: add config of default selected preview detail column via column attributes

--- src/app/components/preview/constants.ts
+++ src/app/components/preview/constants.ts
@@ -1,9 +1,37 @@
 import { Column } from "../../types/grud";
+import { ColumnAndRow } from "./helper";
 
 export const attributeKeys = {
-  PREVIEW_TITLE: "previewTitle"
+  PREVIEW_TITLE: "previewTitle",
+  PREVIEW_DEFAULT_SELECTED: "previewDefaultSelected"
 };
 
 export function isPreviewTitle(column: Column): boolean {
   return !!column.attributes?.[attributeKeys.PREVIEW_TITLE];
+}
+
+export function isValidPreviewColumn(column: Column): boolean {
+  return column.kind === "link" || column.kind === "richtext";
+}
+
+export function getDefaultSelectedColumnId(
+  columnsAndRow: ColumnAndRow[]
+): number | undefined {
+  const columnSetAsDefaultSelected = columnsAndRow.find(
+    ({ column }) => column.attributes?.[attributeKeys.PREVIEW_DEFAULT_SELECTED]
+  )?.column;
+
+  if (columnSetAsDefaultSelected) {
+    return columnSetAsDefaultSelected.id;
+  }
+
+  const validPreviewDetailColumn = columnsAndRow.find(
+    ({ column }) => isValidPreviewColumn(column) && !isPreviewTitle(column)
+  )?.column;
+
+  if (validPreviewDetailColumn) {
+    return validPreviewDetailColumn.id;
+  }
+
+  return undefined;
 }
