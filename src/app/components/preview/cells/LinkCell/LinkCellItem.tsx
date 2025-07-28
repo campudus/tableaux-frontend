48584c6397ba4f49f8f7f19beee767b70f1c3f9b
TimoGebendorfer <gb.timo@gmail.com>
2025-07-28 13:47:36 +0200

    feat: add tooltip with path to column to link cells

--- src/app/components/preview/cells/LinkCell/LinkCellItem.tsx
+++ src/app/components/preview/cells/LinkCell/LinkCellItem.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useState } from "react";
 import f from "lodash/fp";
 import { Column } from "../../../../types/grud";
 import getDisplayValue from "../../../../helpers/getDisplayValue";
 import { setEmptyClassName } from "../../helper";
+import { useDebouncedValue } from "../../../../helpers/useDebouncedValue";
+import Tooltip from "../../../../components/helperComponents/Tooltip/Tooltip";
 
 type LinkCellItemProps = {
   langtag: string;
   column: Column;
   value: any;
   link: string;
+  path: (string | undefined)[];
   isLast: boolean;
 };
 
@@ -18,8 +21,15 @@ export default function LinkCellItem({
   column,
   value,
   link,
+  path,
   isLast
 }: LinkCellItemProps): ReactElement {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const debouncedTooltipVisible = useDebouncedValue(tooltipVisible, 600);
+  const isVisible = tooltipVisible && debouncedTooltipVisible; // delay on open, but close instantly
+  const handleMouseEnter = useCallback(() => setTooltipVisible(true), []);
+  const handleMouseLeave = useCallback(() => setTooltipVisible(false), []);
+
   let displayValue = value;
 
   if (Array.isArray(value)) {
@@ -35,18 +45,25 @@ export default function LinkCellItem({
   }
 
   if (f.isBoolean(displayValue)) {
-    displayValue = displayValue.toString();
+    displayValue = displayValue ? "WAHR" : "FALSCH";
   }
 
   return (
     <div className="link-cell-item">
       <a
-        className={`link-cell__item__value preview-cell-value-link ${setEmptyClassName(
+        className={`link-cell-item__value preview-cell-value-link ${setEmptyClassName(
           displayValue
         )}`}
         href={link}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         {!displayValue || displayValue === "" ? "Leer" : displayValue}
+        {isVisible && (
+          <Tooltip defaultInvert style={{ left: "10px" }}>
+            {path.join(" / ")}
+          </Tooltip>
+        )}
       </a>
 
       {!isLast && <span className="link-cell-item__separator">&bull;</span>}
