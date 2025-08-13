b40910fc89e75381bf9e07d9510276cdb4be9de5
smnhgn <simon.haugen@campudus.com>
2025-08-13 18:34:44 +0200

    fix: positioning of cell

--- src/app/components/cells/currency/CurrencyCell.tsx
+++ src/app/components/cells/currency/CurrencyCell.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import f from "lodash/fp";
 import { ReactElement } from "react";
+import {
+  autoUpdate,
+  useFloating,
+  autoPlacement,
+  offset
+} from "@floating-ui/react-dom";
 import { useDispatch } from "react-redux";
 import actions from "../../../redux/actionCreators";
 import { Cell, Country, CurrencyColumn } from "../../../types/grud";
@@ -24,6 +30,22 @@ export default function CurrencyCell({
   editing,
   langtag
 }: CurrencyCellProps): ReactElement {
+  const { refs, floatingStyles } = useFloating({
+    placement: "top-start",
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset({
+        mainAxis: -22.5,
+        crossAxis: 0,
+        alignmentAxis: 0
+      }),
+      autoPlacement({
+        alignment: "start",
+        allowedPlacements: ["top-start", "bottom-start"],
+        padding: 10
+      })
+    ]
+  });
   const dispatch = useDispatch();
   const column = cell.column as CurrencyColumn;
   const country = getCountryOfLangtag(langtag) as Country;
@@ -43,37 +65,47 @@ export default function CurrencyCell({
   };
 
   return (
-    <div className="cell-content">
-      {countries.map(country => {
-        const canEdit = canUserChangeCountryTypeCell(cell)(country);
-        const value = (cell.value as any)[country];
+    <>
+      <div
+        ref={refs.setReference}
+        style={editing ? { alignSelf: "center" } : {}}
+      />
+      <div
+        ref={refs.setFloating}
+        style={editing ? floatingStyles : {}}
+        className="cell-content"
+      >
+        {countries.map(country => {
+          const canEdit = canUserChangeCountryTypeCell(cell)(country);
+          const value = (cell.value as any)[country];
 
-        return (
-          <div
-            key={country}
-            className={f
-              .compact(["currency-item", f.isNil(value) && "grey-out"])
-              .join(" ")}
-          >
-            {editing && (
-              <div className="country-code">
-                {getLanguageOrCountryIcon(country)}
-              </div>
-            )}
-
-            <CurrencyInput
+          return (
+            <div
               key={country}
-              langtag={langtag}
-              country={country}
-              value={value}
-              onBlur={handleBlur}
-              disabled={!editing || !canEdit}
-            />
+              className={f
+                .compact(["currency-item", f.isNil(value) && "grey-out"])
+                .join(" ")}
+            >
+              {editing && (
+                <div className="country-code">
+                  {getLanguageOrCountryIcon(country)}
+                </div>
+              )}
 
-            <div className="currency-code">{getCurrencyCode(country)}</div>
-          </div>
-        );
-      })}
-    </div>
+              <CurrencyInput
+                key={country}
+                langtag={langtag}
+                country={country}
+                value={value}
+                onBlur={handleBlur}
+                disabled={!editing || !canEdit}
+              />
+
+              <div className="currency-code">{getCurrencyCode(country)}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
