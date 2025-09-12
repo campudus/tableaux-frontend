f0829bc18241af0eb8277a8610095dc05faab5d9
TimoGebendorfer <gb.timo@gmail.com>
2025-09-12 15:16:44 +0200

    feat: add reusable previewUrl function

--- src/app/components/contextMenu/RowContextMenu.jsx
+++ src/app/components/contextMenu/RowContextMenu.jsx
@@ -30,6 +30,7 @@ import { openHistoryOverlay } from "../history/HistoryOverlay";
 import AnnotationContextMenu from "./AnnotationContextMenu";
 import GenericContextMenu from "./GenericContextMenu";
 import { Link } from "react-router-dom";
+import { previewUrl } from "../../helpers/apiUrl";
 
 // Distance between clicked coordinate and the left upper corner of the context menu
 const CLICK_OFFSET = 3;
@@ -256,9 +257,6 @@ class RowContextMenu extends React.Component {
       },
       closeRowContextMenu
     } = this;
-
-    const previewUrl = `/${this.props.langtag}/preview/${table.id}/rows/${cell.row.id}`;
-
     const isSettingsTable = table.type === "settings";
 
     const isDeletingRowAllowed =
@@ -321,7 +319,13 @@ class RowContextMenu extends React.Component {
             : this.mkItem(showEntityView, "show_entity_view", "server")}
           {this.mkItem(showDependency, "show_dependency", "code-fork")}
           {this.mkItem(showTranslations, "show_translation", "flag")}
-          <Link to={previewUrl}>
+          <Link
+            to={previewUrl({
+              langtag: this.props.langtag,
+              tableId: table.id,
+              rowId: cell.row.id
+            })}
+          >
             <i className="fa fa-eye" />
             <div className="item-label">{t("preview:open_preview_view")}</div>
           </Link>
