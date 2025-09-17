a515bab7f9be02918a1169dcf6da086b5cb0712e
TimoGebendorfer <gb.timo@gmail.com>
2025-09-17 11:52:06 +0200

    refactor: simplify LinkCell and LinkListCell components by extracting rendering logic into separate functions

--- src/app/components/preview/cells/LinkCell/LinkCell.tsx
+++ src/app/components/preview/cells/LinkCell/LinkCell.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ConcatColumn, LinkColumn } from "@grud/devtools/types";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement } from "react";
 import { getEmptyClassName } from "../../helper";
 import LinkListCell from "./LinkListCell";
 import LinkCellItem from "./LinkCellItem";
@@ -15,28 +15,19 @@ type LinkCellProps = {
   link: string;
 };
 
-export default function LinkCell({
-  langtag,
-  column,
-  values,
-  link
-}: LinkCellProps): ReactElement {
-  function renderLinkCell(): ReactNode {
-    if (!values || values.length === 0) {
-      return (
-        <a
-          className={`link-cell__item  ${getEmptyClassName(values)}`}
-          href={link}
-        >
-          {i18n.t("preview:empty")}
-        </a>
-      );
-    }
+const EmptyLink = ({ link, values }: { link: string; values: any[] }) => {
+  return (
+    <a className={`link-cell__item ${getEmptyClassName(values)}`} href={link}>
+      {i18n.t("preview:empty")}
+    </a>
+  );
+};
 
-    if (column.toColumn.kind !== "concat") {
-      return values.map((entry, index) => {
+const SingleLinkItems = ({ langtag, column, values }: LinkCellProps) => {
+  return (
+    <>
+      {values.map((entry, index) => {
         const currentColumn = column.toColumn;
-
         return (
           <LinkCellItem
             key={`${entry.id}-${index}`}
@@ -56,20 +47,19 @@ export default function LinkCell({
             isLast={index === values.length - 1}
           />
         );
-      });
-    }
+      })}
+    </>
+  );
+};
 
-    if (
-      (column.toColumn.kind === "concat" &&
-        column.constraint?.cardinality?.to === 1) ||
-      values.length === 1
-    ) {
-      return values.map(entry => {
-        return entry.value.map((value: any, index: number) => {
+const ConcatSingleItems = ({ langtag, column, values }: LinkCellProps) => {
+  return (
+    <>
+      {values.map(entry =>
+        entry.value.map((value: any, index: number) => {
           const currentColumn = (column.toColumn as ConcatColumn).concats.at(
             index
           )!;
-
           return (
             <LinkCellItem
               key={`${entry.id}-${index}`}
@@ -89,14 +79,48 @@ export default function LinkCell({
               isLast={index === entry.value.length - 1}
             />
           );
-        });
-      });
-    }
+        })
+      )}
+    </>
+  );
+};
 
+const cssClass = "link-cell";
+
+export default function LinkCell(props: LinkCellProps): ReactElement {
+  const { values, column, link, langtag } = props;
+
+  if (!values || values.length === 0) {
     return (
-      <LinkListCell langtag={langtag} linkColumn={column} values={values} />
+      <div className={cssClass}>
+        <EmptyLink link={link} values={values} />
+      </div>
     );
   }
 
-  return <div className="link-cell">{renderLinkCell()}</div>;
+  if (column.toColumn.kind !== "concat") {
+    return (
+      <div className={cssClass}>
+        <SingleLinkItems {...props} />
+      </div>
+    );
+  }
+
+  if (
+    (column.toColumn.kind === "concat" &&
+      column.constraint?.cardinality?.to === 1) ||
+    values.length === 1
+  ) {
+    return (
+      <div className={cssClass}>
+        <ConcatSingleItems {...props} />
+      </div>
+    );
+  }
+
+  return (
+    <div className={cssClass}>
+      <LinkListCell langtag={langtag} linkColumn={column} values={values} />
+    </div>
+  );
 }
