3d3a62b11db3253012a74f2a8b226bb4f243506a
smnhgn <simon.haugen@campudus.com>
2025-09-30 13:21:52 +0200

    feat: add breadcrumbs ellipse with dropdown

--- src/app/components/helperComponents/Breadcrumbs.tsx
+++ src/app/components/helperComponents/Breadcrumbs.tsx
@@ -1,24 +1,80 @@
 import React, { Fragment, MouseEvent, ReactNode } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { buildClassName as cn } from "../../helpers/buildClassName";
+import ButtonAction from "./ButtonAction";
+
+type BreadcrumbsLink = {
+  label: ReactNode;
+  isActive?: boolean;
+} & (
+  | { path: string; onClick?: never }
+  | {
+      path?: never;
+      onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+    }
+);
+
+function BreadcrumsbLink({ path, label, onClick, isActive }: BreadcrumbsLink) {
+  return path ? (
+    <NavLink
+      to={path}
+      className={cn("breadcrumbs__link", {}, isActive ? "active" : "")}
+      exact
+    >
+      {label}
+    </NavLink>
+  ) : (
+    <button
+      onClick={onClick}
+      className={cn("breadcrumbs__link", {}, isActive ? "active" : "")}
+    >
+      {label}
+    </button>
+  );
+}
 
 export type BreadcrumbsProps = {
   className?: string;
   // Allow either path or onClick, but not both
-  links: (
-    | { label: ReactNode; path: string; onClick?: never }
-    | {
-        label: ReactNode;
-        path?: never;
-        onClick: (event: MouseEvent<HTMLButtonElement>) => void;
-      }
-  )[];
+  links: BreadcrumbsLink[];
 };
 
 export default function Breadcrumbs({
   className,
   links = []
 }: BreadcrumbsProps) {
+  const history = useHistory();
+  const needsDropdown = links.length > 3;
+
+  const navigate = (path: string) => {
+    history.push(path);
+  };
+
+  if (needsDropdown) {
+    const firstLink = links.at(0);
+    const menuLinks = links.slice(1, -1);
+    const lastLink = links.at(-1);
+
+    return (
+      <div className={cn("breadcrumbs", {}, className)}>
+        <BreadcrumsbLink {...firstLink!} />
+        <i className="fa fa-angle-right breadcrumbs__icon" />
+        <ButtonAction
+          variant="text"
+          label={"..."}
+          options={menuLinks.map(link => {
+            return {
+              label: link.label,
+              onClick: link.onClick ?? (() => navigate(link.path))
+            };
+          })}
+        />
+        <i className="fa fa-angle-right breadcrumbs__icon" />
+        <BreadcrumsbLink {...lastLink!} isActive />
+      </div>
+    );
+  }
+
   return (
     <div className={cn("breadcrumbs", {}, className)}>
       {links.map((link, index) => {
@@ -29,22 +85,7 @@ export default function Breadcrumbs({
           <Fragment key={link.path ?? index}>
             {!isFirst && <i className="fa fa-angle-right breadcrumbs__icon" />}
 
-            {link.path ? (
-              <NavLink to={link.path} className="breadcrumbs__link" exact>
-                {link.label}
-              </NavLink>
-            ) : (
-              <button
-                onClick={link.onClick}
-                className={cn(
-                  "breadcrumbs__link",
-                  {},
-                  isActive ? "active" : ""
-                )}
-              >
-                {link.label}
-              </button>
-            )}
+            <BreadcrumsbLink {...link} isActive={isActive} />
           </Fragment>
         );
       })}
