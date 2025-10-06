f63505fe8e045f5f70d7ececf2e00576610c8a4a
TimoGebendorfer <gb.timo@gmail.com>
2025-10-06 15:25:48 +0200

    feat: add tooltip for all title elements

--- src/app/components/preview/cells/TextCell.tsx
+++ src/app/components/preview/cells/TextCell.tsx
@@ -1,19 +1,34 @@
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useState } from "react";
 import { getEmptyClassName } from "../helper";
 import i18n from "i18next";
+import { useDebouncedValue } from "../../../helpers/useDebouncedValue";
+import { getColumnDisplayName } from "../../../helpers/multiLanguage";
+import Tooltip from "../../helperComponents/Tooltip/Tooltip";
+import { Column } from "../../../types/grud";
 
 type TextCellProps = {
   langtag: string;
+  column: Column;
   multilangValue: Record<string, string>;
+  isTitle?: boolean;
 };
 
 const TEXT_MAX_LENGTH = 250;
 
 export default function TextCell({
   langtag,
-  multilangValue
+  column,
+  multilangValue,
+  isTitle = false
 }: TextCellProps): ReactElement {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const debouncedTooltipVisible = useDebouncedValue(tooltipVisible, 600);
+  const isVisible = tooltipVisible && debouncedTooltipVisible; // delay on open, but close instantly
+  const handleMouseEnter = useCallback(() => setTooltipVisible(true), []);
+  const handleMouseLeave = useCallback(() => setTooltipVisible(false), []);
+
   const value = multilangValue[langtag];
+  const columnDisplayName = getColumnDisplayName(column, langtag);
 
   if (value && value.length > TEXT_MAX_LENGTH) {
     return (
@@ -22,8 +37,18 @@ export default function TextCell({
   }
 
   return (
-    <span className={`text-cell ${getEmptyClassName(value)}`}>
+    <span
+      className={`text-cell ${getEmptyClassName(value)}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       {value || i18n.t("preview:empty")}
+
+      {isTitle && isVisible && columnDisplayName && (
+        <Tooltip defaultInvert style={{ left: "10px", fontSize: "13px" }}>
+          {columnDisplayName}
+        </Tooltip>
+      )}
     </span>
   );
 }
