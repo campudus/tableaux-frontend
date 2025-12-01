60d12503a7923dfe1a8043151eeb8107ca136309
hermann-p <hp@campudus.com>
2025-12-01 14:16:20 +0100

    GRUD_DEV-1194: Fix (rich-)text columns not expanding when read only

--- src/app/components/cells/text/TextCell.jsx
+++ src/app/components/cells/text/TextCell.jsx
@@ -1,5 +1,5 @@
 import f from "lodash/fp";
-import React, { useEffect } from "react";
+import React from "react";
 import { canUserChangeCell } from "../../../helpers/accessManagementHelper";
 import { getTableDisplayName } from "../../../helpers/multiLanguage";
 import MarkdownEditor from "../../markdownEditor/MarkdownEditor";
@@ -8,24 +8,14 @@ import ExpandButton from "./ExpandButton.jsx";
 import TextEditOverlay from "./TextEditOverlay";
 
 const TextCell = props => {
-  const {
-    actions,
-    cell,
-    table,
-    langtag,
-    displayValue,
-    editing,
-    selected,
-    value
-  } = props;
+  const { actions, cell, table, langtag, displayValue, selected } = props;
   const isMultiLine = f.contains("\n", displayValue[langtag]);
 
-  useEffect(() => {
-    if (editing) {
-      const editable = canUserChangeCell(cell, langtag);
-      const context = getTableDisplayName(table, langtag);
-      const isRichText = cell.column.kind === "richtext";
-
+  const handleExpandContent = () => {
+    const editable = canUserChangeCell(cell, langtag);
+    const context = getTableDisplayName(table, langtag);
+    const isRichText = cell.column.kind === "richtext";
+    const showFullContent = () =>
       actions.openOverlay({
         head: <Header context={context} langtag={langtag} />,
         body: isRichText ? (
@@ -37,23 +27,23 @@ const TextCell = props => {
         type: "full-height",
         classes: isRichText ? "text-editor-overlay" : ""
       });
-      actions.toggleCellEditing({ editing: false });
-    }
-  }, [editing, value]);
 
-  const handleSetEditing = () => {
-    actions.toggleCellEditing({ editing: true });
+    if (selected) showFullContent();
+    if (editable) actions.toggleCellEditing({ editing: false });
   };
 
   return (
     <>
-      <div className={`cell-content ${isMultiLine ? "is-multiline" : ""}`}>
+      <div
+        className={`cell-content ${isMultiLine ? "is-multiline" : ""}`}
+        onClick={handleExpandContent}
+      >
         <div>{displayValue[langtag].split("\n")[0]}</div>
         {isMultiLine ? (
           <i className="fa fa-paragraph multiline-indicator" />
         ) : null}
       </div>
-      {selected ? <ExpandButton onTrigger={handleSetEditing} /> : null}
+      {selected ? <ExpandButton onTrigger={handleExpandContent} /> : null}
     </>
   );
 };
