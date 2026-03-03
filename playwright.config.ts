17ce87d3ca93c1d6f7afa28bd31eccde4b0fdbc5
smnhgn <simon.haugen@campudus.com>
2026-03-03 12:28:16 +0100

    chore: add docker setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,12 +21,13 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  timeout: 5_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.GRUD_FRONTEND_URL,
+    baseURL: "http://localhost:5000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -43,15 +44,15 @@ export default defineConfig({
     //   use: { ...devices["Desktop Chrome"] }
     // },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] }
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] }
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] }
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] }
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -72,12 +73,12 @@ export default defineConfig({
       name: "Google Chrome",
       use: { ...devices["Desktop Chrome"], channel: "chrome" }
     }
-  ],
+  ]
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: process.env.GRUD_FRONTEND_URL,
-    reuseExistingServer: !process.env.CI
-  }
+  // webServer: {
+  //   command: "npm run start",
+  //   url: "http://localhost:3000",
+  //   reuseExistingServer: !process.env.CI
+  // }
 });
